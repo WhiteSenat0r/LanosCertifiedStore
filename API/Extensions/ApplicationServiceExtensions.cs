1503dbfd0cdc86407245e2873d21c8abd57a7502
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-05 20:33:35 +0200

    NuGet packages and few more bones to project skeleton added.

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace API.Extensions;
+
+internal static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        services.AddCors(opt =>
+        {
+            opt.AddPolicy("CorsPolicy",
+                policy => { policy.AllowAnyHeader().AllowAnyMethod().WithOrigins(config["ClientUrl"]!); });
+        });
+
+        return services;
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,11 @@
+using API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -15,8 +15,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
