5e1d65be74865b664c5dae13f8931cdc8580a261
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-06 16:41:46 +0200

    ExceptionMiddleware added

---
+++ API/Responses/ApiException.cs
@@ -0,0 +1,7 @@
+﻿namespace API.Responses;
+
+internal class ApiException(int statusCode, string message = null, string details = null)
+    : ApiResponse(statusCode, message)
+{
+    public string Details { get; set; } = details;
+}
