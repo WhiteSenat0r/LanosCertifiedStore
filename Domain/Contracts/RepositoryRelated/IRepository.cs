3ed81f2c2c8d8da039a96a5081448847095ce57e
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-06 14:57:06 +0200

    Created IRepository interface for the further Repository pattern implementation

---
+++ Domain/Contracts/RepositoryRelated/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Contracts.Common;
+
+namespace Domain.Contracts.RepositoryRelated;
+
+public interface IRepository<TEntity> 
+    where TEntity : class, IEntity<Guid>
+{
+    Task<List<TEntity>> GetAllEntitiesAsync
+        (IQuerySpecification<TEntity> querySpecification);
+
+    Task<TEntity> GetSingleEntityBySpecificationAsync
+        (IQuerySpecification<TEntity> querySpecification);
+
+    Task AddNewEntityAsync(TEntity entity);
+    
+    Task AddNewRangeOfEntitiesAsync(IEnumerable<TEntity> entities);
+    
+    void UpdateExistingEntity(TEntity updatedEntity);
+    
+    void UpdateRangeOfExistingEntities(IEnumerable<TEntity> updatedEntities);
+    
+    void RemoveExistingEntity(TEntity removedEntity);
+    
+    void RemoveRangeOfExistingEntities(IEnumerable<TEntity> removedEntities);
+
+    Task<int> CountAsync(IQuerySpecification<TEntity> querySpecification);
+}
