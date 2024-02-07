cc7eb1e35a2b7185ff0d4298e0dca89782f1ad1d
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-07 20:40:18 +0200

    Added delete model command and relevant handler

---
+++ Application/Commands/Models/DeleteModel/DeleteModelCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Application.Core;
+using Application.QuerySpecifications.ModelRelated;
+using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using MediatR;
+
+namespace Application.Commands.Models.DeleteModel;
+
+internal sealed class DeleteModelCommandHandler(IRepository<VehicleModel> modelRepository, IUnitOfWork unitOfWork)
+    : IRequestHandler<DeleteModelCommand, Result<Unit>>
+{
+    public async Task<Result<Unit>> Handle(DeleteModelCommand request, CancellationToken cancellationToken)
+    {
+        var deletedModel = await modelRepository.GetSingleEntityBySpecificationAsync(
+                new ModelByNameQuerySpecification(request.Name));
+
+        if (deletedModel is null)
+            return Result<Unit>.Failure("Such model doesn't exist!");
+        
+        modelRepository.RemoveExistingEntity(deletedModel);
+
+        var result = await unitOfWork.SaveChangesAsync(cancellationToken) > 0;
+
+        return result ? Result<Unit>.Success(Unit.Value) : Result<Unit>.Failure("Failed to delete model!");
+    }
+}
