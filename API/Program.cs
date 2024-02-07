eed9e725fbe681ee49609cbe182158c8d4901c7b
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-07 13:26:15 +0200

    Automatic migrations applyment

--- API/Program.cs
+++ API/Program.cs
@@ -28,4 +28,19 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<ApplicationDatabaseContext>();
+    await context.Database.MigrateAsync();
+}
+
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration!");
+}
+
 app.Run();
