53bdf12881d8d78a7f8b25c0e419c52b6023c54e
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-07 20:43:13 +0200

    Added ModelByNameQuerySpecification and ModelQuerySpecification

---
+++ Application/QuerySpecifications/ModelRelated/ModelQuerySpecification.cs
@@ -0,0 +1,6 @@
+﻿using Application.QuerySpecifications.Common.Classes;
+using Domain.Entities.VehicleRelated.Classes;
+
+namespace Application.QuerySpecifications.ModelRelated;
+
+public sealed class ModelQuerySpecification : QuerySpecification<VehicleModel>;
