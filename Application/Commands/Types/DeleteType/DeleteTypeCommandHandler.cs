dce22865f71557dd287dfd5bae0304136b34ef68
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-08 11:08:45 +0200

    Added delete vehicle type command and a relevant handler

---
+++ Application/Commands/Types/DeleteType/DeleteTypeCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Application.Core;
+using Application.QuerySpecifications.TypeRelated;
+using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using MediatR;
+
+namespace Application.Commands.Types.DeleteType;
+
+internal sealed class DeleteTypeCommandHandler(IRepository<VehicleType> tyeRepository, IUnitOfWork unitOfWork)
+    : IRequestHandler<DeleteTypeCommand, Result<Unit>>
+{
+    public async Task<Result<Unit>> Handle(DeleteTypeCommand request, CancellationToken cancellationToken)
+    {
+        var deletedType = await tyeRepository.GetSingleEntityBySpecificationAsync(
+                new TypeByNameQuerySpecification(request.Name));
+
+        if (deletedType is null)
+            return Result<Unit>.Failure("Such type doesn't exist!");
+        
+        tyeRepository.RemoveExistingEntity(deletedType);
+
+        var result = await unitOfWork.SaveChangesAsync(cancellationToken) > 0;
+
+        return result ? Result<Unit>.Success(Unit.Value) : Result<Unit>.Failure("Failed to delete type!");
+    }
+}
