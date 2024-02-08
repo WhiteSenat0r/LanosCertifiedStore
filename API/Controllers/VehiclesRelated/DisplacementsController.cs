9e8364ee36a1f3b635d6d33b0374e44bdba2b4b6
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-08 13:26:29 +0200

    Updated methods' names in DisplacementsController.cs

--- API/Controllers/VehiclesRelated/DisplacementsController.cs
+++ API/Controllers/VehiclesRelated/DisplacementsController.cs
@@ -11,25 +11,25 @@ namespace API.Controllers.VehiclesRelated;
 public sealed class DisplacementsController : BaseEntityRelatedApiController
 {
     [HttpGet]
-    public async Task<IActionResult> GetBrands()
+    public async Task<IActionResult> GetDisplacements()
     {
         return HandleResult(await Mediator.Send(new ListModelsQuery()));
     }
 
     [HttpPost]
-    public async Task<IActionResult> CreateBrand([FromQuery] double value)
+    public async Task<IActionResult> CreateDisplacement([FromQuery] double value)
     {
         return HandleResult(await Mediator.Send(new CreateDisplacementCommand(value)));
     }
 
     [HttpPut]
-    public async Task<IActionResult> UpdateBrand([FromBody] UpdateDisplacementDto updateDisplacementDto)
+    public async Task<IActionResult> UpdateDisplacement([FromBody] UpdateDisplacementDto updateDisplacementDto)
     {
         return HandleResult(await Mediator.Send(new UpdateDisplacementCommand(updateDisplacementDto)));
     }
 
     [HttpDelete("{value}")]
-    public async Task<IActionResult> DeleteBrand(double value)
+    public async Task<IActionResult> DeleteDisplacement(double value)
     {
         return HandleResult(await Mediator.Send(new DeleteDisplacementCommand(value)));
     }
