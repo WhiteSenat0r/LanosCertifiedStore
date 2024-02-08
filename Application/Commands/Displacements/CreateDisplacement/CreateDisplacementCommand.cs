3e9371bf7250dc6df575de260fedeadaa11fa676
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-08 13:21:21 +0200

    Added create vehicle displacement command and a relevant handler

---
+++ Application/Commands/Displacements/CreateDisplacement/CreateDisplacementCommand.cs
@@ -0,0 +1,6 @@
+﻿using Application.Core;
+using MediatR;
+
+namespace Application.Commands.Displacements.CreateDisplacement;
+
+public sealed record CreateDisplacementCommand(double Value) : IRequest<Result<Unit>>;
