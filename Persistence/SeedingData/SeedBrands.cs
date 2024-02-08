fb5f029459fbe8f5d2747c3f00be71a857bd8c4a
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-08 21:52:26 +0200

    Brands, Colors and Types seeds added.

---
+++ Persistence/SeedingData/SeedBrands.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities.VehicleRelated.Classes;
+
+namespace Persistence.SeedingData;
+
+internal static class SeedBrands
+{
+    public static List<VehicleBrand> GetBrands() =>
+    [
+        new VehicleBrand("Toyota"),
+        new VehicleBrand("Ford"),
+        new VehicleBrand("Honda"),
+        new VehicleBrand("Chevrolet"),
+        new VehicleBrand("Volkswagen"),
+        new VehicleBrand("Nissan"),
+        new VehicleBrand("BMW"),
+        new VehicleBrand("Mercedes-Benz"),
+        new VehicleBrand("Audi"),
+        new VehicleBrand("Hyundai"),
+        new VehicleBrand("Kia"),
+        new VehicleBrand("Mazda"),
+        new VehicleBrand("Subaru"),
+        new VehicleBrand("Lexus"),
+        new VehicleBrand("Jeep"),
+        new VehicleBrand("Tesla"),
+        new VehicleBrand("Volvo"),
+        new VehicleBrand("Porsche"),
+        new VehicleBrand("Ferrari"),
+        new VehicleBrand("Jaguar")
+    ];
+}
---
+++ Persistence/SeedingData/SeedData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence.Contexts;
+
+namespace Persistence.SeedingData;
+
+public static class SeedData
+{
+    public static async Task Seed(ApplicationDatabaseContext context)
+    {
+        if (!await context.VehicleTypes.AnyAsync())
+            await context.VehicleTypes.AddRangeAsync(SeedTypes.GetTypes());
+
+        if (!await context.VehiclesColors.AnyAsync())
+            await context.VehiclesColors.AddRangeAsync(SeedColors.GetColors());
+
+        if (!await context.VehicleDisplacements.AnyAsync())
+            await context.VehicleDisplacements.AddRangeAsync(SeedDisplacements.GetDisplacements());
+
+        if (context.ChangeTracker.HasChanges())
+            await context.SaveChangesAsync();
+    }
+}
