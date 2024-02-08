580cde7f42e931a99d9be47712486eeb31e903ab
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-08 21:37:54 +0200

    Renamed CreateVehicleDto.cs to ActionVehicleDto.cs

--- Application/Dtos/VehicleDtos/ActionVehicleDto.cs
+++ Application/Dtos/VehicleDtos/ActionVehicleDto.cs
@@ -1,7 +1,8 @@
 ﻿namespace Application.Dtos.VehicleDtos;
 
-public sealed class CreateVehicleDto
+public sealed class ActionVehicleDto
 {
+    public Guid Id { get; set; }
     public string Description { get; set; }
     public Guid BrandId { get; set; }
     public Guid ModelId { get; set; }
