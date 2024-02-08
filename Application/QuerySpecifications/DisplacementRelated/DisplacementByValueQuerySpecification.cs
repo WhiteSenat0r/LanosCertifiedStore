eb166db964f8198df0d75b087eec019d68d62815
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-08 13:18:05 +0200

    Added vehicle displacement-related query specifications

---
+++ Application/QuerySpecifications/DisplacementRelated/DisplacementByValueQuerySpecification.cs
@@ -0,0 +1,7 @@
+﻿using Application.QuerySpecifications.Common.Classes;
+using Domain.Entities.VehicleRelated.Classes;
+
+namespace Application.QuerySpecifications.DisplacementRelated;
+
+public sealed class DisplacementByValueQuerySpecification(double value)
+    : QuerySpecification<VehicleDisplacement>(m => m.Value.Equals(value));
---
+++ Application/QuerySpecifications/DisplacementRelated/DisplacementQuerySpecification.cs
@@ -0,0 +1,6 @@
+﻿using Application.QuerySpecifications.Common.Classes;
+using Domain.Entities.VehicleRelated.Classes;
+
+namespace Application.QuerySpecifications.DisplacementRelated;
+
+public sealed class DisplacementQuerySpecification : QuerySpecification<VehicleDisplacement>;
