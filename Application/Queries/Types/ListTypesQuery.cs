d1478e2673a6a6f8ea5c587cd6c28a3eb2eb42f1
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-08 11:00:49 +0200

    Added vehicle type-related query and a relevant handler

---
+++ Application/Queries/Types/ListTypesQuery.cs
@@ -0,0 +1,7 @@
+﻿using Application.Core;
+using Application.Dtos.TypeDtos;
+using MediatR;
+
+namespace Application.Queries.Types;
+
+public sealed record ListTypesQuery : IRequest<Result<IReadOnlyList<TypeDto>>>;
