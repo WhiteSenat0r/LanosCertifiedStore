5c4d9c46006df68743addf09b5a96bccc9b74081
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-08 15:06:40 +0200

    Added documenting attributes for each of vehicle-related controllers

--- API/Controllers/VehiclesRelated/ColorsController.cs
+++ API/Controllers/VehiclesRelated/ColorsController.cs
@@ -1,9 +1,12 @@
 ﻿using API.Controllers.Common;
+using API.Responses;
 using Application.Commands.Colors.CreateColor;
 using Application.Commands.Colors.DeleteColor;
 using Application.Commands.Colors.UpdateColor;
+using Application.Core;
 using Application.Dtos.ColorDtos;
 using Application.Queries.Colors;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers.VehiclesRelated;
@@ -11,24 +14,36 @@ namespace API.Controllers.VehiclesRelated;
 public sealed class ColorsController : BaseEntityRelatedApiController
 {
     [HttpGet]
+    [ProducesResponseType(typeof(Result<IReadOnlyList<ColorDto>>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> GetColors()
     {
         return HandleResult(await Mediator.Send(new ListColorsQuery()));
     }
 
     [HttpPost]
+    [ProducesResponseType(typeof(Result<Unit>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> CreateColor([FromQuery] string name)
     {
         return HandleResult(await Mediator.Send(new CreateColorCommand(name)));
     }
 
     [HttpPut]
+    [ProducesResponseType(typeof(Result<Unit>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> UpdateColor([FromBody] UpdateColorDto updateColorDto)
     {
         return HandleResult(await Mediator.Send(new UpdateColorCommand(updateColorDto)));
     }
 
     [HttpDelete("{name}")]
+    [ProducesResponseType(typeof(Result<Unit>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> DeleteColor(string name)
     {
         return HandleResult(await Mediator.Send(new DeleteColorCommand(name)));
--- API/Controllers/VehiclesRelated/TypesController.cs
+++ API/Controllers/VehiclesRelated/TypesController.cs
@@ -1,9 +1,12 @@
 ﻿using API.Controllers.Common;
+using API.Responses;
 using Application.Commands.Types.CreateType;
 using Application.Commands.Types.DeleteType;
 using Application.Commands.Types.UpdateType;
+using Application.Core;
 using Application.Dtos.TypeDtos;
 using Application.Queries.Types;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers.VehiclesRelated;
@@ -11,24 +14,36 @@ namespace API.Controllers.VehiclesRelated;
 public sealed class TypesController : BaseEntityRelatedApiController
 {
     [HttpGet]
+    [ProducesResponseType(typeof(Result<IReadOnlyList<TypeDto>>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> GetTypes()
     {
         return HandleResult(await Mediator.Send(new ListTypesQuery()));
     }
 
     [HttpPost]
+    [ProducesResponseType(typeof(Result<Unit>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> CreateType([FromQuery] string name)
     {
         return HandleResult(await Mediator.Send(new CreateTypeCommand(name)));
     }
 
     [HttpPut]
+    [ProducesResponseType(typeof(Result<Unit>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> UpdateType([FromBody] UpdateTypeDto updateTypeDto)
     {
         return HandleResult(await Mediator.Send(new UpdateTypeCommand(updateTypeDto)));
     }
 
     [HttpDelete("{name}")]
+    [ProducesResponseType(typeof(Result<Unit>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> DeleteType(string name)
     {
         return HandleResult(await Mediator.Send(new DeleteTypeCommand(name)));
