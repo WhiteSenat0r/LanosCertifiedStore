2c1b8a0fdcaebb403a9a3e25ddd4db839667838d
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-08 13:22:11 +0200

    Added DisplacementsController.cs

---
+++ API/Controllers/VehiclesRelated/DisplacementsController.cs
@@ -0,0 +1,36 @@
+﻿using API.Controllers.Common;
+using Application.Commands.Displacements.CreateDisplacement;
+using Application.Commands.Displacements.DeleteDisplacement;
+using Application.Commands.Displacements.UpdateDisplacement;
+using Application.Dtos.DisplacementDtos;
+using Application.Queries.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.VehiclesRelated;
+
+public sealed class DisplacementsController : BaseEntityRelatedApiController
+{
+    [HttpGet]
+    public async Task<IActionResult> GetBrands()
+    {
+        return HandleResult(await Mediator.Send(new ListModelsQuery()));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateBrand([FromQuery] double value)
+    {
+        return HandleResult(await Mediator.Send(new CreateDisplacementCommand(value)));
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateBrand([FromBody] UpdateDisplacementDto updateDisplacementDto)
+    {
+        return HandleResult(await Mediator.Send(new UpdateDisplacementCommand(updateDisplacementDto)));
+    }
+
+    [HttpDelete("{value}")]
+    public async Task<IActionResult> DeleteBrand(double value)
+    {
+        return HandleResult(await Mediator.Send(new DeleteDisplacementCommand(value)));
+    }
+}
