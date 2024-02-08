71303fd78d807c7ad802957df41c909a4c4f0582
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-08 21:37:15 +0200

    Updated VehicleByIdQuerySpecification.cs

--- Application/QuerySpecifications/VehiclesRelated/VehicleByIdQuerySpecification.cs
+++ Application/QuerySpecifications/VehiclesRelated/VehicleByIdQuerySpecification.cs
@@ -3,4 +3,15 @@ using Domain.Entities.VehicleRelated.Classes;
 
 namespace Application.QuerySpecifications.VehiclesRelated;
 
-public class VehicleByIdQuerySpecification(Guid id) : QuerySpecification<Vehicle>(x => x.Id == id);
+public class VehicleByIdQuerySpecification : QuerySpecification<Vehicle>
+{
+    public VehicleByIdQuerySpecification(Guid id) 
+        : base(v => v.Id.Equals(id))
+    {
+        AddInclude(v => v.Brand);
+        AddInclude(v => v.Model);
+        AddInclude(v => v.Displacement);
+        AddInclude(v => v.Color);
+        AddInclude(v => v.Price);
+    }
+}
