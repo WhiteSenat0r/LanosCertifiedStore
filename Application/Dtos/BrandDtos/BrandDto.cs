f9f74e2f645356569522f057193159d26e1478b6
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-08 23:28:05 +0200

    Seeding implemented

--- Application/Dtos/BrandDtos/BrandDto.cs
+++ Application/Dtos/BrandDtos/BrandDto.cs
@@ -1,5 +1,9 @@
 ﻿using Application.Dtos.Common;
+using Application.Dtos.ModelDtos;
 
 namespace Application.Dtos.BrandDtos;
 
-public sealed class BrandDto : VehicleAspectDto;
+public sealed class BrandDto : VehicleAspectDto
+{
+    public IEnumerable<ModelDto> Models { get; set; }
+}
