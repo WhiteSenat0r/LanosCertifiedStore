f45a6ffeb774c89dbfee8fe8802be2ccb1579155
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-08 13:21:30 +0200

    Added delete vehicle displacement command and a relevant handler

---
+++ Application/Commands/Displacements/DeleteDisplacement/DeleteDisplacementCommand.cs
@@ -0,0 +1,6 @@
+﻿using Application.Core;
+using MediatR;
+
+namespace Application.Commands.Displacements.DeleteDisplacement;
+
+public sealed record DeleteDisplacementCommand(double Value) : IRequest<Result<Unit>>;
---
+++ Application/Commands/Displacements/DeleteDisplacement/DeleteDisplacementCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Application.Core;
+using Application.QuerySpecifications.DisplacementRelated;
+using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using MediatR;
+
+namespace Application.Commands.Displacements.DeleteDisplacement;
+
+internal sealed class DeleteDisplacementCommandHandler(
+    IRepository<VehicleDisplacement> displacementRepository, IUnitOfWork unitOfWork)
+    : IRequestHandler<DeleteDisplacementCommand, Result<Unit>>
+{
+    public async Task<Result<Unit>> Handle(DeleteDisplacementCommand request, CancellationToken cancellationToken)
+    {
+        var deletedDisplacement = await displacementRepository.GetSingleEntityBySpecificationAsync(
+                new DisplacementByValueQuerySpecification(request.Value));
+
+        if (deletedDisplacement is null)
+            return Result<Unit>.Failure("Such displacement doesn't exist!");
+        
+        displacementRepository.RemoveExistingEntity(deletedDisplacement);
+
+        var result = await unitOfWork.SaveChangesAsync(cancellationToken) > 0;
+
+        return result ? Result<Unit>.Success(Unit.Value) : Result<Unit>.Failure("Failed to delete displacement!");
+    }
+}
