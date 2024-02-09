a597b89b5b42ceba7aad54e23249c5a96aafb3ed
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-09 11:11:03 +0200

    Updated type-related query specifications

--- Application/QuerySpecifications/TypeRelated/TypeQuerySpecification.cs
+++ Application/QuerySpecifications/TypeRelated/TypeQuerySpecification.cs
@@ -3,4 +3,11 @@ using Domain.Entities.VehicleRelated.Classes;
 
 namespace Application.QuerySpecifications.TypeRelated;
 
-public class TypeQuerySpecification : QuerySpecification<VehicleType>;
+public class TypeQuerySpecification : QuerySpecification<VehicleType>
+{
+    public TypeQuerySpecification(bool isNotTracked = false)
+    {
+        IsNotTracked = isNotTracked;
+        AddInclude(t => t.Vehicles);
+    }
+}
