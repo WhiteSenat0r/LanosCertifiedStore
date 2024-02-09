033861805ab44a179c9eefc0bb7b9b19e03d8fa9
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-09 16:52:10 +0200

    Updated inheritance in VehicleQuerySpecification class, added methods for filtering, sorting and pagination support

--- Application/QuerySpecifications/VehiclesRelated/VehicleQuerySpecification.cs
+++ Application/QuerySpecifications/VehiclesRelated/VehicleQuerySpecification.cs
@@ -1,18 +1,68 @@
-﻿using Application.QuerySpecifications.Common.Classes;
+﻿using System.Linq.Expressions;
+using Application.QuerySpecifications.Common.Classes;
+using Application.RequestParams;
+using Domain.Contracts.RepositoryRelated;
 using Domain.Entities.VehicleRelated.Classes;
 
 namespace Application.QuerySpecifications.VehiclesRelated;
 
-public class VehicleQuerySpecification : QuerySpecification<Vehicle>
+public sealed class VehicleQuerySpecification : BaseQuerySpecification<Vehicle>
 {
-    public VehicleQuerySpecification(bool isNotTracked = false)
+    public VehicleQuerySpecification(VehicleRequestParameters requestParameters) : base(requestParameters)
     {
-        IsNotTracked = isNotTracked;
+        AddIncludes();
+        Criteria = GetVehicleFilteringCriteria(requestParameters);
+        DeterminateSortingType(requestParameters);
+    }
+
+    private Expression<Func<Vehicle, bool>> GetVehicleFilteringCriteria(
+        VehicleRequestParameters requestParameters) =>
+        vehicle => 
+            (string.IsNullOrEmpty(requestParameters.Brand) ||
+             vehicle.Brand.Name.Equals(requestParameters.Brand)) &&
+            (string.IsNullOrEmpty(requestParameters.Type) ||
+             vehicle.Type.Name.Equals(requestParameters.Type)) &&
+            (string.IsNullOrEmpty(requestParameters.Color) ||
+             vehicle.Color.Name.Equals(requestParameters.Color)) &&
+            (!requestParameters.Displacement.HasValue ||
+             vehicle.Displacement.Value.Equals(requestParameters.Displacement)) &&
+            (!requestParameters.LowerPriceLimit.HasValue || 
+             vehicle.Prices.MaxBy(p => p.IssueDate).Value >= requestParameters.LowerPriceLimit 
+             && vehicle.Prices.MaxBy(
+                 p => p.IssueDate).IssueDate.Date >= requestParameters.MinimalPriceDate.Value) &&
+            (!requestParameters.LowerPriceLimit.HasValue || 
+             vehicle.Prices.MaxBy(p => p.IssueDate).Value <= requestParameters.UpperPriceLimit
+             && vehicle.Prices.MaxBy(
+                 p => p.IssueDate).IssueDate.Date >= requestParameters.MinimalPriceDate.Value);
+
+    private void AddIncludes()
+    {
         AddInclude(v => v.Brand);
         AddInclude(v => v.Model);
         AddInclude(v => v.Displacement);
         AddInclude(v => v.Color);
         AddInclude(v => v.Type);
         AddInclude(v => v.Prices);
+    }
+
+    private protected override void DeterminateSortingType(IRequestParameters requestParameters)
+    {
+        switch (requestParameters.SortingType)
+        {
+            case "brand-desc":
+                AddOrderByDescending(v => v.Brand.Name);
+                break;
+            case "price-asc":
+                AddOrderByAscending(v => v.Prices.OrderBy(p => p.IssueDate)
+                    .ThenByDescending(p => p.Value).FirstOrDefault());
+                break;
+            case "price-desc":
+                AddOrderByDescending(v => v.Prices.OrderByDescending(p => p.IssueDate)
+                    .ThenByDescending(p => p.Value).FirstOrDefault());
+                break;
+            case "brand-asc":
+                AddOrderByAscending(v => v.Brand.Name);
+                break;
+        }
     }
 }
