b30a361ce32993e1bd8adcdab016a09f992dd9c4
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-09 16:52:52 +0200

    Added PaginationResult class

---
+++ Application/Core/PaginationResult.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Contracts.RepositoryRelated;
+
+namespace Application.Core;
+
+public sealed class PaginationResult<T>
+{
+    public PaginationResult(
+        IEnumerable<T> items, IRequestParameters requestParameters, int totalItemsQuantity)
+    {
+        Items = items;
+        CurrentPageItemsQuantity = Items.Count();
+        PageIndex = requestParameters.PageIndex;
+        TotalItemsQuantity = totalItemsQuantity;
+    }
+    
+    public IEnumerable<T> Items { get; }
+    
+    public int TotalItemsQuantity { get; }
+    
+    public int CurrentPageItemsQuantity { get; }
+    
+    public int PageIndex { get; }
+}
