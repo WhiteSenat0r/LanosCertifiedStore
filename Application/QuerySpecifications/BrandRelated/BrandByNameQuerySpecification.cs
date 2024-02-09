5a93c7e8d884961878bfe0b6f13ccefb7ab2f875
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-09 11:10:34 +0200

    Updated brand-related query specifications

--- Application/QuerySpecifications/BrandRelated/BrandByNameQuerySpecification.cs
+++ Application/QuerySpecifications/BrandRelated/BrandByNameQuerySpecification.cs
@@ -3,5 +3,13 @@ using Domain.Entities.VehicleRelated.Classes;
 
 namespace Application.QuerySpecifications.BrandRelated;
 
-public sealed class BrandByNameQuerySpecification(string name)
-    : QuerySpecification<VehicleBrand>(b => b.Name.Equals(name));
+public sealed class BrandByNameQuerySpecification
+    : QuerySpecification<VehicleBrand>
+{
+    public BrandByNameQuerySpecification(string name, bool isNotTracked = false) 
+        : base(b => b.Name.Equals(name))
+    {
+        IsNotTracked = isNotTracked;
+        AddInclude(b => b.Models);
+    }
+}
