2dd223839c5b009813494abf077d0903e479474a
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-09 16:50:12 +0200

    Added RequestParameters base class and VehicleRequestParameters

---
+++ Application/RequestParams/Common/Classes/RequestParameters.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Contracts.RepositoryRelated;
+
+namespace Application.RequestParams.Common.Classes;
+
+public abstract class RequestParameters : IRequestParameters
+{
+    private int _itemQuantity = MaximumItemQuantity;
+
+    public const int MaximumItemQuantity = 24;
+
+    public int PageIndex { get; set; } = 1;
+
+    public int ItemQuantity
+    {
+        get => _itemQuantity;
+        set => _itemQuantity = value > MaximumItemQuantity ? MaximumItemQuantity : value;
+    }
+
+    public string SortingType { get; set; }
+}
