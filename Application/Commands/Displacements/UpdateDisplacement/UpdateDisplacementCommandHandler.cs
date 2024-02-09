ab6434db1ca90494811f40d5c0f5ec7e5a9074c0
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-09 13:59:39 +0200

    Added additional check during displacement update process

--- Application/Commands/Displacements/UpdateDisplacement/UpdateDisplacementCommandHandler.cs
+++ Application/Commands/Displacements/UpdateDisplacement/UpdateDisplacementCommandHandler.cs
@@ -16,6 +16,12 @@ internal sealed class UpdateDisplacementCommandHandler(
                 new DisplacementByValueQuerySpecification(request.UpdateDisplacementDto.CurrentValue));
 
         if (existingDisplacement is null) return null;
+        
+        var updatedValueDisplacement = await displacementRepository.GetSingleEntityBySpecificationAsync(
+            new DisplacementByValueQuerySpecification(request.UpdateDisplacementDto.UpdatedValue));
+
+        if (updatedValueDisplacement is not null)
+            return Result<Unit>.Failure("Displacement with such value already exists!");
 
         existingDisplacement.Value = request.UpdateDisplacementDto.UpdatedValue;
         
