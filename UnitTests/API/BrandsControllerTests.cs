c2db3a7d3c95ebcdb2285960973410fef24bc241
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-11 20:43:25 +0200

    Added brand controller tests

---
+++ UnitTests/API/BrandsControllerTests.cs
@@ -0,0 +1,190 @@
+﻿using API.Controllers.VehiclesRelated;
+using Application.Commands.Brands.CreateBrand;
+using Application.Commands.Brands.DeleteBrand;
+using Application.Commands.Brands.UpdateBrand;
+using Application.Core;
+using Application.Dtos.BrandDtos;
+using Application.Queries.Brands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace UnitTests.API;
+
+public class BrandsControllerTests
+{
+    private readonly Mock<IMediator> _mediatorMock;
+    private readonly BrandsController _brandsController;
+
+    public BrandsControllerTests()
+    {
+        _mediatorMock = new Mock<IMediator>();
+        _brandsController = new BrandsController
+        {
+            ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    RequestServices = new ServiceCollection()
+                        .AddSingleton(_mediatorMock.Object).BuildServiceProvider()
+                }
+            }
+        };
+    }
+
+    [Fact]
+    public async Task GetBrands_ReturnsOk_WhenSuccessful()
+    {
+        // Arrange
+        var brands = new List<BrandDto>();
+        _mediatorMock.Setup(m => m.Send(
+            It.IsAny<ListBrandsQuery>(), default))
+            .ReturnsAsync(Result<IReadOnlyList<BrandDto>>.Success(brands));
+
+        // Act
+        var result = await _brandsController.GetBrands() as OkObjectResult;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+        Assert.Equal(brands, result.Value);
+    }
+    
+    [Fact]
+    public async Task GetBrands_ReturnsNotFound_WhenResultIsNullOrEmpty()
+    {
+        // Arrange
+        _mediatorMock.Setup(m => m.Send(
+                It.IsAny<ListBrandsQuery>(), default))
+            .ReturnsAsync(Result<IReadOnlyList<BrandDto>>.Success(null));
+
+        // Act
+        var result = await _brandsController.GetBrands() as NotFoundObjectResult;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(StatusCodes.Status404NotFound, result.StatusCode);
+        Assert.NotNull(result.Value);
+    }
+    
+    [Fact]
+    public async Task GetBrands_ReturnsBadRequest_WhenResultIsFailure()
+    {
+        // Arrange
+        _mediatorMock.Setup(m => m.Send(
+                It.IsAny<ListBrandsQuery>(), default))
+            .ReturnsAsync(Result<IReadOnlyList<BrandDto>>.Failure(null));
+
+        // Act
+        var result = await _brandsController.GetBrands() as BadRequestObjectResult;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(StatusCodes.Status400BadRequest, result.StatusCode);
+        Assert.NotNull(result.Value);
+    }
+    
+    [Fact]
+    public async Task CreateBrand_ReturnsOk_WhenSuccessful()
+    {
+        // Arrange
+        _mediatorMock.Setup(m => m.Send(
+                It.IsAny<CreateBrandCommand>(), default))
+            .ReturnsAsync(Result<Unit>.Success(new Unit()));
+
+        // Act
+        var result = await _brandsController.CreateBrand("TestBrand") as OkObjectResult;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+    }
+    
+    [Fact]
+    public async Task CreateBrand_ReturnsBadRequest_WhenResultIsFailure()
+    {
+        // Arrange
+        _mediatorMock.Setup(m => m.Send(
+                It.IsAny<CreateBrandCommand>(), default))
+            .ReturnsAsync(Result<Unit>.Failure(null));
+
+        // Act
+        var result = await _brandsController.CreateBrand(null) as BadRequestObjectResult;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(StatusCodes.Status400BadRequest, result.StatusCode);
+    }
+    
+    [Fact]
+    public async Task UpdateBrand_ReturnsOk_WhenSuccessful()
+    {
+        // Arrange
+        _mediatorMock.Setup(m => m.Send(
+                It.IsAny<UpdateBrandCommand>(), default))
+            .ReturnsAsync(Result<Unit>.Success(new Unit()));
+
+        // Act
+        var result = await _brandsController.UpdateBrand(
+            new UpdateBrandDto
+        {
+            CurrentName = "Test",
+            UpdatedName = "TestUpdated"
+        }) as OkObjectResult;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+    }
+    
+    [Fact]
+    public async Task UpdateBrand_ReturnsBadRequest_WhenResultIsFailure()
+    {
+        // Arrange
+        _mediatorMock.Setup(m => m.Send(
+                It.IsAny<UpdateBrandCommand>(), default))
+            .ReturnsAsync(Result<Unit>.Failure(null));
+
+        // Act
+        var result = await _brandsController.UpdateBrand(null) as BadRequestObjectResult;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(StatusCodes.Status400BadRequest, result.StatusCode);
+        Assert.NotNull(result.Value);
+    }
+    
+    [Fact]
+    public async Task DeleteBrand_ReturnsOk_WhenSuccessful()
+    {
+        // Arrange
+        _mediatorMock.Setup(m => m.Send(
+                It.IsAny<DeleteBrandCommand>(), default))
+            .ReturnsAsync(Result<Unit>.Success(new Unit()));
+
+        // Act
+        var result = await _brandsController.DeleteBrand("Test") as OkObjectResult;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(StatusCodes.Status200OK, result.StatusCode);
+    }
+    
+    [Fact]
+    public async Task DeleteBrand_ReturnsBadRequest_WhenResultIsFailure()
+    {
+        // Arrange
+        _mediatorMock.Setup(m => m.Send(
+                It.IsAny<DeleteBrandCommand>(), default))
+            .ReturnsAsync(Result<Unit>.Failure(null));
+
+        // Act
+        var result = await _brandsController.DeleteBrand("Test") as BadRequestObjectResult;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(StatusCodes.Status400BadRequest, result.StatusCode);
+    }
+}
