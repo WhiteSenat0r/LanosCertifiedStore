4a533155027dc4d97a5c3fc2136ab9eb359fb1a9
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-12 02:28:34 +0200

    Displacement commands covered with tests

---
+++ UnitTests/Application/Commands/Displacements/CreateDisplacementCommandTest.cs
@@ -0,0 +1,60 @@
+﻿using Application.Commands.Displacements.CreateDisplacement;
+using Application.Core;
+using Application.QuerySpecifications.DisplacementRelated;
+using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using MediatR;
+using Moq;
+
+namespace UnitTests.Application.Commands.Displacements;
+
+public class CreateDisplacementCommandTest
+{
+    private readonly Mock<IRepository<VehicleDisplacement>> displacementRepositoryMock = new();
+    private readonly Mock<IUnitOfWork> unitOfWorkMock = new();
+    
+    private const double DisplacementValue = 2.0;
+    
+    [Fact]
+    public async Task Handle_Should_ReturnFailureResult_IfDisplacementAlreadyExists()
+    {
+        // Arrange
+
+        var command = new CreateDisplacementCommand(DisplacementValue);
+        
+        displacementRepositoryMock.Setup(x =>
+                x.GetSingleEntityBySpecificationAsync(It.IsAny<DisplacementByValueQuerySpecification>()))
+            .ReturnsAsync(new VehicleDisplacement(DisplacementValue));
+
+        var handler = new CreateDisplacementCommandHandler(displacementRepositoryMock.Object, unitOfWorkMock.Object);
+
+        // Act
+        var result = await handler.Handle(command, default);
+
+        // Assert 
+        Assert.False(result.IsSuccess);
+        Assert.IsType<Result<Unit>>(result);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnSuccessResult_IfDisplacementDoesNotExists()
+    {
+        // Arrange
+        var command = new CreateDisplacementCommand(DisplacementValue);
+
+        displacementRepositoryMock.Setup(x =>
+                x.GetSingleEntityBySpecificationAsync(It.IsAny<DisplacementByValueQuerySpecification>()))
+            .ReturnsAsync((VehicleDisplacement)null!);
+
+        unitOfWorkMock.Setup(x => x.SaveChangesAsync(default)).ReturnsAsync(1);
+
+        var handler = new CreateDisplacementCommandHandler(displacementRepositoryMock.Object, unitOfWorkMock.Object);
+
+        // Act
+        var result = await handler.Handle(command, default);
+
+        // Assert 
+        Assert.True(result.IsSuccess);
+        Assert.IsType<Result<Unit>>(result);
+    }
+}
---
+++ UnitTests/Application/Commands/Displacements/UpdateDisplacementCommandTest.cs
@@ -0,0 +1,86 @@
+﻿using Application.Commands.Displacements.UpdateDisplacement;
+using Application.Core;
+using Application.Dtos.DisplacementDtos;
+using Application.QuerySpecifications.DisplacementRelated;
+using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using MediatR;
+using Moq;
+
+namespace UnitTests.Application.Commands.Displacements;
+
+public class UpdateDisplacementCommandTest
+{
+    private readonly Mock<IRepository<VehicleDisplacement>> displacementRepositoryMock = new();
+    private readonly Mock<IUnitOfWork> unitOfWorkMock = new();
+
+    private readonly UpdateDisplacementDto updateDisplacementDto = new()
+    {
+        CurrentValue = 2.0,
+        UpdatedValue = 2.5
+    };
+
+    [Fact]
+    public async Task Handle_Should_ReturnFailureResult_IfDisplacementWithCurrentNameIsNull()
+    {
+        // Arrange
+        var command = new UpdateDisplacementCommand(updateDisplacementDto);
+
+        displacementRepositoryMock.Setup(x =>
+                x.GetSingleEntityBySpecificationAsync(It.IsAny<DisplacementByValueQuerySpecification>()))
+            .ReturnsAsync((VehicleDisplacement)null!);
+
+        var handler = new UpdateDisplacementCommandHandler(displacementRepositoryMock.Object, unitOfWorkMock.Object);
+
+        // Act
+        var result = await handler.Handle(command, default);
+
+        // Assert
+        Assert.False(result.IsSuccess);
+        Assert.IsType<Result<Unit>>(result);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnFailureResult_IfDisplacementWithUpdatedNameIsNotNull()
+    {
+        // Arrange
+        var command = new UpdateDisplacementCommand(updateDisplacementDto);
+
+        displacementRepositoryMock.SetupSequence(x =>
+                x.GetSingleEntityBySpecificationAsync(It.IsAny<DisplacementByValueQuerySpecification>()))
+            .ReturnsAsync(new VehicleDisplacement(updateDisplacementDto.CurrentValue))
+            .ReturnsAsync(new VehicleDisplacement(updateDisplacementDto.UpdatedValue));
+
+        var handler = new UpdateDisplacementCommandHandler(displacementRepositoryMock.Object, unitOfWorkMock.Object);
+
+        // Act
+        var result = await handler.Handle(command, default);
+
+        // Assert
+        Assert.False(result.IsSuccess);
+        Assert.IsType<Result<Unit>>(result);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnSuccessResult_IfDisplacementIsNotNullAndDisplacementWithUpdatedNameDoesNotExists()
+    {
+        // Arrange
+        var command = new UpdateDisplacementCommand(updateDisplacementDto);
+
+        displacementRepositoryMock.SetupSequence(x =>
+                x.GetSingleEntityBySpecificationAsync(It.IsAny<DisplacementByValueQuerySpecification>()))
+            .ReturnsAsync(new VehicleDisplacement(updateDisplacementDto.CurrentValue))
+            .ReturnsAsync((VehicleDisplacement)null!);
+        
+        unitOfWorkMock.Setup(x => x.SaveChangesAsync(default)).ReturnsAsync(1);
+        
+        var handler = new UpdateDisplacementCommandHandler(displacementRepositoryMock.Object, unitOfWorkMock.Object);
+
+        // Act
+        var result = await handler.Handle(command, default);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.IsType<Result<Unit>>(result);
+    }
+}
