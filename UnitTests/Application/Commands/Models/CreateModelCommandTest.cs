81167aab93c187363f0d9ede22451d277d2f3ca2
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-12 02:28:25 +0200

    Model commands covered with tests

---
+++ UnitTests/Application/Commands/Models/CreateModelCommandTest.cs
@@ -0,0 +1,61 @@
+﻿using Application.Commands.Models.CreateModel;
+using Application.Core;
+using Application.QuerySpecifications.ModelRelated;
+using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using MediatR;
+using Moq;
+
+namespace UnitTests.Application.Commands.Models;
+
+public class CreateModelCommandTest
+{
+    private readonly Mock<IRepository<VehicleModel>> modelRepositoryMock = new();
+    private readonly Mock<IUnitOfWork> unitOfWorkMock = new();
+    
+    private const string ModelName = "test color";
+    private const string BrandName = "test brand";
+
+    private VehicleBrand VehicleBrand { get; } = new VehicleBrand(BrandName);
+    
+    [Fact]
+    public async Task Handle_Should_ReturnFailureResult_IfModelAlreadyExists()
+    {
+        var command = new CreateModelCommand(VehicleBrand.Id, ModelName);
+        
+        modelRepositoryMock.Setup(x =>
+                x.GetSingleEntityBySpecificationAsync(It.IsAny<ModelByNameQuerySpecification>()))
+            .ReturnsAsync(new VehicleModel(VehicleBrand.Id, ModelName));
+
+        var handler = new CreateModelCommandHandler(modelRepositoryMock.Object, unitOfWorkMock.Object);
+
+        // Act
+        var result = await handler.Handle(command, default);
+
+        // Assert 
+        Assert.False(result.IsSuccess);
+        Assert.IsType<Result<Unit>>(result);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnSuccessResult_IfModelDoesNotExists()
+    {
+        // Arrange
+        var command = new CreateModelCommand(VehicleBrand.Id, ModelName);
+
+        modelRepositoryMock.Setup(x =>
+                x.GetSingleEntityBySpecificationAsync(It.IsAny<ModelQuerySpecification>()))
+            .ReturnsAsync((VehicleModel)null!);
+
+        unitOfWorkMock.Setup(x => x.SaveChangesAsync(default)).ReturnsAsync(1);
+
+        var handler = new CreateModelCommandHandler(modelRepositoryMock.Object, unitOfWorkMock.Object);
+
+        // Act
+        var result = await handler.Handle(command, default);
+
+        // Assert 
+        Assert.True(result.IsSuccess);
+        Assert.IsType<Result<Unit>>(result);
+    }
+}
---
+++ UnitTests/Application/Commands/Models/UpdateModelCommandTest.cs
@@ -0,0 +1,90 @@
+﻿using Application.Commands.Models.UpdateModel;
+using Application.Core;
+using Application.Dtos.ModelDtos;
+using Application.QuerySpecifications.ModelRelated;
+using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using MediatR;
+using Moq;
+
+namespace UnitTests.Application.Commands.Models;
+
+public class UpdateModelCommandTest
+{
+    private readonly Mock<IRepository<VehicleModel>> modelRepositoryMock = new();
+    private readonly Mock<IUnitOfWork> unitOfWorkMock = new();
+
+    private readonly UpdateModelDto updateModelDto = new()
+    {
+        CurrentName = "testCurrentName",
+        UpdatedName = "testUpdatedName"
+    };
+
+    private const string BrandName = "test brand";
+
+    private VehicleBrand VehicleBrand { get; } = new VehicleBrand(BrandName);
+
+    [Fact]
+    public async Task Handle_Should_ReturnFailureResult_IfModelWithCurrentNameIsNull()
+    {
+        // Arrange
+        var command = new UpdateModelCommand(updateModelDto);
+
+        modelRepositoryMock.Setup(x =>
+                x.GetSingleEntityBySpecificationAsync(It.IsAny<ModelByNameQuerySpecification>()))
+            .ReturnsAsync((VehicleModel)null!);
+
+        var handler = new UpdateModelCommandHandler(modelRepositoryMock.Object, unitOfWorkMock.Object);
+
+        // Act
+        var result = await handler.Handle(command, default);
+
+        // Assert
+        Assert.False(result.IsSuccess);
+        Assert.IsType<Result<Unit>>(result);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnFailureResult_IfModelWithUpdatedNameIsNotNull()
+    {
+        // Arrange
+        var command = new UpdateModelCommand(updateModelDto);
+
+        modelRepositoryMock.SetupSequence(x =>
+                x.GetSingleEntityBySpecificationAsync(It.IsAny<ModelByNameQuerySpecification>()))
+            .ReturnsAsync(new VehicleModel(VehicleBrand.Id, updateModelDto.CurrentName))
+            .ReturnsAsync(new VehicleModel(VehicleBrand.Id, updateModelDto.UpdatedName));
+
+        var handler = new UpdateModelCommandHandler(modelRepositoryMock.Object, unitOfWorkMock.Object);
+
+        // Act
+        var result = await handler.Handle(command, default);
+
+        // Assert
+        Assert.False(result.IsSuccess);
+        Assert.IsType<Result<Unit>>(result);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnSuccessResult_IfModelIsNotNullAndModelWithUpdatedNameDoesNotExists()
+    {
+        // Arrange
+        var command = new UpdateModelCommand(updateModelDto);
+
+        modelRepositoryMock.SetupSequence(x =>
+                x.GetSingleEntityBySpecificationAsync(It.IsAny<ModelByNameQuerySpecification>()))
+            .ReturnsAsync(new VehicleModel(VehicleBrand.Id, updateModelDto.CurrentName))
+            .ReturnsAsync((VehicleModel)null!);
+
+        unitOfWorkMock.Setup(x => x.SaveChangesAsync(default)).ReturnsAsync(1);
+
+        var handler = new UpdateModelCommandHandler(modelRepositoryMock.Object, unitOfWorkMock.Object);
+
+        // Act
+        var result = await handler.Handle(command, default);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.IsType<Result<Unit>>(result);
+    }
+}
