ab18f0d1c6846b32b8f7b3aab9847dacb4b0ffa3
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-12 15:48:37 +0200

    Added ListDisplacementsQueryHandlerTests.cs and ListDisplacementsQueryTests.cs

---
+++ UnitTests/Application/Queries/DisplacementRelated/ListDisplacementsQueryHandlerTests.cs
@@ -0,0 +1,44 @@
+﻿using Application.Dtos.DisplacementDtos;
+using Application.Queries.Displacements;
+using Application.QuerySpecifications.DisplacementRelated;
+using AutoMapper;
+using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using Moq;
+
+namespace UnitTests.Application.Queries.DisplacementRelated;
+
+public class ListDisplacementsQueryHandlerTests
+{
+    private readonly Mock<IRepository<VehicleDisplacement>> _repository = new();
+    private readonly Mock<IMapper> _mapper = new();
+    
+    [Fact]
+    public async void Handle_ReturnsHandledQueryResult()
+    {
+        // Arrange
+        var displacements = new List<VehicleDisplacement>
+        {
+            new(1d), new(2d)
+        };
+        
+        _repository.Setup(r =>
+                r.GetAllEntitiesAsync(It.IsAny<DisplacementQuerySpecification>()))
+            .ReturnsAsync(displacements);
+        
+        _mapper.Setup(m => m.Map<IReadOnlyList<VehicleDisplacement>, IReadOnlyList<DisplacementDto>>(
+                It.IsAny<IReadOnlyList<VehicleDisplacement>>()))
+            .Returns((IReadOnlyList<VehicleDisplacement> source) =>
+                source.Select(x => new DisplacementDto
+                {
+                    Value = x.Value
+                }).ToList());
+        
+        var handler = new ListDisplacementsQueryHandler(_repository.Object, _mapper.Object);
+        var query = new ListDisplacementsQuery();
+        var result = await handler.Handle(query, default);
+        
+        // Assert
+        Assert.NotNull(result);
+    }
+}
---
+++ UnitTests/Application/Queries/DisplacementRelated/ListDisplacementsQueryTests.cs
@@ -0,0 +1,16 @@
+﻿using Application.Queries.Displacements;
+
+namespace UnitTests.Application.Queries.DisplacementRelated;
+
+public class ListDisplacementsQueryTests
+{
+    [Fact]
+    public void Constructor_InstantiatesNewQuery()
+    {
+        // Arrange & Act
+        var result = new ListDisplacementsQuery();
+        
+        // Assert
+        Assert.NotNull(result);
+    }
+}
