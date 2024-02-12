be5a7d1aa8c6ee3813f4b56bdd65b6f52ad9ebad
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-12 02:28:15 +0200

    Type commands covered with tests

---
+++ UnitTests/Application/Commands/Types/DeleteTypeCommandTest.cs
@@ -0,0 +1,60 @@
+﻿using Application.Commands.Types.DeleteType;
+using Application.Core;
+using Application.QuerySpecifications.TypeRelated;
+using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using MediatR;
+using Moq;
+
+namespace UnitTests.Application.Commands.Types;
+
+public class DeleteTypeCommandTest
+{
+    private readonly Mock<IRepository<VehicleType>> brandRepositoryMock = new();
+    private readonly Mock<IUnitOfWork> unitOfWorkMock = new();
+
+    private const string TypeName = "testType";
+
+    [Fact]
+    public async Task Handle_Should_ReturnFailureResult_IfTypeIsNull()
+    {
+        // Arrange
+        var command = new DeleteTypeCommand(TypeName);
+
+        brandRepositoryMock.Setup(x =>
+                x.GetSingleEntityBySpecificationAsync(It.IsAny<TypeByNameQuerySpecification>()))
+            .ReturnsAsync((VehicleType)null!);
+
+        var handler = new DeleteTypeCommandHandler(brandRepositoryMock.Object, unitOfWorkMock.Object);
+
+        // Act
+        var result = await handler.Handle(command, default);
+
+        // Assert
+        Assert.False(result.IsSuccess);
+        Assert.IsType<Result<Unit>>(result);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnSuccessResult_IfTypeIsNotNull()
+    {
+        // Arrange
+        var command = new DeleteTypeCommand(TypeName);
+
+        brandRepositoryMock.Setup(x =>
+                x.GetSingleEntityBySpecificationAsync(It.IsAny<TypeByNameQuerySpecification>()))
+            .ReturnsAsync(new VehicleType(TypeName));
+
+        unitOfWorkMock.Setup(x => x.SaveChangesAsync(default))
+            .ReturnsAsync(1);
+
+        var handler = new DeleteTypeCommandHandler(brandRepositoryMock.Object, unitOfWorkMock.Object);
+
+        // Act
+        var result = await handler.Handle(command, default);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.IsType<Result<Unit>>(result);
+    }
+}
---
+++ UnitTests/Application/Commands/Types/UpdateTypeCommandTest.cs
@@ -0,0 +1,86 @@
+﻿using Application.Commands.Types.UpdateType;
+using Application.Core;
+using Application.Dtos.TypeDtos;
+using Application.QuerySpecifications.TypeRelated;
+using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using MediatR;
+using Moq;
+
+namespace UnitTests.Application.Commands.Types;
+
+public class UpdateTypeCommandTest
+{
+    private readonly Mock<IRepository<VehicleType>> brandRepositoryMock = new();
+    private readonly Mock<IUnitOfWork> unitOfWorkMock = new();
+
+    private readonly UpdateTypeDto updateTypeDto = new()
+    {
+        CurrentName = "testCurrentName",
+        UpdatedName = "testUpdatedName"
+    };
+
+    [Fact]
+    public async Task Handle_Should_ReturnFailureResult_IfTypeWithCurrentNameIsNull()
+    {
+        // Arrange
+        var command = new UpdateTypeCommand(updateTypeDto);
+
+        brandRepositoryMock.Setup(x =>
+                x.GetSingleEntityBySpecificationAsync(It.IsAny<TypeByNameQuerySpecification>()))
+            .ReturnsAsync((VehicleType)null!);
+
+        var handler = new UpdateTypeCommandHandler(brandRepositoryMock.Object, unitOfWorkMock.Object);
+
+        // Act
+        var result = await handler.Handle(command, default);
+
+        // Assert
+        Assert.False(result.IsSuccess);
+        Assert.IsType<Result<Unit>>(result);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnFailureResult_IfTypeWithUpdatedNameIsNotNull()
+    {
+        // Arrange
+        var command = new UpdateTypeCommand(updateTypeDto);
+
+        brandRepositoryMock.SetupSequence(x =>
+                x.GetSingleEntityBySpecificationAsync(It.IsAny<TypeByNameQuerySpecification>()))
+            .ReturnsAsync(new VehicleType(updateTypeDto.CurrentName))
+            .ReturnsAsync(new VehicleType(updateTypeDto.UpdatedName));
+
+        var handler = new UpdateTypeCommandHandler(brandRepositoryMock.Object, unitOfWorkMock.Object);
+
+        // Act
+        var result = await handler.Handle(command, default);
+
+        // Assert
+        Assert.False(result.IsSuccess);
+        Assert.IsType<Result<Unit>>(result);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnSuccessResult_IfTypeIsNotNullAndTypeWithUpdatedNameDoesNotExists()
+    {
+        // Arrange
+        var command = new UpdateTypeCommand(updateTypeDto);
+
+        brandRepositoryMock.SetupSequence(x =>
+                x.GetSingleEntityBySpecificationAsync(It.IsAny<TypeByNameQuerySpecification>()))
+            .ReturnsAsync(new VehicleType(updateTypeDto.CurrentName))
+            .ReturnsAsync((VehicleType)null!);
+        
+        unitOfWorkMock.Setup(x => x.SaveChangesAsync(default)).ReturnsAsync(1);
+        
+        var handler = new UpdateTypeCommandHandler(brandRepositoryMock.Object, unitOfWorkMock.Object);
+
+        // Act
+        var result = await handler.Handle(command, default);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.IsType<Result<Unit>>(result);
+    }
+}
