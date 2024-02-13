7c328a05a053548e2ebe1e741396b7b428e44ab6
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-13 20:13:58 +0200

    Refactored domain entities and related classes

--- LanosCertifiedStore.DomainLayer.Domain/Entities/VehicleRelated/Classes/Vehicle.cs
+++ LanosCertifiedStore.DomainLayer.Domain/Entities/VehicleRelated/Classes/Vehicle.cs
@@ -1,39 +1,32 @@
-﻿using Domain.Contracts.Common;
+﻿namespace Domain.Entities.VehicleRelated.Classes;
 
-namespace Domain.Entities.VehicleRelated.Classes;
-
-public sealed class Vehicle : IEntity<Guid>
+public sealed class Vehicle
 {
     public Guid Id { get; init; } = Guid.NewGuid();
-    public string Description { get; set; } = null!;
-    public Guid BrandId { get; set; }
-    public VehicleBrand Brand { get; set; } = null!;
-    public Guid ModelId { get; set; }
-    public VehicleModel Model { get; set; } = null!;
-    public Guid ColorId { get; set; }
-    public VehicleColor Color { get; set; } = null!;
-    public Guid TypeId { get; set; }
-    public VehicleType Type { get; set; } = null!;
-    public Guid DisplacementId { get; set; }
-    public VehicleDisplacement Displacement { get; set; } = null!;
-    public ICollection<VehiclePrice> Prices { get; set; } = new List<VehiclePrice>();
+    public string Description { get; init; } = null!;
+    public VehicleBrand Brand { get; init; } = null!;
+    public VehicleModel Model { get; init; } = null!;
+    public VehicleColor Color { get; init; } = null!;
+    public VehicleType Type { get; init; } = null!;
+    public VehicleDisplacement Displacement { get; init; } = null!;
+    public ICollection<VehiclePrice> Prices { get; init; } = new List<VehiclePrice>();
 
     public Vehicle() { }
     public Vehicle(
-        Guid brandId,
-        Guid modelId,
-        Guid typeId,
-        Guid colorId,
-        Guid displacementId,
+        VehicleBrand brand,
+        VehicleModel model,
+        VehicleColor color,
+        VehicleType type,
+        VehicleDisplacement displacement,
         decimal price,
         string description)
     {
-        BrandId = brandId;
-        ModelId = modelId;
-        TypeId = typeId;
-        ColorId = colorId;
-        DisplacementId = displacementId;
-        Prices.Add(new VehiclePrice(Id, price));
+        Prices.Add(new VehiclePrice(price));
+        Brand = brand;
+        Model = model;
+        Color = color;
+        Type = type;
+        Displacement = displacement;
         Description = description;
     }
 }
--- LanosCertifiedStore.DomainLayer.Domain/Entities/VehicleRelated/Classes/VehiclePrice.cs
+++ LanosCertifiedStore.DomainLayer.Domain/Entities/VehicleRelated/Classes/VehiclePrice.cs
@@ -1,19 +1,14 @@
-﻿using Domain.Contracts.Common;
+﻿namespace Domain.Entities.VehicleRelated.Classes;
 
-namespace Domain.Entities.VehicleRelated.Classes;
-
-public sealed class VehiclePrice : IEntity<Guid>
+public sealed class VehiclePrice
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid Id { get; init; } = Guid.NewGuid();
     public decimal Value { get; set; }
     public DateTime IssueDate { get; set; } = DateTime.UtcNow;
-    public Guid VehicleId { get; set; }
-    public Vehicle Vehicle { get; set; } = null!;
 
     public VehiclePrice() { }
-    public VehiclePrice(Guid vehicleId, decimal value)
+    public VehiclePrice(decimal value)
     {
-        VehicleId = vehicleId;
         Value = value;
     }
 }
