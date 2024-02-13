96fe4baef23b873162e948dee0961f628eca6567
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-13 22:08:33 +0200

    Created templates of all repositories for their further implementation

--- LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/VehicleColorRepository.cs
+++ LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/VehicleColorRepository.cs
@@ -1,8 +1,41 @@
-﻿using Domain.Entities.VehicleRelated.Classes;
-using Persistence.Contexts;
+﻿using AutoMapper;
+using Domain.Entities.VehicleRelated.Classes;
+using Microsoft.EntityFrameworkCore;
+using Persistence.DataModels;
 using Persistence.Repositories.Common.Classes;
 
 namespace Persistence.Repositories;
 
-internal class VehicleColorRepository(ApplicationDatabaseContext dbContext)
-    : GenericRepository<VehicleColor>(dbContext);
+internal class VehicleColorRepository(IMapper mapper, DbContext dbContext)
+    : GenericRepository<VehicleColor, VehicleColorDataModel>(mapper, dbContext)
+{
+    public override async Task<List<VehicleColor>> GetAllEntitiesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override async Task<VehicleColor?> GetEntityByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override async Task AddNewEntityAsync(VehicleColor entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void UpdateExistingEntity(VehicleColor updatedEntity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void RemoveExistingEntity(VehicleColor removedEntity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override async Task<int> CountAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
--- LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/VehicleRepository.cs
+++ LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/VehicleRepository.cs
@@ -1,8 +1,41 @@
-﻿using Domain.Entities.VehicleRelated.Classes;
-using Persistence.Contexts;
+﻿using AutoMapper;
+using Domain.Entities.VehicleRelated.Classes;
+using Microsoft.EntityFrameworkCore;
+using Persistence.DataModels;
 using Persistence.Repositories.Common.Classes;
 
 namespace Persistence.Repositories;
 
-internal class VehicleRepository(ApplicationDatabaseContext dbContext)
-    : GenericRepository<Vehicle>(dbContext);
+internal class VehicleRepository(IMapper mapper, DbContext dbContext)
+    : GenericRepository<Vehicle, VehicleDataModel>(mapper, dbContext)
+{
+    public override async Task<List<Vehicle>> GetAllEntitiesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override async Task<Vehicle?> GetEntityByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override async Task AddNewEntityAsync(Vehicle entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void UpdateExistingEntity(Vehicle updatedEntity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void RemoveExistingEntity(Vehicle removedEntity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override async Task<int> CountAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
