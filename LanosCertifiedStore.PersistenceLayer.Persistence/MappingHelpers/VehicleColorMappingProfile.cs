adfdc7e26f33128377464135360e9f871b50b9e9
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-14 01:26:41 +0200

    VehicleColorRepostitory implemented

---
+++ LanosCertifiedStore.PersistenceLayer.Persistence/MappingHelpers/VehicleColorMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Domain.Entities.VehicleRelated.Classes;
+using Persistence.DataModels;
+
+namespace Persistence.MappingHelpers;
+
+internal sealed class VehicleColorMappingProfile : Profile
+{
+    public VehicleColorMappingProfile()
+    {
+        CreateMap<VehicleColorDataModel, VehicleColor>();
+        CreateMap<VehicleColor, VehicleColorDataModel>();
+    }
+}
--- LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/VehicleColorRepository.cs
+++ LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/VehicleColorRepository.cs
@@ -1,41 +1,56 @@
 ﻿using AutoMapper;
 using Domain.Entities.VehicleRelated.Classes;
 using Microsoft.EntityFrameworkCore;
+using Persistence.Contexts;
 using Persistence.DataModels;
 using Persistence.Repositories.Common.Classes;
 
 namespace Persistence.Repositories;
 
-internal class VehicleColorRepository(IMapper mapper, DbContext dbContext)
+internal class VehicleColorRepository(IMapper mapper, ApplicationDatabaseContext dbContext)
     : GenericRepository<VehicleColor, VehicleColorDataModel>(mapper, dbContext)
 {
-    public override async Task<List<VehicleColor>> GetAllEntitiesAsync()
+    public override async Task<IReadOnlyList<VehicleColor>> GetAllEntitiesAsync()
     {
-        throw new NotImplementedException();
+        var colorModels = await dbContext.VehiclesColors
+            .AsNoTracking()
+            .ToListAsync();
+
+        return mapper.Map<IReadOnlyList<VehicleColorDataModel>, IReadOnlyList<VehicleColor>>(colorModels);
     }
 
     public override async Task<VehicleColor?> GetEntityByIdAsync(Guid id)
     {
-        throw new NotImplementedException();
+        var colorModel = await dbContext.VehiclesColors
+            .AsNoTracking()
+            .SingleOrDefaultAsync(x => x.Id == id);
+
+        return colorModel is null ? null : mapper.Map<VehicleColorDataModel, VehicleColor>(colorModel);
     }
 
     public override async Task AddNewEntityAsync(VehicleColor entity)
     {
-        throw new NotImplementedException();
+        var colorModel = mapper.Map<VehicleColor, VehicleColorDataModel>(entity);
+
+        await dbContext.VehiclesColors.AddAsync(colorModel);
     }
 
     public override void UpdateExistingEntity(VehicleColor updatedEntity)
     {
-        throw new NotImplementedException();
+        var colorModel = mapper.Map<VehicleColor, VehicleColorDataModel>(updatedEntity);
+
+        dbContext.VehiclesColors.Update(colorModel);
     }
 
-    public override void RemoveExistingEntity(VehicleColor removedEntity)
+    public override void RemoveExistingEntity(VehicleColor removedEntity) // TODO same here: maybe id????
     {
-        throw new NotImplementedException();
+        var colorModel = mapper.Map<VehicleColor, VehicleColorDataModel>(removedEntity);
+
+        dbContext.VehiclesColors.Remove(colorModel);
     }
 
     public override async Task<int> CountAsync()
     {
-        throw new NotImplementedException();
+        return await dbContext.VehiclesColors.CountAsync();
     }
 }
