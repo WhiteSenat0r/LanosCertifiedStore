7f606bba2da015aa10b729caa5392bd884da5481
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-14 15:42:11 +0200

    vehiclerepository implemented

---
+++ LanosCertifiedStore.PersistenceLayer.Persistence/MappingHelpers/VehicleMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Domain.Entities.VehicleRelated.Classes;
+using Persistence.DataModels;
+
+namespace Persistence.MappingHelpers;
+
+internal sealed class VehicleMappingProfile : Profile
+{
+    public VehicleMappingProfile()
+    {
+        CreateMap<Vehicle, VehicleDataModel>();
+        CreateMap<VehicleDataModel, Vehicle>();
+    }
+}
