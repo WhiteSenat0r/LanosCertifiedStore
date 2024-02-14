e0137fc31de0fb472444628d264e1525cac4e621
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-14 20:08:59 +0200

    unitofwork temporary implementation

--- LanosCertifiedStore.PersistenceLayer.Persistence/UnitOfWork.cs
+++ LanosCertifiedStore.PersistenceLayer.Persistence/UnitOfWork.cs
@@ -1,12 +1,18 @@
-﻿using Domain.Contracts.RepositoryRelated;
+using Domain.Contracts.Common;
+using Domain.Contracts.RepositoryRelated;
 using Persistence.Contexts;
 
 namespace Persistence;
 
 internal sealed class UnitOfWork(ApplicationDatabaseContext context) : IUnitOfWork 
 {
+    public IRepository<TEntity> Repository<TEntity>() where TEntity : IEntity<Guid>
+    {
+        throw new NotImplementedException();
+    }
+
     public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
         return await context.SaveChangesAsync(cancellationToken);
     }
-}
+} 
