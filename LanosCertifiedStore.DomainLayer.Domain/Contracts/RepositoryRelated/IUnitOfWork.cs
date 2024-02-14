a8ff7c14b2d74c0ffcef21c10389dc2279d1de4b
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-14 22:37:12 +0200

    Changed Repository method's name to RetrieveRepository in IUnitOfWork.cs

--- LanosCertifiedStore.DomainLayer.Domain/Contracts/RepositoryRelated/IUnitOfWork.cs
+++ LanosCertifiedStore.DomainLayer.Domain/Contracts/RepositoryRelated/IUnitOfWork.cs
@@ -4,6 +4,6 @@ namespace Domain.Contracts.RepositoryRelated;
 
 public interface IUnitOfWork
 {
-    IRepository<TEntity> Repository<TEntity>() where TEntity : IEntity<Guid>;
+    IRepository<TEntity> RetrieveRepository<TEntity>() where TEntity : IEntity<Guid>;
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
