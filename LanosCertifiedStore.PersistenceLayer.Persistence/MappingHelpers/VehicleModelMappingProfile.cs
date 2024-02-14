15bf7ccf5518e65bec7bf79dee001475c76c41b6
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-14 15:07:07 +0200

    vehicle model repository or something

---
+++ LanosCertifiedStore.PersistenceLayer.Persistence/MappingHelpers/VehicleModelMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Domain.Entities.VehicleRelated.Classes;
+using Persistence.DataModels;
+
+namespace Persistence.MappingHelpers;
+
+internal sealed class VehicleModelMappingProfile : Profile
+{
+    public VehicleModelMappingProfile()
+    {
+        CreateMap<VehicleModelDataModel, VehicleModel>();
+        CreateMap<VehicleModel, VehicleModelDataModel>();
+    }
+}
--- LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/VehicleModelRepository.cs
+++ LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/VehicleModelRepository.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Domain.Contracts.RepositoryRelated;
 using Domain.Entities.VehicleRelated.Classes;
 using Microsoft.EntityFrameworkCore;
 using Persistence.DataModels;
@@ -11,31 +12,56 @@ internal class VehicleModelRepository(IMapper mapper, DbContext dbContext)
 {
     public override async Task<IReadOnlyList<VehicleModel>> GetAllEntitiesAsync()
     {
+        var vehicleModelDataModels = await Context.Set<VehicleModelDataModel>()
+            .Include(x => x.VehicleBrandDataModel)
+            .AsNoTracking()
+            .ToListAsync();
+
+        return mapper.Map<IReadOnlyList<VehicleModelDataModel>, IReadOnlyList<VehicleModel>>(vehicleModelDataModels);
+    }
+
+    public override Task<IReadOnlyList<VehicleModel>> GetAllEntitiesAsync(
+        IFilteringRequestParameters<VehicleModel> filteringRequestParameters)
+    {
         throw new NotImplementedException();
     }
 
     public override async Task<VehicleModel?> GetEntityByIdAsync(Guid id)
     {
-        throw new NotImplementedException();
+        var vehicleModelDataModel = await Context.Set<VehicleModelDataModel>()
+            .Include(x => x.VehicleBrandDataModel)
+            .AsNoTracking()
+            .SingleOrDefaultAsync(x => x.Id.Equals(id));
+
+        return vehicleModelDataModel is null
+            ? null
+            : mapper.Map<VehicleModelDataModel, VehicleModel>(vehicleModelDataModel);
     }
 
     public override async Task AddNewEntityAsync(VehicleModel entity)
     {
-        throw new NotImplementedException();
+        var vehicleModelDataModel = mapper.Map<VehicleModel, VehicleModelDataModel>(entity);
+
+        await Context.Set<VehicleModelDataModel>().AddAsync(vehicleModelDataModel);
     }
 
     public override void UpdateExistingEntity(VehicleModel updatedEntity)
     {
-        throw new NotImplementedException();
+        var vehicleModelDataModel = mapper.Map<VehicleModel, VehicleModelDataModel>(updatedEntity);
+
+        Context.Set<VehicleModelDataModel>().Update(vehicleModelDataModel);
     }
 
     public override void RemoveExistingEntity(VehicleModel removedEntity)
     {
-        throw new NotImplementedException();
+        var vehicleModelDataModel = mapper.Map<VehicleModel, VehicleModelDataModel>(removedEntity);
+
+        Context.Set<VehicleModelDataModel>().Remove(vehicleModelDataModel);
+
     }
 
     public override async Task<int> CountAsync()
     {
-        throw new NotImplementedException();
+        return await Context.Set<VehicleModelDataModel>().CountAsync();
     }
 }
