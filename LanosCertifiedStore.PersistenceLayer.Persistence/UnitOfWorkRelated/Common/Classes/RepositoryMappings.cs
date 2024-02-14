3ba54a697cf0ead87c960eb11c19aac339a6ba64
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-14 22:36:17 +0200

    Added abstract class with repository mappings

---
+++ LanosCertifiedStore.PersistenceLayer.Persistence/UnitOfWorkRelated/Common/Classes/RepositoryMappings.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using Persistence.Repositories;
+
+namespace Persistence.UnitOfWorkRelated.Common.Classes;
+
+internal abstract class RepositoryMappings
+{
+    public static readonly Dictionary<Type, Type> VehicleRelatedRepositoryMappings = new()
+    {
+        { typeof(IRepository<Vehicle>), typeof(VehicleRepository) },
+        { typeof(IRepository<VehicleBrand>), typeof(VehicleBrandRepository) },
+        { typeof(IRepository<VehicleModel>), typeof(VehicleModelRepository) },
+        { typeof(IRepository<VehicleType>), typeof(VehicleTypeRepository) },
+        { typeof(IRepository<VehicleColor>), typeof(VehicleColorRepository) },
+        { typeof(IRepository<VehicleDisplacement>), typeof(VehicleDisplacementRepository) },
+        { typeof(IRepository<VehiclePrice>), typeof(VehiclePriceRepository) }
+    };
+}
