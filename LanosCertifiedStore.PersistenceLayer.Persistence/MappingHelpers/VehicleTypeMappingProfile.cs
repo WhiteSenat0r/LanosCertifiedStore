e2a5e64390b4b40befa200058154a2938fb42dbf
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-14 11:52:12 +0200

    Added vehicle type-related mapping profiles

---
+++ LanosCertifiedStore.PersistenceLayer.Persistence/MappingHelpers/VehicleTypeMappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Domain.Entities.VehicleRelated.Classes;
+using Persistence.DataModels;
+
+namespace Persistence.MappingHelpers;
+
+internal sealed class VehicleTypeMappingProfile : Profile
+{
+    public VehicleTypeMappingProfile()
+    {
+        AddMappingProfileFromEntityToModel();
+        AddMappingProfileFromModelToEntity();
+    }
+
+    private void AddMappingProfileFromEntityToModel()
+    {
+        CreateMap<VehicleType, VehicleTypeDataModel>()
+            .ForMember(model => model.Id, entity => entity.MapFrom(type => type.Id))
+            .ForMember(model => model.Name, entity => entity.MapFrom(type => type.Name));
+    }
+    
+    private void AddMappingProfileFromModelToEntity()
+    {
+        CreateMap<VehicleBrandDataModel, VehicleBrand>()
+            .ForMember(entity => entity.Id, model => model.MapFrom(type => type.Id))
+            .ForMember(entity => entity.Name, model => model.MapFrom(type => type.Name));
+    }
+}
