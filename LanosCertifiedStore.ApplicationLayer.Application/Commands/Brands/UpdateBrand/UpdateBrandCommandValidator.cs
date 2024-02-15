06a0f02c4ec66a9e89db20f04e62d225c7d79345
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-15 21:44:11 +0200

    Create UpdateBrandCommandValidator.cs

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Commands/Brands/UpdateBrand/UpdateBrandCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using FluentValidation;
+
+namespace Application.Commands.Brands.UpdateBrand;
+
+internal sealed class UpdateBrandCommandValidator : AbstractValidator<UpdateBrandCommand>
+{
+    public UpdateBrandCommandValidator(IUnitOfWork unitOfWork)
+    {
+        RuleFor(x => x.UpdateBrandDto.UpdatedName).NotEmpty().MinimumLength(2).MaximumLength(64);
+        RuleFor(x => x.UpdateBrandDto.UpdatedName).MustAsync(async (updatedName, _) =>
+        {
+            var brands = await unitOfWork.RetrieveRepository<VehicleBrand>().GetAllEntitiesAsync();
+            return brands.All(x => x.Name != updatedName);
+        }).WithMessage("Brand name must be unique!");
+    }
+}
