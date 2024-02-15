7b6fd8dabbc0021a13c08ff6e1466e44bf39b2cb
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-15 13:19:48 +0200

    Added draft template of the vehicle price repository

--- LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/VehiclePriceRepository.cs
+++ LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/VehiclePriceRepository.cs
@@ -14,12 +14,21 @@ internal class VehiclePriceRepository(IMapper mapper, ApplicationDatabaseContext
     public override async Task<IReadOnlyList<VehiclePrice>> GetAllEntitiesAsync(
         IFilteringRequestParameters<VehiclePrice> filteringRequestParameters = null!)
     {
-        throw new NotImplementedException();
+        var priceModels = await Context.Set<VehiclePriceDataModel>()
+            .Include(p => p.Vehicle)
+            .AsNoTracking()
+            .ToListAsync();
+        
+        return Mapper.Map<IReadOnlyList<VehiclePriceDataModel>, IReadOnlyList<VehiclePrice>>(priceModels);
     }
 
     public override async Task<VehiclePrice?> GetEntityByIdAsync(Guid id)
     {
-        throw new NotImplementedException();
+        var priceModel = await Context.Set<VehiclePriceDataModel>()
+            .AsNoTracking()
+            .SingleOrDefaultAsync(x => x.Id == id);
+
+        return priceModel is null ? null : Mapper.Map<VehiclePriceDataModel, VehiclePrice>(priceModel);
     }
 
     private protected override async Task<IQueryable<VehiclePriceDataModel>> HandleQueryFiltering(
