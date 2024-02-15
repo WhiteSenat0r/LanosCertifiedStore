fd895d986f1a71bc447c5213a7904e77b60cdfe2
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-15 20:03:46 +0200

    CreateBrandCommandValidator added

--- LanosCertifiedStore.PresentationLayer.API/Controllers/Common/BaseEntityRelatedApiController.cs
+++ LanosCertifiedStore.PresentationLayer.API/Controllers/Common/BaseEntityRelatedApiController.cs
@@ -1,5 +1,6 @@
 ﻿using API.Responses;
 using Application.Core;
+using Domain.Contracts.ValidationRelated;
 using Domain.Shared;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +13,14 @@ public abstract class BaseEntityRelatedApiController : BaseApiController
         if (result is null)
             return NotFound(new ApiResponse(NotFound().StatusCode));
 
+        if (result is IValidationResult validationResult)
+            return BadRequest(CreateProblemDetails(
+                "Validation Error",
+                BadRequest().StatusCode,
+                result.Error!,
+                validationResult.Errors
+            ));
+
         return result.IsSuccess switch
         {
             true when result.Value is not null => Ok(result.Value),
@@ -19,4 +28,14 @@ public abstract class BaseEntityRelatedApiController : BaseApiController
             _ => BadRequest(new ApiResponse(BadRequest().StatusCode, result.Error!.Message))
         };
     }
+
+    private static ProblemDetails CreateProblemDetails(
+        string title, int status, Error error, Error[]? errors = null) => new()
+    {
+        Title = title,
+        Status = status,
+        Type = error.Code,
+        Detail = error.Message,
+        Extensions = { { nameof(errors), errors } }
+    };
 }
