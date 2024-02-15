2f4ff3ad7c19f3ad3ff99b56f0ed46d62cde0a00
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-15 13:20:01 +0200

    Added vehicle price mapping profile

---
+++ LanosCertifiedStore.PersistenceLayer.Persistence/MappingHelpers/VehiclePriceMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Domain.Entities.VehicleRelated.Classes;
+using Persistence.DataModels;
+
+namespace Persistence.MappingHelpers;
+
+internal sealed class VehiclePriceMappingProfile : Profile
+{
+    public VehiclePriceMappingProfile()
+    {
+        AddMappingProfileFromEntityToModel();
+        AddMappingProfileFromModelToEntity();
+    }
+    
+    private void AddMappingProfileFromModelToEntity() =>
+        CreateMap<VehiclePriceDataModel, VehiclePrice>();
+    
+    private void AddMappingProfileFromEntityToModel() =>
+        CreateMap<VehiclePrice, VehiclePriceDataModel>();
+}
