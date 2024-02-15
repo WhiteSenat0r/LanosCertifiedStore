fb82898d7099a7aaa68bf57e13417be89ff848bb
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-15 21:34:01 +0200

    Added vehicle query evaluator-related mappings and settings

---
+++ LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/VehicleRelated/QueryEvaluationRelated/Common/Classes/VehicleFilteringCriteria.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq.Expressions;
+using Domain.Contracts.RepositoryRelated;
+using Domain.Contracts.RequestParametersRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using Persistence.DataModels;
+using Persistence.QueryEvaluation.Common;
+
+namespace Persistence.Repositories.VehicleRelated.QueryEvaluationRelated.Common.Classes;
+
+internal class VehicleFilteringCriteria : BaseFilteringCriteria<Vehicle, VehicleDataModel>
+{ 
+    internal override Expression<Func<VehicleDataModel, bool>> GetCriteria(
+        IFilteringRequestParameters<Vehicle>? filteringRequestParameters)
+    {
+        var vehicleFilteringParameters = filteringRequestParameters as IVehicleFilteringRequestParameters;
+
+        return vehicle =>
+            (string.IsNullOrEmpty(vehicleFilteringParameters!.Brand)
+             || vehicle.Brand.Name.Equals(vehicleFilteringParameters.Brand)) &&
+            (string.IsNullOrEmpty(vehicleFilteringParameters.Type)
+             || vehicle.Type.Name.Equals(vehicleFilteringParameters.Type)) &&
+            (string.IsNullOrEmpty(vehicleFilteringParameters.Color)
+             || vehicle.Color.Name.Equals(vehicleFilteringParameters.Color)) &&
+            (!vehicleFilteringParameters.Displacement.HasValue
+             || vehicle.Displacement.Value.Equals(vehicleFilteringParameters.Displacement)) &&
+            (!vehicleFilteringParameters.LowerPriceLimit.HasValue ||
+             (vehicle.Prices.OrderByDescending(p => p.IssueDate)
+               .FirstOrDefault()!.Value >= vehicleFilteringParameters.LowerPriceLimit.Value &&
+              vehicle.Prices.OrderByDescending(p => p.IssueDate)
+               .FirstOrDefault()!.IssueDate >= vehicleFilteringParameters.MinimalPriceDate)) &&
+            (!vehicleFilteringParameters.UpperPriceLimit.HasValue ||
+             (vehicle.Prices.OrderByDescending(p => p.IssueDate)
+               .FirstOrDefault()!.Value <= vehicleFilteringParameters.UpperPriceLimit.Value &&
+              vehicle.Prices.OrderByDescending(p => p.IssueDate)
+               .FirstOrDefault()!.IssueDate >= vehicleFilteringParameters.MinimalPriceDate));
+    }
+}
---
+++ LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/VehicleRelated/QueryEvaluationRelated/Common/Classes/VehicleSortingTypes.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+using Persistence.DataModels;
+
+namespace Persistence.Repositories.VehicleRelated.QueryEvaluationRelated.Common.Classes;
+
+internal abstract class VehicleSortingTypes
+{
+    public static readonly Dictionary<string, Expression<Func<VehicleDataModel, object>>> 
+        Options = new()
+    {
+        { "brand-desc", vehicle => vehicle.Brand.Name },
+        { "price-asc", vehicle => vehicle.Prices.OrderByDescending(p => p.IssueDate).FirstOrDefault()!.Value },
+        { "price-desc", vehicle => vehicle.Prices.OrderByDescending(p => p.IssueDate).FirstOrDefault()!.Value },
+        { "brand-asc", vehicle => vehicle.Brand.Name },
+        { "default", vehicle => vehicle.Brand.Name }
+    };
+}
