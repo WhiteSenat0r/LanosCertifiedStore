a04a47c918658a78120ff74a3bbc0670e9394c91
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-16 22:16:36 +0200

    Added VehiclePriceQueryEvaluator.cs and related helping classes

---
+++ LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/VehiclePriceRelated/QueryEvaluationRelated/Common/Classes/VehiclePriceIncludedAspects.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+using Persistence.DataModels;
+
+namespace Persistence.Repositories.VehiclePriceRelated.QueryEvaluationRelated.Common.Classes;
+
+internal abstract class VehiclePriceIncludedAspects
+{
+    public static readonly List<Expression<Func<VehiclePriceDataModel, object>>> IncludedAspects =
+    [
+        vehiclePrice => vehiclePrice.Vehicle,
+    ];
+}
---
+++ LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/VehiclePriceRelated/QueryEvaluationRelated/Common/Classes/VehiclePriceSortingSettings.cs
@@ -0,0 +1,6 @@
+﻿using Persistence.DataModels;
+using Persistence.QueryEvaluation.Common;
+
+namespace Persistence.Repositories.VehiclePriceRelated.QueryEvaluationRelated.Common.Classes;
+
+internal class VehiclePriceSortingSettings : BaseSortingSettings<VehiclePriceDataModel>;
