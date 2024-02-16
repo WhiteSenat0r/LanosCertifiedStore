36b3402268f350764b97b9e8d0367b7b3923c314
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-16 18:08:08 +0200

    Added VehicleTypeFilteringRequestParameters.cs and IVehicleTypeFilteringRequestParameters.cs

---
+++ LanosCertifiedStore.DomainLayer.Domain/Contracts/RequestParametersRelated/IVehicleTypeFilteringRequestParameters.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+
+namespace Domain.Contracts.RequestParametersRelated;
+
+public interface IVehicleTypeFilteringRequestParameters : IFilteringRequestParameters<VehicleType>
+{
+    string? Name { get; set; }
+}
