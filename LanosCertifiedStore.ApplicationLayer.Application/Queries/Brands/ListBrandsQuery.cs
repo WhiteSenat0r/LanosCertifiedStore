3efc05fddcf58f648e033bf428840cb7cb52073b
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-16 11:43:05 +0200

    Moved ListBrandsQuery to a new directory

--- LanosCertifiedStore.ApplicationLayer.Application/Queries/Brands/ListBrandsQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿using Application.Core;
-using Application.Dtos.BrandDtos;
-using Domain.Shared;
-using MediatR;
-
-namespace Application.Queries.Brands;
-
-public sealed record ListBrandsQuery : IRequest<Result<IReadOnlyList<BrandDto>>>;
