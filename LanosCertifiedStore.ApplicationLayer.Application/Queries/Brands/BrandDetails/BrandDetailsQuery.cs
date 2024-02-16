46419fe1c476efe8b54b4708b4074e622db5e5b6
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-16 11:43:21 +0200

    Added BrandDetailsQuery.cs and BrandDetailsQueryHandler.cs

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Queries/Brands/BrandDetails/BrandDetailsQuery.cs
@@ -0,0 +1,7 @@
+﻿using Application.Dtos.BrandDtos;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Queries.Brands.BrandDetails;
+
+public sealed record BrandDetailsQuery(Guid Id) : IRequest<Result<BrandDto>>;
