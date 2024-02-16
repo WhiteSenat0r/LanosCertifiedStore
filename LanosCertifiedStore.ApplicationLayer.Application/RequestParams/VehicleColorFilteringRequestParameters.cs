cdd6720e8fdb0a25859a2933e115b9669edff15e
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-16 17:28:02 +0200

    Added IVehicleColorFilteringRequestParameters.cs and VehicleColorFilteringRequestParameters.cs

---
+++ LanosCertifiedStore.ApplicationLayer.Application/RequestParams/VehicleColorFilteringRequestParameters.cs
@@ -0,0 +1,11 @@
+﻿using Application.RequestParams.Common.Classes;
+using Domain.Contracts.RequestParametersRelated;
+using Domain.Entities.VehicleRelated.Classes;
+
+namespace Application.RequestParams;
+
+public sealed class VehicleColorFilteringRequestParameters : BaseFilteringRequestParameters<VehicleColor>,
+    IVehicleColorFilteringRequestParameters
+{
+    public string? Name { get; set; }
+}
---
+++ LanosCertifiedStore.DomainLayer.Domain/Contracts/RequestParametersRelated/IVehicleColorFilteringRequestParameters.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+
+namespace Domain.Contracts.RequestParametersRelated;
+
+public interface IVehicleColorFilteringRequestParameters : IFilteringRequestParameters<VehicleColor>
+{
+    string? Name { get; set; }
+}
