75b0694158d383ffb576fc51e3578ecfedf4b627
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-16 22:12:38 +0200

    Added IVehiclePriceFilteringRequestParameters.cs and VehiclePriceFilteringRequestParameters.cs

---
+++ LanosCertifiedStore.DomainLayer.Domain/Contracts/RequestParametersRelated/IVehiclePriceFilteringRequestParameters.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+
+namespace Domain.Contracts.RequestParametersRelated;
+
+public interface IVehiclePriceFilteringRequestParameters : IFilteringRequestParameters<Vehicle>
+{
+    decimal? LowerPriceLimit { get; set; }
+    decimal? UpperPriceLimit { get; set; }
+}
