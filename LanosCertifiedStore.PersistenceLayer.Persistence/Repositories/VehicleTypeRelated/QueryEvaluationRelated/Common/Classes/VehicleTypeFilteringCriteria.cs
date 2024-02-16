6faf4a0004f72c58e20ffd3641c3d9c666a22ef8
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-16 18:09:55 +0200

    Added VehicleTypeQueryEvaluator.cs and related helping classes

---
+++ LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/VehicleTypeRelated/QueryEvaluationRelated/Common/Classes/VehicleTypeFilteringCriteria.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using Domain.Contracts.RepositoryRelated;
+using Domain.Contracts.RequestParametersRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using Persistence.DataModels;
+using Persistence.QueryEvaluation.Common;
+
+namespace Persistence.Repositories.VehicleTypeRelated.QueryEvaluationRelated.Common.Classes;
+
+internal class VehicleTypeFilteringCriteria : BaseFilteringCriteria<VehicleType, VehicleTypeDataModel>
+{ 
+    internal override Expression<Func<VehicleTypeDataModel, bool>> GetCriteria(
+        IFilteringRequestParameters<VehicleType>? filteringRequestParameters)
+    { 
+        var vehicleTypeFilteringParameters = filteringRequestParameters as IVehicleTypeFilteringRequestParameters;
+
+        return vehicleType =>
+            string.IsNullOrEmpty(vehicleTypeFilteringParameters!.Name)
+            || vehicleType.Name.Equals(vehicleTypeFilteringParameters.Name);
+    }
+}
---
+++ LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/VehicleTypeRelated/QueryEvaluationRelated/VehicleTypeQueryEvaluator.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using Microsoft.EntityFrameworkCore;
+using Persistence.DataModels;
+using Persistence.QueryEvaluation;
+using Persistence.Repositories.VehicleTypeRelated.QueryEvaluationRelated.Common.Classes;
+
+namespace Persistence.Repositories.VehicleTypeRelated.QueryEvaluationRelated;
+
+internal class VehicleTypeQueryEvaluator(
+    IEnumerable<Expression<Func<VehicleTypeDataModel, object>>> includedAspects,
+    IFilteringRequestParameters<VehicleType>? filteringRequestParameters,
+    DbSet<VehicleTypeDataModel> dataModels,
+    VehicleTypeFilteringCriteria typeFilteringCriteria)
+    : BaseQueryEvaluator<VehicleType, VehicleTypeDataModel>(
+        includedAspects,
+        filteringRequestParameters,
+        dataModels,
+        typeFilteringCriteria)
+{
+    private protected override VehicleTypeSortingSettings GetQuerySortingSettings()
+    {
+        if (string.IsNullOrEmpty(filteringRequestParameters!.SortingType))
+            return new VehicleTypeSortingSettings
+            {
+                OrderByAscendingExpression = VehicleTypeSortingTypes.Options["default"]
+            };
+
+        var settings = new VehicleTypeSortingSettings();
+
+        if (filteringRequestParameters.SortingType.Contains("-asc"))
+            settings.OrderByAscendingExpression = VehicleTypeSortingTypes.Options
+                [filteringRequestParameters.SortingType];
+        else if (filteringRequestParameters.SortingType.Contains("-desc"))
+            settings.OrderByDescendingExpression = VehicleTypeSortingTypes.Options
+                [filteringRequestParameters.SortingType];
+        
+        return settings;
+    }
+}
