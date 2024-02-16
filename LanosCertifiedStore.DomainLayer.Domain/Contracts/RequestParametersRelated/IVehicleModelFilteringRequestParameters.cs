4a593d0c5a5f31f7704d31c2453c1c5d2d7651ca
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-16 15:43:24 +0200

    Added IVehicleModelFilteringRequestParameters.cs and VehicleModelFilteringRequestParameters.cs

---
+++ LanosCertifiedStore.DomainLayer.Domain/Contracts/RequestParametersRelated/IVehicleModelFilteringRequestParameters.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+
+namespace Domain.Contracts.RequestParametersRelated;
+
+public interface IVehicleModelFilteringRequestParameters : IFilteringRequestParameters<VehicleModel>
+{
+    string? Name { get; set; }
+    string? ContainedBrandName { get; set; }
+}
