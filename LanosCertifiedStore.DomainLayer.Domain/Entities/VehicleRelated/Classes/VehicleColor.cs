1a03e51e33bc5a95919f00df10db20443e5b2736
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-17 19:57:20 +0200

    Entity, model, and model configuration configured

--- LanosCertifiedStore.DomainLayer.Domain/Entities/VehicleRelated/Classes/VehicleColor.cs
+++ LanosCertifiedStore.DomainLayer.Domain/Entities/VehicleRelated/Classes/VehicleColor.cs
@@ -4,8 +4,13 @@ namespace Domain.Entities.VehicleRelated.Classes;
 
 public sealed class VehicleColor : NamedVehicleAspect
 {
+    public string HexValue { get; set; } = null!;
     public ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
     
-    public VehicleColor() { }
-    public VehicleColor(string name) : base(name) {}
+    public VehicleColor() {}
+
+    public VehicleColor(string name, string hexValue) : base(name)
+    {
+        HexValue = hexValue;
+    }
 }
