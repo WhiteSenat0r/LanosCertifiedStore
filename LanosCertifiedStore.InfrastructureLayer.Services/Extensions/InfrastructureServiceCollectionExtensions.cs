770f38f31a2c8cb861ef28ba243ab91c57347751
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-17 22:46:14 +0200

    Added InfrastructureServiceCollectionExtensions.cs

---
+++ LanosCertifiedStore.InfrastructureLayer.Services/Extensions/InfrastructureServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Application.Contracts.ServicesRelated.ImageService;
+using LanosCertifiedStore.InfrastructureLayer.Services.ImagesRelated;
+using LanosCertifiedStore.InfrastructureLayer.Services.ImagesRelated.Common;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LanosCertifiedStore.InfrastructureLayer.Services.Extensions;
+
+public static class InfrastructureServiceCollectionExtensions
+{
+    public static IServiceCollection AddExternalServices(this IServiceCollection services, IConfiguration config)
+    {
+        AddCloudinaryConfiguration(services, config);
+
+        services.AddScoped<IImageService, ImageService>();
+        
+        return services;
+    }
+
+    private static void AddCloudinaryConfiguration(IServiceCollection services, IConfiguration config)
+    {
+        services.Configure<CloudinarySettings>(cloudinarySettings =>
+        {
+            cloudinarySettings.ApiKey = config.GetSection("Cloudinary")["ApiKey"]!;
+            cloudinarySettings.ApiSecret = config.GetSection("Cloudinary")["ApiSecret"]!;
+            cloudinarySettings.CloudName = config.GetSection("Cloudinary")["CloudName"]!;
+        });
+    }
+}
