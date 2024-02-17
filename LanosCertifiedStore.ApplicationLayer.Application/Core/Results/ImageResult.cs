2dd17c2717bd4379b918a41d8f7fec6184792015
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-17 22:52:11 +0200

    Added ImageResult.cs

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Core/Results/ImageResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Core.Results;
+
+public record ImageResult
+{
+    public string? ImageId { get; init; }
+    public string? ImageUrl { get; init; }
+    public bool IsUploadedSuccessfully { get; init; }
+}
