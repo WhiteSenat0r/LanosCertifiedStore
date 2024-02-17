ab425167b05aef76c6fd35a63d6f6203e5daf277
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-17 22:53:51 +0200

    Added VehicleImage entity

---
+++ LanosCertifiedStore.DomainLayer.Domain/Entities/VehicleRelated/Classes/VehicleImage.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Contracts.Common;
+
+namespace Domain.Entities.VehicleRelated.Classes;
+
+public sealed class VehicleImage : IEntity<Guid>
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public string CloudImageId { get; init; } = null!;
+    public string ImageUrl { get; init; } = null!;
+    public bool IsMainImage { get; init; }
+    public Vehicle Vehicle { get; init; } = null!;
+
+    public VehicleImage() { }
+    
+    public VehicleImage(string cloudImageId, string imageUrl, bool isMainImage)
+    {
+        CloudImageId = cloudImageId;
+        ImageUrl = imageUrl;
+        IsMainImage = isMainImage;
+    }
+}
