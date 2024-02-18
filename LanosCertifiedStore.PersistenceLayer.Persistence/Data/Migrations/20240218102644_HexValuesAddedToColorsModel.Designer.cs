da62dd120a740d112c1b078e8a531314669febe4
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-18 12:27:40 +0200

    Serhiy Kukharchuk

--- LanosCertifiedStore.PersistenceLayer.Persistence/Data/Migrations/20240218102644_HexValuesAddedToColorsModel.Designer.cs
+++ LanosCertifiedStore.PersistenceLayer.Persistence/Data/Migrations/20240218102644_HexValuesAddedToColorsModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence.Contexts.ApplicationDatabaseContext;
 
@@ -11,9 +12,11 @@ using Persistence.Contexts.ApplicationDatabaseContext;
 namespace Persistence.Data.Migrations
 {
     [DbContext(typeof(ApplicationDatabaseContext))]
-    partial class ApplicationDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240218102644_HexValuesAddedToColorsModel")]
+    partial class HexValuesAddedToColorsModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
