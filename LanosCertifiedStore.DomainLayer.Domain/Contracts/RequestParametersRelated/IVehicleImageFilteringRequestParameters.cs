e1e5e17470c7265a6b5528a8934388937d50cde9
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-18 11:20:14 +0200

    Added IVehicleImageFilteringRequestParameters.cs and VehicleImageFilteringRequestParameters.cs

---
+++ LanosCertifiedStore.DomainLayer.Domain/Contracts/RequestParametersRelated/IVehicleImageFilteringRequestParameters.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+
+namespace Domain.Contracts.RequestParametersRelated;
+
+public interface IVehicleImageFilteringRequestParameters : IFilteringRequestParameters<VehicleImage>
+{ 
+    Guid? RelatedVehicleId { get; set; }
+}
