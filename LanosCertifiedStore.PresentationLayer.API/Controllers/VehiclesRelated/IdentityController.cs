98d1c576c583f1e51da53d5a2efe4aac5be1ff50
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-21 01:29:58 +0200

    Create IdentityController.cs

---
+++ LanosCertifiedStore.PresentationLayer.API/Controllers/VehiclesRelated/IdentityController.cs
@@ -0,0 +1,23 @@
+﻿using API.Controllers.Common;
+using Application.Commands.Identity;
+using Application.Commands.Identity.Login;
+using Application.Commands.Identity.Register;
+using Application.Dtos.IdentityDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.VehiclesRelated;
+
+public sealed class IdentityController : BaseIdentityRelatedController 
+{
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login([FromBody] LoginDto loginDto)
+    {
+        return HandleResult(await Mediator.Send(new LoginCommand(loginDto)));
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<UserDto>> Register([FromBody] RegisterDto registerDto)
+    {
+        return HandleResult(await Mediator.Send(new RegisterCommand(registerDto)));
+    }
+}
