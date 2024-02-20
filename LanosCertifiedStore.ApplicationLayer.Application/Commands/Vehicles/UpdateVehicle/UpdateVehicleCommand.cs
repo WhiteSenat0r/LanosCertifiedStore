c780be4bf73674af45b53cfe00bf203575d19152
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-20 17:24:58 +0200

    Finished implementation of UpdateVehicleCommandHandler.cs and related classes

--- LanosCertifiedStore.ApplicationLayer.Application/Commands/Vehicles/UpdateVehicle/UpdateVehicleCommand.cs
+++ LanosCertifiedStore.ApplicationLayer.Application/Commands/Vehicles/UpdateVehicle/UpdateVehicleCommand.cs
@@ -1,7 +1,11 @@
-﻿using Application.Dtos.VehicleDtos;
-using Domain.Shared;
-using MediatR;
+﻿using Application.Commands.Vehicles.Common;
+using Application.Dtos.VehicleDtos;
+using Microsoft.AspNetCore.Http;
 
 namespace Application.Commands.Vehicles.UpdateVehicle;
 
-public record UpdateVehicleCommand(ActionVehicleDto ActionVehicleDto) : IRequest<Result<Unit>>;
+public record UpdateVehicleCommand(
+    UpdateVehicleDto UpdateVehicleDto,
+    ICollection<IFormFile>? Images,
+    string? MainImageName,
+    Guid? MainImageId) : IActionVehicleCommandBase;
--- LanosCertifiedStore.ApplicationLayer.Application/Commands/Vehicles/UpdateVehicle/UpdateVehicleCommandHandler.cs
+++ LanosCertifiedStore.ApplicationLayer.Application/Commands/Vehicles/UpdateVehicle/UpdateVehicleCommandHandler.cs
@@ -1,4 +1,7 @@
-﻿using AutoMapper;
+﻿using Application.Commands.Vehicles.Common;
+using Application.Contracts.ServicesRelated.ImageService;
+using Application.Dtos.VehicleDtos;
+using AutoMapper;
 using Domain.Contracts.RepositoryRelated;
 using Domain.Entities.VehicleRelated.Classes;
 using Domain.Shared;
@@ -6,98 +9,160 @@ using MediatR;
 
 namespace Application.Commands.Vehicles.UpdateVehicle;
 
-internal sealed class UpdateVehicleCommandHandler(
-    IMapper mapper,
-    IUnitOfWork unitOfWork)
-    : IRequestHandler<UpdateVehicleCommand, Result<Unit>>
+internal sealed class UpdateVehicleCommandHandler(IMapper mapper, IUnitOfWork unitOfWork,
+    IImageService imageService) : ActionVehicleCommandHandlerBase, IRequestHandler<UpdateVehicleCommand, Result<Unit>>
 {
+    private Vehicle? _updatedEntity;
+    
     public async Task<Result<Unit>> Handle(UpdateVehicleCommand request, CancellationToken cancellationToken)
     {
-        var vehicleToUpdate =
-            await unitOfWork.RetrieveRepository<Vehicle>().GetEntityByIdAsync(request.ActionVehicleDto.Id);
+        try
+        {
+            if (IsPresentMainImageFlagConflict(request))
+                return Result<Unit>.Failure(new Error(
+                    "UpdateError", "Failed to update a vehicle due to the main image flag conflict!"));
+            
+            _updatedEntity =
+                await unitOfWork.RetrieveRepository<Vehicle>().GetEntityByIdAsync(request.UpdateVehicleDto.Id);
 
-        if (vehicleToUpdate is null)
-            return Result<Unit>.Failure(Error.NotFound);
+            if (_updatedEntity is null)
+                return Result<Unit>.Failure(Error.NotFound);
 
-        var createRequestVehicleResult = await CreateVehicle(request);
+            var requestVehicleResult = 
+                await CreateVehicleBaseInstance<UpdateVehicleCommand, UpdateVehicleDto>(mapper, unitOfWork, request);
 
-        if (!createRequestVehicleResult.IsSuccess)
-            return Result<Unit>.Failure(createRequestVehicleResult.Error!);
+            if (!requestVehicleResult.IsSuccess)
+                return Result<Unit>.Failure(requestVehicleResult.Error!);
+        
+            await TrySetupNewVehicleData(request, requestVehicleResult);
 
+            var result = await GetVehicleUpdateResult(_updatedEntity!, cancellationToken);
 
-        ApplyChangesDataOnUpdatedVehicle(createRequestVehicleResult.Value!, vehicleToUpdate);
+            return result
+                ? Result<Unit>.Success(Unit.Value)
+                : throw new Exception("Failed to update a vehicle!");
+        }
+        catch (Exception e)
+        {
+            await imageService.TryRollbackImageUploadAsync();
+            return Result<Unit>.Failure(new Error("UpdateError", "Failed to update a vehicle!"));
+        }
+    }
 
-        if (IsAlteredPriceValue(request, vehicleToUpdate))
-            await InitializeNewPriceAsync(request, vehicleToUpdate);
+    private async Task TrySetupNewVehicleData(UpdateVehicleCommand request, Result<Vehicle> requestVehicleResult)
+    {
+        await TryRemoveImagesAndUpdateIsMainFlags(request, requestVehicleResult);
+        await TryUploadNewImages(request);
+        await TryUpdatePrice(request);
+        mapper.Map(requestVehicleResult.Value!, _updatedEntity!);
+    }
+    
+    private async Task TryRemoveImagesAndUpdateIsMainFlags(
+        UpdateVehicleCommand request, Result<Vehicle> requestVehicleResult)
+    {
+        var removedImages = GetRemovedImages(_updatedEntity!, requestVehicleResult);
 
+        await TryRemoveImagesFromVehicle(removedImages, _updatedEntity!);
+        TrySetUploadedImageAsMain(request, _updatedEntity!);
+    }
+
+    private async Task TryUploadNewImages(UpdateVehicleCommand request)
+    {
+        var uploadedImagesResult = await TryAddImagesToCloudinary(imageService, request, _updatedEntity!.Id);
+
+        if (uploadedImagesResult.IsSuccess)
+        {
+            foreach (var image in uploadedImagesResult.Value!) 
+                await unitOfWork.RetrieveRepository<VehicleImage>().AddNewEntityAsync(image);
+        }
+    }
+    
+    private async Task TryUpdatePrice(UpdateVehicleCommand request)
+    {
+        if (IsAlteredPriceValue(request, _updatedEntity!))
+            await InitializeNewPrice(request, _updatedEntity!);
+    }
+
+    private static bool IsPresentMainImageFlagConflict(UpdateVehicleCommand request)
+    {
+        return request.MainImageId.HasValue &&
+               (!string.IsNullOrEmpty(request.MainImageName) || !string.IsNullOrWhiteSpace(request.MainImageName));
+    }
+
+    private async Task<bool> GetVehicleUpdateResult(
+        Vehicle vehicleToUpdate, CancellationToken cancellationToken)
+    {
         unitOfWork.RetrieveRepository<Vehicle>().UpdateExistingEntity(vehicleToUpdate);
 
         var result = await unitOfWork.SaveChangesAsync(cancellationToken) > 0;
 
-        return result
-            ? Result<Unit>.Success(Unit.Value)
-            : Result<Unit>.Failure(new Error("UpdateError", "Failed to update a vehicle!"));
+        if (!result) await imageService.TryRollbackImageUploadAsync();
+        
+        return result;
     }
 
-    private async Task InitializeNewPriceAsync(UpdateVehicleCommand request, Vehicle updatedVehicle)
+    private void TrySetUploadedImageAsMain(UpdateVehicleCommand request, Vehicle vehicleToUpdate)
     {
-        var newPrice = new VehiclePrice(updatedVehicle, request.ActionVehicleDto.Price);
+        if (!request.MainImageId.HasValue) return;
+
+        foreach (var image in vehicleToUpdate.Images) image.IsMainImage = false;
+        
+        var mainImage = vehicleToUpdate.Images.Single(image => image.Id.Equals(request.MainImageId.Value));
+
+        mainImage.IsMainImage = true;
+
+        foreach (var image in vehicleToUpdate.Images) 
+            unitOfWork.RetrieveRepository<VehicleImage>().UpdateExistingEntity(image);
+        
+        unitOfWork.ClearChangeTrackerData();
+    }
+
+    private async Task TryRemoveImagesFromVehicle(
+        IEnumerable<VehicleImage> removedImages,
+        Vehicle vehicleToUpdate)
+    {
+        if (!removedImages.Any()) return;
+
+        var imageRepository = unitOfWork.RetrieveRepository<VehicleImage>();
+        
+        foreach (var removedImage in removedImages)
+        {
+            var updatedVehicleRemovedImage = vehicleToUpdate.Images.Single(
+                image => image.Id.Equals(removedImage.Id));
+
+            vehicleToUpdate.Images.Remove(updatedVehicleRemovedImage);
+            await imageRepository.RemoveExistingEntity(updatedVehicleRemovedImage.Id);
+            await imageService.TryDeletePhotoAsync(removedImage.CloudImageId);
+        }
+
+        await unitOfWork.SaveChangesAsync();
+        
+        unitOfWork.ClearChangeTrackerData();
+    }
+
+    private IEnumerable<VehicleImage> GetRemovedImages(
+        Vehicle vehicleToUpdate, Result<Vehicle> requestVehicleResult)
+    {
+        var removedImages = new List<VehicleImage>();
+        
+        foreach (var image in vehicleToUpdate.Images)
+        {
+            var requestVehicleImage = requestVehicleResult.Value!.Images.SingleOrDefault(
+                requestImage => requestImage.Id.Equals(image.Id));
+            
+            if (requestVehicleImage is null) removedImages.Add(image);
+        }
+
+        return removedImages;
+    }
+
+    private async Task InitializeNewPrice(UpdateVehicleCommand request, Vehicle updatedVehicle)
+    {
+        var newPrice = new VehiclePrice(updatedVehicle, request.UpdateVehicleDto.Price);
 
         await unitOfWork.RetrieveRepository<VehiclePrice>().AddNewEntityAsync(newPrice);
-
-        updatedVehicle.Prices.Add(newPrice);
-    }
-
-    private void ApplyChangesDataOnUpdatedVehicle(Vehicle vehicle, Vehicle updatedVehicle)
-    {
-        mapper.Map(vehicle, updatedVehicle);
     }
 
     private bool IsAlteredPriceValue(UpdateVehicleCommand request, Vehicle updatedVehicle) =>
-        !updatedVehicle.Prices.MaxBy(p => p.IssueDate)!.Value.Equals(request.ActionVehicleDto.Price);
-
-    private async Task<Result<Vehicle>> CreateVehicle(UpdateVehicleCommand request)
-    {
-        var vehicleModel =
-            await unitOfWork.RetrieveRepository<VehicleModel>().GetEntityByIdAsync(request.ActionVehicleDto.ModelId);
-
-        if (vehicleModel is null)
-            return Result<Vehicle>.Failure(new Error("NotFound", "Such model doesn't exists!"));
-
-        var vehicleBrand =
-            await unitOfWork.RetrieveRepository<VehicleBrand>().GetEntityByIdAsync(request.ActionVehicleDto.BrandId);
-
-        if (vehicleBrand is null)
-            return Result<Vehicle>.Failure(new Error("NotFound", "Such brand doesn't exists!"));
-
-        var vehicleColor =
-            await unitOfWork.RetrieveRepository<VehicleColor>().GetEntityByIdAsync(request.ActionVehicleDto.ColorId);
-
-        if (vehicleColor is null)
-            return Result<Vehicle>.Failure(new Error("NotFound", "Such color doesn't exists!"));
-
-        var vehicleType = await unitOfWork.RetrieveRepository<VehicleType>()
-            .GetEntityByIdAsync(request.ActionVehicleDto.TypeId);
-
-        if (vehicleType is null)
-            return Result<Vehicle>.Failure(new Error("NotFound", "Such type doesn't exists!"));
-
-        var vehicleDisplacement =
-            await unitOfWork.RetrieveRepository<VehicleDisplacement>()
-                .GetEntityByIdAsync(request.ActionVehicleDto.DisplacementId);
-
-        if (vehicleDisplacement is null)
-            return Result<Vehicle>.Failure(new Error("NotFound", "Such displacement doesn't exists!"));
-
-        var vehicle = new Vehicle(
-            brand: vehicleBrand,
-            model: vehicleModel,
-            color: vehicleColor,
-            type: vehicleType,
-            displacement: vehicleDisplacement,
-            price: request.ActionVehicleDto.Price,
-            description: request.ActionVehicleDto.Description);
-
-        return Result<Vehicle>.Success(vehicle);
-    }
+        !updatedVehicle.Prices.MaxBy(p => p.IssueDate)!.Value.Equals(request.UpdateVehicleDto.Price);
 }
