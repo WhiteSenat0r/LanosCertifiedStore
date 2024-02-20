bf74048d8bb449144c3b5fac5b825d7ea62bc935
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-21 01:27:58 +0200

    authentication service added

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Contracts/ServicesRelated/IdentityRelated/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using Application.Dtos.IdentityDtos;
+using Domain.Entities.VehicleRelated.Classes;
+
+namespace Application.Contracts.ServicesRelated.IdentityRelated;
+
+public interface IAuthenticationService
+{
+    Task<User?> LoginAsync(LoginDto loginDto);
+    Task<User?> RegisterAsync(RegisterDto registerDto);
+}
---
+++ LanosCertifiedStore.InfrastructureLayer.Services/IdentityRelated/AuthenticationService.cs
@@ -0,0 +1,29 @@
+﻿using Application.Contracts.ServicesRelated.IdentityRelated;
+using Application.Dtos.IdentityDtos;
+using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+
+namespace LanosCertifiedStore.InfrastructureLayer.Services.IdentityRelated;
+
+internal sealed class AuthenticationService(
+    IUserRepository userRepository,
+    IPasswordHasher passwordHasher) : IAuthenticationService
+{
+    public async Task<User?> LoginAsync(LoginDto loginDto)
+    {
+        var user = await userRepository.GetUserByEmailAsync(loginDto.Email);
+
+        passwordHasher.VerifyPassword(loginDto.Password, user.PasswordHash);
+        
+        return user;
+    }
+
+    public async Task<User?> RegisterAsync(RegisterDto registerDto)
+    {
+        var user = new User(registerDto.FirstName, registerDto.LastName, registerDto.Email);
+
+        await userRepository.CreateUserAsync(user, passwordHasher.HashPassword(registerDto.Password));
+
+        return user;
+    }
+}
