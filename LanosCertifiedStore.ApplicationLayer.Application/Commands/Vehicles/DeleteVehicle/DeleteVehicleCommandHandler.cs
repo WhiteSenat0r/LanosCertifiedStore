313f6c5173343201e0d440371cc235efdc2a93ce
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-20 17:24:28 +0200

    Finished implementation of DeleteVehicleCommandHandler.cs

--- LanosCertifiedStore.ApplicationLayer.Application/Commands/Vehicles/DeleteVehicle/DeleteVehicleCommandHandler.cs
+++ LanosCertifiedStore.ApplicationLayer.Application/Commands/Vehicles/DeleteVehicle/DeleteVehicleCommandHandler.cs
@@ -1,15 +1,25 @@
-﻿using Domain.Contracts.RepositoryRelated;
+﻿using Application.Contracts.ServicesRelated.ImageService;
+using Domain.Contracts.RepositoryRelated;
 using Domain.Entities.VehicleRelated.Classes;
 using Domain.Shared;
 using MediatR;
 
 namespace Application.Commands.Vehicles.DeleteVehicle;
 
-internal sealed class DeleteVehicleCommandHandler(IUnitOfWork unitOfWork)
+internal sealed class DeleteVehicleCommandHandler(IUnitOfWork unitOfWork, IImageService imageService)
     : IRequestHandler<DeleteVehicleCommand, Result<Unit>>
 {
     public async Task<Result<Unit>> Handle(DeleteVehicleCommand request, CancellationToken cancellationToken)
     {
+        var updatedEntity =
+            await unitOfWork.RetrieveRepository<Vehicle>().GetEntityByIdAsync(request.Id);
+
+        if (updatedEntity is null)
+            return Result<Unit>.Failure(Error.NotFound);
+
+        foreach (var image in updatedEntity.Images)
+            await imageService.TryDeletePhotoAsync(image.CloudImageId);
+        
         await unitOfWork.RetrieveRepository<Vehicle>().RemoveExistingEntity(request.Id);
 
         var result = await unitOfWork.SaveChangesAsync(cancellationToken) > 0;
