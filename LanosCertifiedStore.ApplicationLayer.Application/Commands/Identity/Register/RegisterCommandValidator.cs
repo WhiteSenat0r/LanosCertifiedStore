9b5c6055da27bc560219dbd916f1293b10a474b0
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-21 02:21:23 +0200

    Login and Register command validations added

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Commands/Identity/Register/RegisterCommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+
+namespace Application.Commands.Identity.Register;
+
+internal sealed class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+{
+    public RegisterCommandValidator()
+    {
+        RuleFor(x => x.RegisterDto.FirstName)
+            .NotEmpty()
+            .MinimumLength(2)
+            .MaximumLength(32);
+
+        RuleFor(x => x.RegisterDto.LastName)
+            .NotEmpty()
+            .MinimumLength(2)
+            .MaximumLength(32);
+
+        RuleFor(x => x.RegisterDto.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.RegisterDto.Password)
+            .NotEmpty()
+            .Must(password => password.Any(char.IsDigit)).WithMessage("Password should contain digits!")
+            .Must(password => password.Any(char.IsUpper) && password.Any(char.IsLower))
+            .WithMessage("Password should contain upper and lower-case characters!");
+    }
+}
