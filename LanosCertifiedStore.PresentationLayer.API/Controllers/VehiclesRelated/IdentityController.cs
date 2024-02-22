a403a54f6285d2f98f21754342ac11124bdcddc3
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-22 21:26:08 +0200

    Added the option for authentication status retrieval in IdentityController.cs

--- LanosCertifiedStore.PresentationLayer.API/Controllers/VehiclesRelated/IdentityController.cs
+++ LanosCertifiedStore.PresentationLayer.API/Controllers/VehiclesRelated/IdentityController.cs
@@ -1,5 +1,6 @@
 ﻿using API.Controllers.Common;
 using API.Responses;
+using Application.Commands.Identity.Authentication.AuthenticationStatus;
 using Application.Commands.Identity.Authentication.Login;
 using Application.Commands.Identity.Authentication.Register;
 using Application.Dtos.IdentityDtos.AuthenticationDtos;
@@ -17,6 +18,15 @@ public sealed class IdentityController : BaseIdentityRelatedController
     public async Task<ActionResult<UserDto>> Login([FromBody] LoginDto loginDto)
     {
         return HandleResult(await Mediator.Send(new LoginCommand(loginDto, Response)));
+    }
+    
+    [HttpGet("authenticationStatus")]
+    [ProducesResponseType(typeof(Dictionary<string, bool>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status401Unauthorized)]
+    public async Task<ActionResult<Dictionary<string, bool>>> GetAuthenticationStatus()
+    {
+        return HandleResult(await Mediator.Send(new AuthenticationStatusCommand(Request)));
     }
 
     [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
