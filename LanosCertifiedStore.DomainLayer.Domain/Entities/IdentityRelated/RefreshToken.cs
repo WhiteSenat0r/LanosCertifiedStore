0fed9f7c94600cd1b2f15ebd51d48a5570192af9
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-22 18:32:01 +0200

    Updated RefreshToken implementation

--- LanosCertifiedStore.DomainLayer.Domain/Entities/IdentityRelated/RefreshToken.cs
+++ LanosCertifiedStore.DomainLayer.Domain/Entities/IdentityRelated/RefreshToken.cs
@@ -8,8 +8,14 @@ public sealed class RefreshToken : IIdentifiable<Guid>
     public Guid Id { get; init; } = Guid.NewGuid();
     public User User { get; init; } = default!;
     public string Value { get; init; } = default!;
-    public DateTime ExpiryDate { get; init; }
+    public DateTime ExpiryDate { get; init; } = DateTime.UtcNow.AddDays(14);
     public DateTime? RevocationDate { get; init; }
     public bool IsExpired { get; init; }
     public bool IsValid { get; init; }
+    
+    public RefreshToken()
+    {
+        IsValid = RevocationDate is null && !IsExpired;
+        IsExpired = DateTime.UtcNow >= ExpiryDate;
+    }
 }
