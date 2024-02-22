b609aada0047d892bfdcb83a5ed623bf988e2395
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-22 15:40:48 +0200

    Added RefreshToken class

---
+++ LanosCertifiedStore.DomainLayer.Domain/Entities/IdentityRelated/RefreshToken.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Contracts.Common;
+using Domain.Entities.UserRelated;
+
+namespace Domain.Entities.IdentityRelated;
+
+public sealed class RefreshToken : IEntity<Guid>
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public User User { get; init; } = default!;
+    public string Value { get; init; } = default!;
+    public DateTime ExpiryDate { get; init; }
+    public DateTime? RevocationDate { get; init; }
+    public bool IsExpired { get; init; }
+    public bool IsValid { get; init; }
+}
