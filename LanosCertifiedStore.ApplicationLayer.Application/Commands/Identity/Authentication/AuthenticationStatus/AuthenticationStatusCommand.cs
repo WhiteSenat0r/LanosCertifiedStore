1d439568196f466395034bf1ad9769b26583995b
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-22 21:26:29 +0200

    Added AuthenticationStatusCommand.cs and AuthenticationStatusCommandHandler.cs

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Commands/Identity/Authentication/AuthenticationStatus/AuthenticationStatusCommand.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Shared;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Commands.Identity.Authentication.AuthenticationStatus;
+
+public sealed record AuthenticationStatusCommand(
+    HttpRequest HttpRequest) : IRequest<Result<IDictionary<string, bool>>>;
---
+++ LanosCertifiedStore.ApplicationLayer.Application/Commands/Identity/Authentication/AuthenticationStatus/AuthenticationStatusCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using Application.Contracts.ServicesRelated.IdentityRelated;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Commands.Identity.Authentication.AuthenticationStatus;
+
+internal sealed class AuthenticationStatusCommandHandler(
+    IAuthenticationService authenticationService) 
+    : IRequestHandler<AuthenticationStatusCommand, Result<IDictionary<string, bool>>>
+{
+    public async Task<Result<IDictionary<string, bool>>> Handle(
+        AuthenticationStatusCommand request, CancellationToken cancellationToken)
+    {
+        var userLoginResult = authenticationService.GetAuthenticationStatus(request.HttpRequest);
+        
+        return Result<IDictionary<string, bool>>.Success(userLoginResult);
+    }
+}
