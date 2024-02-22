9d4c476e081259e51c499d1c279bf3d822286803
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-22 18:29:20 +0200

    Moved JwtTokenOptions.cs and JwtTokenProvider.cs to a new directory

--- LanosCertifiedStore.InfrastructureLayer.Services/IdentityRelated/JwtRelated/JwtOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LanosCertifiedStore.InfrastructureLayer.Services.IdentityRelated.JwtRelated;
-
-public class JwtOptions
-{
-    public string Issuer { get; init; }
-    public string Audience { get; init; }
-    public string SecretKey { get; init; }
-}
