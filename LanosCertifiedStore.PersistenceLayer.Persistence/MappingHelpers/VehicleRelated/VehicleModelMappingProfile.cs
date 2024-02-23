13f2add330578e8c5c67429c2494eecece76de94
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-23 11:49:31 +0200

    Fixed issue with vehicle model mapping

--- LanosCertifiedStore.PersistenceLayer.Persistence/MappingHelpers/VehicleRelated/VehicleModelMappingProfile.cs
+++ LanosCertifiedStore.PersistenceLayer.Persistence/MappingHelpers/VehicleRelated/VehicleModelMappingProfile.cs
@@ -15,6 +15,7 @@ internal sealed class VehicleModelMappingProfile : Profile
     private void AddMappingProfileFromModelToEntity() =>
         CreateMap<VehicleModel, VehicleModelDataModel>()
             .ForMember(d => d.VehicleBrand, o => o.MapFrom(s => s.Brand.Name))
+            .ForMember(d => d.VehicleBrandId, o => o.MapFrom(s => s.Brand.Id))
             .ForMember(model => model.VehicleBrand, options => options.Ignore());
     
     private void AddMappingProfileFromEntityToModel() =>
