5291b39d235d20f4229a58b7cdb703560b8ce6b1
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-28 16:35:20 +0200

    Added SelectionProfile property in IVehicleImageFilteringRequestParameters.cs and VehicleImageFilteringRequestParameters.cs

--- LanosCertifiedStore.DomainLayer.Domain/Contracts/RequestParametersRelated/IVehicleImageFilteringRequestParameters.cs
+++ LanosCertifiedStore.DomainLayer.Domain/Contracts/RequestParametersRelated/IVehicleImageFilteringRequestParameters.cs
@@ -1,9 +1,11 @@
 ﻿using Domain.Contracts.RepositoryRelated;
 using Domain.Entities.VehicleRelated.Classes;
+using Domain.Enums.RequestParametersRelated;
 
 namespace Domain.Contracts.RequestParametersRelated;
 
 public interface IVehicleImageFilteringRequestParameters : IFilteringRequestParameters<VehicleImage>
 { 
     Guid? RelatedVehicleId { get; set; }
+    VehicleImageSelectionProfile SelectionProfile { get; }
 }
