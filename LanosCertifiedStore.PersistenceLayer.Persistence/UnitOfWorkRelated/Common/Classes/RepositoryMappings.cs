4812aa588906bb4752a18866746282698d43ac66
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-28 16:40:37 +0200

    Updated mappings in RepositoryMappings.cs

--- LanosCertifiedStore.PersistenceLayer.Persistence/UnitOfWorkRelated/Common/Classes/RepositoryMappings.cs
+++ LanosCertifiedStore.PersistenceLayer.Persistence/UnitOfWorkRelated/Common/Classes/RepositoryMappings.cs
@@ -1,14 +1,8 @@
 ﻿using Domain.Contracts.RepositoryRelated;
-using Domain.Entities.UserRelated;
 using Domain.Entities.VehicleRelated.Classes;
-using Persistence.Repositories.IdentityRelated;
 using Persistence.Repositories.VehicleBrandRelated;
 using Persistence.Repositories.VehicleColorRelated;
 using Persistence.Repositories.VehicleImageRelated;
-using Persistence.Repositories.VehicleModelRelated;
-using Persistence.Repositories.VehiclePriceRelated;
-using Persistence.Repositories.VehicleRelated;
-using Persistence.Repositories.VehicleTypeRelated;
 
 namespace Persistence.UnitOfWorkRelated.Common.Classes;
 
@@ -16,13 +10,13 @@ internal abstract class RepositoryMappings
 {
     public static readonly Dictionary<Type, Type> VehicleRelatedRepositoryMappings = new()
     {
-        { typeof(IRepository<Vehicle>), typeof(VehicleRepository) },
+        // { typeof(IRepository<Vehicle>), typeof(VehicleRepository) },
         { typeof(IRepository<VehicleBrand>), typeof(VehicleBrandRepository) },
-        { typeof(IRepository<VehicleModel>), typeof(VehicleModelRepository) },
-        { typeof(IRepository<VehicleType>), typeof(VehicleTypeRepository) },
+        // { typeof(IRepository<VehicleModel>), typeof(VehicleModelRepository) },
+        // { typeof(IRepository<VehicleType>), typeof(VehicleTypeRepository) },
         { typeof(IRepository<VehicleColor>), typeof(VehicleColorRepository) },
-        { typeof(IRepository<VehiclePrice>), typeof(VehiclePriceRepository) },
+        // { typeof(IRepository<VehiclePrice>), typeof(VehiclePriceRepository) },
         { typeof(IRepository<VehicleImage>), typeof(VehicleImageRepository) },
-        { typeof(IRepository<User>), typeof(UserRepository) }
+        // { typeof(IRepository<User>), typeof(UserRepository) }
     };
 }
