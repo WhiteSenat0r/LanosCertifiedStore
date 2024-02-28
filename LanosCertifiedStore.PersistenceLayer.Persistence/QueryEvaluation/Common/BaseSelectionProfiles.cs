0bb2aaf4c196f033dcb3ded5623cca79810bcebb
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-28 10:25:22 +0200

    Added BaseSelectionProfiles.cs

---
+++ LanosCertifiedStore.PersistenceLayer.Persistence/QueryEvaluation/Common/BaseSelectionProfiles.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Contracts.Common;
+using Domain.Contracts.RepositoryRelated;
+using Persistence.DataModels.VehicleRelated;
+
+namespace Persistence.QueryEvaluation.Common;
+
+internal abstract class BaseSelectionProfiles<TSelectionProfile, TEntity, TDataModel>
+    where TSelectionProfile : struct, Enum
+    where TEntity : IIdentifiable<Guid>
+    where TDataModel : class, IIdentifiable<Guid>
+{
+    public abstract IQueryable<VehicleBrandDataModel> GetSuitableSelectionProfileQueryable(
+        IQueryable<TDataModel> inputQueryable,
+        IFilteringRequestParameters<TEntity>? requestParameters = null);
+}
