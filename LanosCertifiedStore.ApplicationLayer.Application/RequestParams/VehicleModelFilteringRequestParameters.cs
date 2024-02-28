226676fafe236f21f9f38016acfe3154b2af8143
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-28 17:07:08 +0200

    Added VehicleModelSelectionProfile.cs, updated IVehicleModelFilteringRequestParameters.cs and VehicleModelFilteringRequestParameters.cs

--- LanosCertifiedStore.ApplicationLayer.Application/RequestParams/VehicleModelFilteringRequestParameters.cs
+++ LanosCertifiedStore.ApplicationLayer.Application/RequestParams/VehicleModelFilteringRequestParameters.cs
@@ -1,6 +1,7 @@
 ﻿using Application.RequestParams.Common.Classes;
 using Domain.Contracts.RequestParametersRelated;
 using Domain.Entities.VehicleRelated.Classes;
+using Domain.Enums.RequestParametersRelated;
 
 namespace Application.RequestParams;
 
@@ -9,4 +10,5 @@ public sealed class VehicleModelFilteringRequestParameters : BaseFilteringReques
 {
     public string? Name { get; set; }
     public string? ContainedBrandName { get; set; }
+    public VehicleModelSelectionProfile SelectionProfile { get; set; } = VehicleModelSelectionProfile.Default;
 }
