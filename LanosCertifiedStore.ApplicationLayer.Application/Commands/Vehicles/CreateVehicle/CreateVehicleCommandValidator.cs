81d413d93c233486ac0f066f4c76a644015ba6d3
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-29 00:56:23 +0200

    Update CreateVehicleCommandValidator.cs

--- LanosCertifiedStore.ApplicationLayer.Application/Commands/Vehicles/CreateVehicle/CreateVehicleCommandValidator.cs
+++ LanosCertifiedStore.ApplicationLayer.Application/Commands/Vehicles/CreateVehicle/CreateVehicleCommandValidator.cs
@@ -1,10 +1,12 @@
-﻿using FluentValidation;
+﻿using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using FluentValidation;
 
 namespace Application.Commands.Vehicles.CreateVehicle;
 
 internal sealed class CreateVehicleCommandValidator : AbstractValidator<CreateVehicleCommand>
 {
-    public CreateVehicleCommandValidator()
+    public CreateVehicleCommandValidator(IUnitOfWork unitOfWork)
     {
         RuleFor(x => x.Description)
             .NotEmpty()
@@ -18,5 +20,31 @@ internal sealed class CreateVehicleCommandValidator : AbstractValidator<CreateVe
         RuleFor(x => x.Displacement)
             .NotEmpty()
             .GreaterThan(0);
+
+        VehicleModel? model = null;
+
+        RuleFor(x => x.ModelId)
+            .NotEmpty()
+            .MustAsync(async (modelId, _) =>
+            {
+                model = await unitOfWork.RetrieveRepository<VehicleModel>().GetEntityByIdAsync(modelId);
+
+                return model is not null;
+            })
+            .WithMessage("Such model doesn't exists!");
+
+        RuleFor(x => x.TypeId)
+            .NotEmpty()
+            .Must(typeId =>
+            {
+                if (model is not null)
+                    return model.AvailableTypes
+                        .Select(x => x.Id)
+                        .ToList()
+                        .Contains(typeId);
+
+                return false;
+            })
+            .WithMessage("This type is not available for this model!");
     }
 }
