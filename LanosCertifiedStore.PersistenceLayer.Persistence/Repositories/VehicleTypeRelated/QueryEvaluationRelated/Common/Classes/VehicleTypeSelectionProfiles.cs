38963f44e1730ec4ca7eb3855d2e4c972bfef316
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-28 17:48:59 +0200

    Added VehicleTypeSelectionProfiles.cs

---
+++ LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/VehicleTypeRelated/QueryEvaluationRelated/Common/Classes/VehicleTypeSelectionProfiles.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using Domain.Enums.RequestParametersRelated;
+using Persistence.DataModels.VehicleRelated;
+using Persistence.QueryEvaluation.Common;
+
+namespace Persistence.Repositories.VehicleTypeRelated.QueryEvaluationRelated.Common.Classes;
+
+internal class VehicleTypeSelectionProfiles : 
+    BaseSelectionProfiles<VehicleTypeSelectionProfile, VehicleType, VehicleTypeDataModel>
+{
+    private readonly Dictionary<VehicleColorSelectionProfile,
+            Func<IQueryable<VehicleTypeDataModel>, IQueryable<VehicleTypeDataModel>>>
+        _mappedProfiles = new()
+        {
+            { VehicleColorSelectionProfile.Default, GetDefaultProfileQueryable }
+        };
+
+    public override IQueryable<VehicleTypeDataModel> GetSuitableSelectionProfileQueryable(
+        IQueryable<VehicleTypeDataModel> inputQueryable,
+        IFilteringRequestParameters<VehicleType>? requestParameters = null) =>
+        _mappedProfiles[VehicleColorSelectionProfile.Default](inputQueryable);
+
+    private static IQueryable<VehicleTypeDataModel> GetDefaultProfileQueryable(
+        IQueryable<VehicleTypeDataModel> queryable) =>
+        queryable.Select(vehicleType => new VehicleTypeDataModel
+        {
+            Id = vehicleType.Id,
+            Name = vehicleType.Name,
+        });
+}
