7b25a71bfbc52bcbddd2ed2041a19fc804dacdfd
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-28 16:37:24 +0200

    Added VehicleImageSelectionProfiles.cs

---
+++ LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/VehicleImageRelated/QueryEvaluationRelated/Common/Classes/VehicleImageSelectionProfiles.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using Domain.Enums.RequestParametersRelated;
+using Persistence.DataModels.VehicleRelated;
+using Persistence.QueryEvaluation.Common;
+
+namespace Persistence.Repositories.VehicleImageRelated.QueryEvaluationRelated.Common.Classes;
+
+internal class VehicleImageSelectionProfiles : 
+    BaseSelectionProfiles<VehicleImageSelectionProfile, VehicleImage, VehicleImageDataModel>
+{
+    private readonly Dictionary<VehicleImageSelectionProfile,
+            Func<IQueryable<VehicleImageDataModel>, IQueryable<VehicleImageDataModel>>>
+        _mappedProfiles = new()
+        {
+            { VehicleImageSelectionProfile.Default, GetDefaultProfileQueryable }
+        };
+
+    public override IQueryable<VehicleImageDataModel> GetSuitableSelectionProfileQueryable(
+        IQueryable<VehicleImageDataModel> inputQueryable,
+        IFilteringRequestParameters<VehicleImage>? requestParameters = null) =>
+        _mappedProfiles[VehicleImageSelectionProfile.Default](inputQueryable);
+
+    private static IQueryable<VehicleImageDataModel> GetDefaultProfileQueryable(
+        IQueryable<VehicleImageDataModel> queryable) =>
+        queryable.Select(vehicleImage => new VehicleImageDataModel
+        {
+            Id = vehicleImage.Id,
+            CloudImageId = vehicleImage.CloudImageId,
+            ImageUrl = vehicleImage.ImageUrl,
+            IsMainImage = vehicleImage.IsMainImage
+        });
+}
