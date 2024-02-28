0591a58d9938500da24d4ae41cf14506c7486058
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-02-28 23:34:53 +0200

    Create AddImagesToVehicleCommand.cs
    for the future

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Commands/Vehicles/AddImagesToVehicle/AddImagesToVehicleCommand.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Shared;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Commands.Vehicles.AddImagesToVehicle;
+
+public sealed record AddImagesToVehicleCommand(
+    Guid VehicleId,
+    ICollection<IFormFile> Images,
+    string MainImageName) : IRequest<Result<Unit>>;
