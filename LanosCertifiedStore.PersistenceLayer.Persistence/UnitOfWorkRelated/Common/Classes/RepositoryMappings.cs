ee050459f4ff6e1bb598c3683e71b1908162553e
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-29 10:27:56 +0200

    Updated RepositoryMappings.cs

--- LanosCertifiedStore.PersistenceLayer.Persistence/UnitOfWorkRelated/Common/Classes/RepositoryMappings.cs
+++ LanosCertifiedStore.PersistenceLayer.Persistence/UnitOfWorkRelated/Common/Classes/RepositoryMappings.cs
@@ -5,6 +5,7 @@ using Persistence.Repositories.VehicleColorRelated;
 using Persistence.Repositories.VehicleImageRelated;
 using Persistence.Repositories.VehicleModelRelated;
 using Persistence.Repositories.VehiclePriceRelated;
+using Persistence.Repositories.VehicleRelated;
 using Persistence.Repositories.VehicleTypeRelated;
 
 namespace Persistence.UnitOfWorkRelated.Common.Classes;
@@ -13,7 +14,7 @@ internal abstract class RepositoryMappings
 {
     public static readonly Dictionary<Type, Type> VehicleRelatedRepositoryMappings = new()
     {
-        // { typeof(IRepository<Vehicle>), typeof(VehicleRepository) },
+        { typeof(IRepository<Vehicle>), typeof(VehicleRepository) },
         { typeof(IRepository<VehicleBrand>), typeof(VehicleBrandRepository) },
         { typeof(IRepository<VehicleModel>), typeof(VehicleModelRepository) },
         { typeof(IRepository<VehicleType>), typeof(VehicleTypeRepository) },
