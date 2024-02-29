296881559074f8c3768a2ee879c5696053ca7476
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-29 20:01:01 +0200

    Added VehicleModelRelatedMappingProfile.cs

---
+++ LanosCertifiedStore.ApplicationLayer.Application/MappingProfiles/VehicleModelRelatedMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using Application.Dtos.ModelDtos;
+using AutoMapper;
+using Domain.Entities.VehicleRelated.Classes;
+
+namespace Application.MappingProfiles;
+
+public class VehicleModelRelatedMappingProfile : Profile
+{
+    public VehicleModelRelatedMappingProfile()
+    {
+        CreateMap<VehicleModel, ModelDto>()
+            .ForMember(d => d.VehicleBrand, o => o.MapFrom(s => s.Brand.Name))
+            .ForMember(d => d.AvailableTypes, o => o.MapFrom(s => s.AvailableTypes));
+    }
+}
