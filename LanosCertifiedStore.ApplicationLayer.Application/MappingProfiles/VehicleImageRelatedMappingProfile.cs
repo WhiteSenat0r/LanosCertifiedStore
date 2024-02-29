162fd2a9727a8714f0d8f8f13f96fecbd1111bd0
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-29 20:00:46 +0200

    Added VehicleImageRelatedMappingProfile.cs

---
+++ LanosCertifiedStore.ApplicationLayer.Application/MappingProfiles/VehicleImageRelatedMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using Application.Dtos.ImageDtos;
+using AutoMapper;
+using Domain.Entities.VehicleRelated.Classes;
+
+namespace Application.MappingProfiles;
+
+public class VehicleImageRelatedMappingProfile : Profile
+{
+    public VehicleImageRelatedMappingProfile()
+    {
+        CreateMap<VehicleImage, ImageDto>();
+        CreateMap<ImageDto, VehicleImage>();
+    }
+}
