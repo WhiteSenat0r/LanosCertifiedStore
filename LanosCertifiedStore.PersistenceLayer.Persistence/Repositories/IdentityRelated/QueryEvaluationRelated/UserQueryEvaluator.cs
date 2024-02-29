12a330d63c78bd64e7a27c5c3f4d02c7b53a7360
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-29 10:33:13 +0200

    Temporarily commented user repository implementation

--- LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/IdentityRelated/QueryEvaluationRelated/UserQueryEvaluator.cs
+++ LanosCertifiedStore.PersistenceLayer.Persistence/Repositories/IdentityRelated/QueryEvaluationRelated/UserQueryEvaluator.cs
@@ -1,42 +1,34 @@
-﻿using System.Linq.Expressions;
-using Domain.Contracts.RepositoryRelated;
-using Domain.Entities.UserRelated;
-using Microsoft.EntityFrameworkCore;
-using Persistence.DataModels.UserRelated;
-using Persistence.QueryEvaluation;
-using Persistence.QueryEvaluation.Common;
-using Persistence.Repositories.IdentityRelated.QueryEvaluationRelated.Common.Classes;
+﻿namespace Persistence.Repositories.IdentityRelated.QueryEvaluationRelated;
 
-namespace Persistence.Repositories.IdentityRelated.QueryEvaluationRelated;
-
-internal sealed class UserQueryEvaluator(
-    IEnumerable<Expression<Func<UserDataModel, object>>> includedAspects,
-    IFilteringRequestParameters<User>? filteringRequestParameters,
-    DbSet<UserDataModel> dataModels,
-    UserFilteringCriteria userFilteringCriteria)
-    : BaseQueryEvaluator<User, UserDataModel>(
-        includedAspects,
-        filteringRequestParameters,
-        dataModels,
-        userFilteringCriteria)
-{
-    private protected override BaseSortingSettings<UserDataModel> GetQuerySortingSettings()
-    {
-        if (string.IsNullOrEmpty(filteringRequestParameters!.SortingType))
-            return new UserSortingSettings
-            {
-                OrderByAscendingExpression = UserSortingTypes.Options["default"]
-            };
-
-        var settings = new UserSortingSettings();
-
-        if (filteringRequestParameters.SortingType.Contains("-asc"))
-            settings.OrderByAscendingExpression = UserSortingTypes.Options
-                [filteringRequestParameters.SortingType];
-        else if (filteringRequestParameters.SortingType.Contains("-desc"))
-            settings.OrderByDescendingExpression = UserSortingTypes.Options
-                [filteringRequestParameters.SortingType];
-
-        return settings;
-    }
-}
+// TODO Add implementation
+// internal sealed class UserQueryEvaluator(
+//     IEnumerable<Expression<Func<UserDataModel, object>>> selectionProfiles,
+//     IFilteringRequestParameters<User>? filteringRequestParameters,
+//     DbSet<UserDataModel> dataModels,
+//     UserFilteringCriteria userFilteringCriteria)
+//     : BaseQueryEvaluator<User, UserDataModel>(
+//         selectionProfiles,
+//         filteringRequestParameters,
+//         dataModels,
+//         userFilteringCriteria)
+// {
+//     private protected override BaseSortingSettings<UserDataModel> GetQuerySortingSettings()
+//     {
+//         if (string.IsNullOrEmpty(filteringRequestParameters!.SortingType))
+//             return new UserSortingSettings
+//             {
+//                 OrderByAscendingExpression = UserSortingTypes.Options["default"]
+//             };
+//
+//         var settings = new UserSortingSettings();
+//
+//         if (filteringRequestParameters.SortingType.Contains("-asc"))
+//             settings.OrderByAscendingExpression = UserSortingTypes.Options
+//                 [filteringRequestParameters.SortingType];
+//         else if (filteringRequestParameters.SortingType.Contains("-desc"))
+//             settings.OrderByDescendingExpression = UserSortingTypes.Options
+//                 [filteringRequestParameters.SortingType];
+//
+//         return settings;
+//     }
+// }
