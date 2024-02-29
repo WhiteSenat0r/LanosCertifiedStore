10056b4c29320205c04072254d95adbb5ab1a2a8
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-29 14:33:08 +0200

    Added transaction pipeline behavior

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Behaviors/TransactionPipelineBehavior.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Contracts.RepositoryRelated;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Behaviors;
+
+public class TransactionPipelineBehavior<TRequest, TResponse>(
+    IUnitOfWork unitOfWork) : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : Result<Unit>
+{
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        await unitOfWork.BeginTransactionAsync();
+
+        var response = await next();
+        
+        try
+        {
+            await unitOfWork.CommitTransactionAsync();
+        }
+        catch
+        {
+            await unitOfWork.RollbackTransactionAsync();
+        }
+
+        return response;
+    }
+}
