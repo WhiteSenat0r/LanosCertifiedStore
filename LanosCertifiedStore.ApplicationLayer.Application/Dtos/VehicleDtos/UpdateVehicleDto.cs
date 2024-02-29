2280d1b140d5f7b69736e25b90943650bafb68d2
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-29 18:22:48 +0200

    Refactored vehicle-related DTOs

--- LanosCertifiedStore.ApplicationLayer.Application/Dtos/VehicleDtos/UpdateVehicleDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Application.Dtos.ImageDtos;
-using Application.Dtos.VehicleDtos.Common;
-
-namespace Application.Dtos.VehicleDtos;
-
-public sealed record UpdateVehicleDto : ActionVehicleDto
-{
-    public List<ImageDto> Images { get; set; } = null!;
-}
