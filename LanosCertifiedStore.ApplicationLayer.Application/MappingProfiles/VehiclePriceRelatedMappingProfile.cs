53826d72ab26ffcf704b443c8680b1c87ca4c9ff
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-02-29 20:01:26 +0200

    Added VehiclePriceRelatedMappingProfile.cs

---
+++ LanosCertifiedStore.ApplicationLayer.Application/MappingProfiles/VehiclePriceRelatedMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using Application.Dtos.PriceDtos;
+using AutoMapper;
+using Domain.Entities.VehicleRelated.Classes;
+
+namespace Application.MappingProfiles;
+
+public class VehiclePriceRelatedMappingProfile : Profile
+{
+    public VehiclePriceRelatedMappingProfile()
+    {
+        CreateMap<VehiclePrice, PriceDto>();
+    }
+}
