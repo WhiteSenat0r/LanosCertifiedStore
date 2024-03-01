43d7fab2f3d0c6a4a2584581c9c722c067d84c90
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-01 11:20:41 +0200

    Refactored DeleteBrandCommandHandler.cs

--- LanosCertifiedStore.ApplicationLayer.Application/Commands/Brands/DeleteBrand/DeleteBrandCommandHandler.cs
+++ LanosCertifiedStore.ApplicationLayer.Application/Commands/Brands/DeleteBrand/DeleteBrandCommandHandler.cs
@@ -1,21 +1,46 @@
-﻿using Domain.Contracts.RepositoryRelated;
+﻿using Application.Commands.Common;
+using Domain.Contracts.RepositoryRelated;
 using Domain.Entities.VehicleRelated.Classes;
 using Domain.Shared;
 using MediatR;
 
 namespace Application.Commands.Brands.DeleteBrand;
 
-internal sealed class DeleteBrandCommandHandler(IUnitOfWork unitOfWork)
-    : IRequestHandler<DeleteBrandCommand, Result<Unit>>
+internal sealed class DeleteBrandCommandHandler : CommandBase, IRequestHandler<DeleteBrandCommand, Result<Unit>>
 {
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteBrandCommandHandler(IUnitOfWork unitOfWork) : base(unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+        PossibleErrorMessages = 
+            ["Removing a brand was not successful!", "Error occured during the brand removal!"];
+        PossibleErrorCode = "DeleteBrandError";
+    }
+
     public async Task<Result<Unit>> Handle(DeleteBrandCommand request, CancellationToken cancellationToken)
     {
-        await unitOfWork.RetrieveRepository<VehicleBrand>().RemoveExistingEntity(request.Id);
+        var brandRepository = _unitOfWork.RetrieveRepository<VehicleBrand>();
+        
+        var removedBrand = await brandRepository.GetEntityByIdAsync(request.Id);
+        
+        if (removedBrand is null) return Result<Unit>.Failure(Error.NotFound);
 
-        var result = await unitOfWork.SaveChangesAsync(cancellationToken) > 0;
+        await RemoveBrandWithRelatedData(request.Id, removedBrand.Models, brandRepository);
 
-        return result
-            ? Result<Unit>.Success(Unit.Value)
-            : Result<Unit>.Failure(new Error("DeleteError", "Failed to delete brand"));
+        return await TrySaveChanges(cancellationToken);
+    }
+
+    private async Task RemoveBrandWithRelatedData(
+        Guid removedBrandId, 
+        IEnumerable<VehicleModel> relatedModels,
+        IRepository<VehicleBrand> brandRepository)
+    {
+        var modelRepository = _unitOfWork.RetrieveRepository<VehicleModel>();
+        
+        foreach (var relatedModel in relatedModels)
+            await modelRepository.RemoveExistingEntity(relatedModel.Id);
+
+        await brandRepository.RemoveExistingEntity(removedBrandId);
     }
 }
