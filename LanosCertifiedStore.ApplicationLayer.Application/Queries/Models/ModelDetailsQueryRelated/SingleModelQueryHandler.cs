e5b68999071daaf7deed61456e2111640a9abf24
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-02 16:03:25 +0200

    Added single model query and its handler

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Queries/Models/ModelDetailsQueryRelated/SingleModelQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Application.Dtos.ModelDtos;
+using AutoMapper;
+using Domain.Contracts.RepositoryRelated;
+using Domain.Entities.VehicleRelated.Classes;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Queries.Models.ModelDetailsQueryRelated;
+
+internal sealed class SingleModelQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    : IRequestHandler<SingleModelQuery, Result<ModelDto>>
+{
+    public async Task<Result<ModelDto>> Handle(SingleModelQuery request,
+        CancellationToken cancellationToken)
+    {
+        var model = await unitOfWork.RetrieveRepository<VehicleModel>().GetEntityByIdAsync(request.Id);
+
+        if (model is null) return null!;
+        
+        var modelToReturn = mapper.Map<VehicleModel, ModelDto>(model!);
+
+        return Result<ModelDto>.Success(modelToReturn);
+    }
+}
