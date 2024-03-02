fd5ccaa06bd2d4bb76e32fd3b4e37b760abff0f5
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-02 16:03:50 +0200

    Placed ModelsQuery.cs and ModelsQueryHandler.cs into the new directory

--- LanosCertifiedStore.ApplicationLayer.Application/Queries/Models/ModelQueryRelated/ModelsQuery.cs
+++ LanosCertifiedStore.ApplicationLayer.Application/Queries/Models/ModelQueryRelated/ModelsQuery.cs
@@ -3,7 +3,7 @@ using Domain.Contracts.RequestParametersRelated;
 using Domain.Shared;
 using MediatR;
 
-namespace Application.Queries.Models;
+namespace Application.Queries.Models.ModelQueryRelated;
 
 public sealed record ModelsQuery(IVehicleModelFilteringRequestParameters RequestParameters)
     : IRequest<Result<IReadOnlyList<ModelDto>>>;
