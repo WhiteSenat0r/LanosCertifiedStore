b181996fe2a1a25db8e58faeea7d02b0285f5e9e
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-06 12:49:20 +0200

    Added PriceRangeQueryHandler.cs and related handler

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Queries/Vehicles/VehiclePriceRangeQueryRelated/VehiclePriceRangeQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.RequestParams;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Queries.Vehicles.VehiclePriceRangeQueryRelated;
+
+public sealed record VehiclePriceRangeQuery(VehicleFilteringRequestParameters RequestParameters) : 
+    IRequest<Result<IDictionary<string, decimal>>>;
