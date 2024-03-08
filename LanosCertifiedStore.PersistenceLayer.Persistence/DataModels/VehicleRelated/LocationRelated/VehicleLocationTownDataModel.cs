afdf9a5b5818370f934b886d7d40fd665cd59428
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-08 16:56:19 +0200

    Added VehicleLocationTownDataModel.cs

---
+++ LanosCertifiedStore.PersistenceLayer.Persistence/DataModels/VehicleRelated/LocationRelated/VehicleLocationTownDataModel.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities.VehicleRelated.Classes.Common.Classes;
+
+namespace Persistence.DataModels.VehicleRelated.LocationRelated;
+
+public sealed class VehicleLocationTownDataModel : NamedVehicleAspect
+{
+    public Guid LocationRegionId { get; set; }
+    public VehicleLocationRegionDataModel LocationRegion { get; init; } = null!;
+    
+    public VehicleLocationTownDataModel() { }
+
+    public VehicleLocationTownDataModel(string name) : base(name) { }
+
+    public VehicleLocationTownDataModel(string name, VehicleLocationRegionDataModel locationRegion) : base(name) => 
+        LocationRegion = locationRegion;
+}
