242f075a795ad7c12a43290679b03f9eeee82257
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-08 16:55:43 +0200

    Added VehicleDrivetrainTypeDataModel.cs

---
+++ LanosCertifiedStore.PersistenceLayer.Persistence/DataModels/VehicleRelated/TypeRelated/VehicleDrivetrainTypeDataModel.cs
@@ -0,0 +1,10 @@
+﻿using Persistence.DataModels.Common.Classes;
+
+namespace Persistence.DataModels.VehicleRelated.TypeRelated;
+
+internal sealed class VehicleDrivetrainTypeDataModel : NamedVehicleTypeAspect
+{
+    public VehicleDrivetrainTypeDataModel() { }
+
+    public VehicleDrivetrainTypeDataModel(string name) : base(name) { }
+}
