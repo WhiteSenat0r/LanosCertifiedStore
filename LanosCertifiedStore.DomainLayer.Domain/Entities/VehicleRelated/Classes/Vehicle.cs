825a32bdceb087b0647fb4b91a78fd36c8cd5ad3
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-08 16:49:03 +0200

    Updated Vehicle.cs with new aspects

--- LanosCertifiedStore.DomainLayer.Domain/Entities/VehicleRelated/Classes/Vehicle.cs
+++ LanosCertifiedStore.DomainLayer.Domain/Entities/VehicleRelated/Classes/Vehicle.cs
@@ -1,4 +1,6 @@
 ﻿using Domain.Contracts.Common;
+using Domain.Entities.VehicleRelated.Classes.LocationRelated;
+using Domain.Entities.VehicleRelated.Classes.TypesRelated;
 
 namespace Domain.Entities.VehicleRelated.Classes;
 
@@ -6,11 +8,18 @@ public sealed class Vehicle : IIdentifiable<Guid>
 {
     public Guid Id { get; init; } = Guid.NewGuid();
     public double Displacement { get; init; }
+    public int Mileage { get; init; }
+    public int ProductionYear { get; init; }
     public string Description { get; init; } = null!;
+    public VehicleLocationTown LocationTown { get; init; } = null!;
     public VehicleBrand Brand { get; init; } = null!;
     public VehicleModel Model { get; init; } = null!;
     public VehicleColor Color { get; init; } = null!;
-    public VehicleType Type { get; init; } = null!;
+    public VehicleType VehicleType { get; init; } = null!;
+    public VehicleBodyType BodyType { get; init; } = null!;
+    public VehicleEngineType EngineType { get; init; } = null!;
+    public VehicleTransmissionType TransmissionType { get; init; } = null!;
+    public VehicleDrivetrainType DrivetrainType { get; init; } = null!;
     public ICollection<VehicleImage> Images { get; init; } = new List<VehicleImage>();
     public ICollection<VehiclePrice> Prices { get; init; } = new List<VehiclePrice>();
 
@@ -20,7 +29,7 @@ public sealed class Vehicle : IIdentifiable<Guid>
         VehicleBrand brand,
         VehicleModel model,
         VehicleColor color,
-        VehicleType type,
+        VehicleType vehicleType,
         decimal price,
         double displacement,
         string description)
@@ -29,7 +38,7 @@ public sealed class Vehicle : IIdentifiable<Guid>
         Brand = brand;
         Model = model;
         Color = color;
-        Type = type;
+        VehicleType = vehicleType;
         Displacement = displacement;
         Description = description;
     }
@@ -38,7 +47,7 @@ public sealed class Vehicle : IIdentifiable<Guid>
         VehicleBrand brand,
         VehicleModel model,
         VehicleColor color,
-        VehicleType type,
+        VehicleType vehicleType,
         decimal price,
         double displacement,
         string description,
@@ -48,7 +57,7 @@ public sealed class Vehicle : IIdentifiable<Guid>
         Brand = brand;
         Model = model;
         Color = color;
-        Type = type;
+        VehicleType = vehicleType;
         Displacement = displacement;
         Description = description;
         Images = images;
