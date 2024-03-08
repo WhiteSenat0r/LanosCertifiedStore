bebebcf69c0a480062080835997b8c90e69d01a5
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-08 16:44:19 +0200

    Added location-related entities

---
+++ LanosCertifiedStore.DomainLayer.Domain/Entities/VehicleRelated/Classes/LocationRelated/VehicleLocationRegion.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities.VehicleRelated.Classes.Common.Classes;
+
+namespace Domain.Entities.VehicleRelated.Classes.LocationRelated;
+
+public sealed class VehicleLocationRegion : NamedVehicleAspect
+{
+    public ICollection<VehicleLocationTown> RelatedTowns { get; init; } = [];
+    
+    public VehicleLocationRegion() { }
+
+    public VehicleLocationRegion(string name) : base(name) { }
+}
---
+++ LanosCertifiedStore.DomainLayer.Domain/Entities/VehicleRelated/Classes/LocationRelated/VehicleLocationTown.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities.VehicleRelated.Classes.Common.Classes;
+
+namespace Domain.Entities.VehicleRelated.Classes.LocationRelated;
+
+public sealed class VehicleLocationTown : NamedVehicleAspect
+{
+    public VehicleLocationRegion LocationRegion { get; init; } = null!;
+    
+    public VehicleLocationTown() { }
+
+    public VehicleLocationTown(string name) : base(name) { }
+
+    public VehicleLocationTown(string name, VehicleLocationRegion locationRegion) : base(name) => 
+        LocationRegion = locationRegion;
+}
