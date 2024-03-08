c6ea4d7309956736f0bcd48f5f786127b83110a2
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-08 16:45:10 +0200

    Added VehicleDrivetrainType.cs

---
+++ LanosCertifiedStore.DomainLayer.Domain/Entities/VehicleRelated/Classes/TypesRelated/VehicleDrivetrainType.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities.VehicleRelated.Classes.Common.Classes;
+
+namespace Domain.Entities.VehicleRelated.Classes.TypesRelated;
+
+public sealed class VehicleDrivetrainType : NamedVehicleTypeAspect
+{
+    public VehicleDrivetrainType() { }
+    
+    public VehicleDrivetrainType(string name) : base(name) { }
+}
