55492bf48d6de42935887f02b9d81d2dda45a4fd
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-08 16:57:38 +0200

    Updated VehicleDataModel.cs

--- LanosCertifiedStore.PersistenceLayer.Persistence/DataModels/VehicleRelated/VehicleDataModel.cs
+++ LanosCertifiedStore.PersistenceLayer.Persistence/DataModels/VehicleRelated/VehicleDataModel.cs
@@ -1,4 +1,6 @@
 ﻿using Domain.Contracts.Common;
+using Persistence.DataModels.VehicleRelated.LocationRelated;
+using Persistence.DataModels.VehicleRelated.TypeRelated;
 
 namespace Persistence.DataModels.VehicleRelated;
 
@@ -7,14 +9,26 @@ internal sealed class VehicleDataModel : IIdentifiable<Guid>
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Description { get; set; } = null!;
     public double Displacement { get; set; }
+    public int Mileage { get; set; }
+    public int ProductionYear { get; set; }
+    public Guid LocationTownId { get; set; }
+    public VehicleLocationTownDataModel LocationTown { get; set; } = null!;
     public Guid BrandId { get; set; }
     public VehicleBrandDataModel Brand { get; set; } = null!;
     public Guid ModelId { get; set; }
     public VehicleModelDataModel Model { get; set; } = null!;
     public Guid ColorId { get; set; }
     public VehicleColorDataModel Color { get; set; } = null!;
-    public Guid TypeId { get; set; }
-    public VehicleTypeDataModel Type { get; set; } = null!;
+    public Guid VehicleTypeId { get; set; }
+    public VehicleTypeDataModel VehicleType { get; set; } = null!;
+    public Guid BodyTypeId { get; set; }
+    public VehicleBodyTypeDataModel BodyType { get; set; } = null!;
+    public Guid EngineTypeId { get; set; }
+    public VehicleEngineTypeDataModel EngineType { get; set; } = null!;
+    public Guid TransmissionTypeId { get; set; }
+    public VehicleTransmissionTypeDataModel TransmissionType { get; set; } = null!;
+    public Guid DrivetrainTypeId { get; set; }
+    public VehicleDrivetrainTypeDataModel DrivetrainType { get; set; } = null!;
     public ICollection<VehicleImageDataModel> Images { get; set; } = new List<VehicleImageDataModel>();
     public ICollection<VehiclePriceDataModel> Prices { get; set; } = new List<VehiclePriceDataModel>();
 
@@ -23,7 +37,7 @@ internal sealed class VehicleDataModel : IIdentifiable<Guid>
     public VehicleDataModel(
         Guid brandId,
         Guid modelId,
-        Guid typeId,
+        Guid vehicleTypeId,
         Guid colorId,
         decimal price,
         double displacement,
@@ -31,7 +45,7 @@ internal sealed class VehicleDataModel : IIdentifiable<Guid>
     {
         BrandId = brandId;
         ModelId = modelId;
-        TypeId = typeId;
+        VehicleTypeId = vehicleTypeId;
         ColorId = colorId;
         Displacement = displacement;
         Prices.Add(new VehiclePriceDataModel(Id, price));
