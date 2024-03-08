62f06edea76216a38b8d2b4681146fcd40d6aca4
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-08 16:55:51 +0200

    Added VehicleEngineTypeDataModel.cs

---
+++ LanosCertifiedStore.PersistenceLayer.Persistence/DataModels/VehicleRelated/TypeRelated/VehicleEngineTypeDataModel.cs
@@ -0,0 +1,10 @@
+﻿using Persistence.DataModels.Common.Classes;
+
+namespace Persistence.DataModels.VehicleRelated.TypeRelated;
+
+internal sealed class VehicleEngineTypeDataModel : NamedVehicleTypeAspect
+{
+    public VehicleEngineTypeDataModel() { }
+
+    public VehicleEngineTypeDataModel(string name) : base(name) { }
+}
