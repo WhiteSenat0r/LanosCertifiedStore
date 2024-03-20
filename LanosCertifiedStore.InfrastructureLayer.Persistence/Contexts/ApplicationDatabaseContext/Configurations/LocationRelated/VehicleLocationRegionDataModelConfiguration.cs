e5d4ecc3352ff65ff22de2023cab5e3816c8b77e
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-20 16:11:26 +0200

    Added location region and town configurations

---
+++ LanosCertifiedStore.InfrastructureLayer.Persistence/Contexts/ApplicationDatabaseContext/Configurations/LocationRelated/VehicleLocationRegionDataModelConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Persistence.DataModels.VehicleRelated.LocationRelated;
+
+namespace Persistence.Contexts.ApplicationDatabaseContext.Configurations.LocationRelated;
+
+internal sealed class VehicleLocationRegionConfiguration : IEntityTypeConfiguration<VehicleLocationRegionDataModel>
+{
+    public void Configure(EntityTypeBuilder<VehicleLocationRegionDataModel> builder)
+    {
+        builder.Property(p => p.Name)
+            .IsRequired()
+            .HasMaxLength(64);
+        
+        builder.HasMany(m => m.RelatedTowns)
+            .WithOne(l => l.LocationRegion)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasIndex(p => p.Name).IsUnique();
+    }
+}
