071d31b3a4098365684da80916d8798ee7931d55
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-20 16:01:46 +0200

    Changed the used database to PostgreSQL

--- LanosCertifiedStore.InfrastructureLayer.Persistence/SeedingData/SeedData.cs
+++ LanosCertifiedStore.InfrastructureLayer.Persistence/SeedingData/SeedData.cs
@@ -7,37 +7,38 @@ public static class SeedData
 {
     public static async Task Seed(ApplicationDatabaseContext context)
     {
-        var types = SeedTypes.GetTypes();
-        if (!await context.VehicleTypes.AnyAsync())
-            await context.VehicleTypes.AddRangeAsync(types);
-
-        var colors = SeedColors.GetColors();
-        if (!await context.VehiclesColors.AnyAsync())
-            await context.VehiclesColors.AddRangeAsync(colors);
-        
-        var brands = SeedBrands.GetBrands();
-        if (!await context.VehiclesBrands.AnyAsync())
-            await context.VehiclesBrands.AddRangeAsync(brands);
-        
-        if (context.ChangeTracker.HasChanges())
-            await context.SaveChangesAsync();
-
-        var models = SeedModels.GetModels(brands, types);
-        if (!await context.VehicleModels.AnyAsync()) 
-            await context.VehicleModels.AddRangeAsync(models);
-        
-        if (context.ChangeTracker.HasChanges())
-            await context.SaveChangesAsync();
-        
-        var vehicles = SeedVehicles.GetVehicles(types, colors, brands, models);
-        if (!await context.Vehicles.AnyAsync())
-            await context.Vehicles.AddRangeAsync(vehicles);
-        
-        var images = SeedImages.GetImages(vehicles);
-        if (!await context.VehicleImages.AnyAsync())
-            await context.VehicleImages.AddRangeAsync(images);
-        
-        if (context.ChangeTracker.HasChanges())
-            await context.SaveChangesAsync();
+        // TODO
+        // var types = SeedTypes.GetTypes();
+        // if (!await context.VehicleTypes.AnyAsync())
+        //     await context.VehicleTypes.AddRangeAsync(types);
+        //
+        // var colors = SeedColors.GetColors();
+        // if (!await context.VehiclesColors.AnyAsync())
+        //     await context.VehiclesColors.AddRangeAsync(colors);
+        //
+        // var brands = SeedBrands.GetBrands();
+        // if (!await context.VehiclesBrands.AnyAsync())
+        //     await context.VehiclesBrands.AddRangeAsync(brands);
+        //
+        // if (context.ChangeTracker.HasChanges())
+        //     await context.SaveChangesAsync();
+        //
+        // var models = SeedModels.GetModels(brands, types);
+        // if (!await context.VehicleModels.AnyAsync()) 
+        //     await context.VehicleModels.AddRangeAsync(models);
+        //
+        // if (context.ChangeTracker.HasChanges())
+        //     await context.SaveChangesAsync();
+        //
+        // var vehicles = SeedVehicles.GetVehicles(types, colors, brands, models);
+        // if (!await context.Vehicles.AnyAsync())
+        //     await context.Vehicles.AddRangeAsync(vehicles);
+        //
+        // var images = SeedImages.GetImages(vehicles);
+        // if (!await context.VehicleImages.AnyAsync())
+        //     await context.VehicleImages.AddRangeAsync(images);
+        //
+        // if (context.ChangeTracker.HasChanges())
+        //     await context.SaveChangesAsync();
     }
 }
