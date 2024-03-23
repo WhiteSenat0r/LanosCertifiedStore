b343b38c4f42be9f6e64aa08ffb216d544bed687
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-23 20:23:13 +0200

    Added SeedTowns.cs

---
+++ LanosCertifiedStore.InfrastructureLayer.Persistence/SeedingData/SeedTowns.cs
@@ -0,0 +1,24 @@
+﻿using Persistence.DataModels.VehicleRelated.LocationRelated;
+
+namespace Persistence.SeedingData;
+
+internal static class SeedTowns
+{
+    public static List<VehicleLocationTownDataModel> GetTowns(
+        ICollection<VehicleLocationRegionDataModel> regions,
+        ICollection<VehicleLocationAreaDataModel> areas,
+        Dictionary<string, Dictionary<string, List<string>>> regionAreaTownDictionary) =>
+        regionAreaTownDictionary.SelectMany(
+            regionAreaPair => regionAreaPair.Value.SelectMany(
+                areaTownPair => areaTownPair.Value.Select(
+                    town => new VehicleLocationTownDataModel(
+                        town,
+                        areas.Single(r => r.Name.Equals(areaTownPair.Key)),
+                        regions.Single(r => r.Name.Equals(regionAreaPair.Key)))
+                    {
+                        LocationRegionId = regions.Single(
+                            r => r.Name.Equals(regionAreaPair.Key)).Id,
+                        LocationAreaId = areas.Single(
+                            r => r.Name.Equals(areaTownPair.Key)).Id,
+                    }))).ToList();
+}
