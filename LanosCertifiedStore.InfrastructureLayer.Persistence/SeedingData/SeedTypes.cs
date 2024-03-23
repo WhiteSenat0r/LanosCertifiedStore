90f8eaa60450c3590bd5c9b87f81ac2618879d9c
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-23 20:23:26 +0200

    Altered SeedTypes.cs

--- LanosCertifiedStore.InfrastructureLayer.Persistence/SeedingData/SeedTypes.cs
+++ LanosCertifiedStore.InfrastructureLayer.Persistence/SeedingData/SeedTypes.cs
@@ -6,22 +6,7 @@ internal static class SeedTypes
 {
     public static List<VehicleTypeDataModel> GetTypes() =>
     [
-        new VehicleTypeDataModel("Седан"),
-        new VehicleTypeDataModel("Хетчбек"),
-        new VehicleTypeDataModel("Купе"),
-        new VehicleTypeDataModel("Універсал"),
-        new VehicleTypeDataModel("Кросовер"),
-        new VehicleTypeDataModel("Спорткар"),
-        new VehicleTypeDataModel("Позашляховик"),
-        new VehicleTypeDataModel("Мінівен"),
-        new VehicleTypeDataModel("Пікап"),
-        new VehicleTypeDataModel("Лімузин"),
-        new VehicleTypeDataModel("Кабріолет"),
-        new VehicleTypeDataModel("Фургон"),
-        new VehicleTypeDataModel("Компактвен"),
-        new VehicleTypeDataModel("Електромобіль"),
-        new VehicleTypeDataModel("Гібрид"),
-        new VehicleTypeDataModel("Конвертібл"),
+        new VehicleTypeDataModel("Легковик"),
         new VehicleTypeDataModel("Автобус"),
         new VehicleTypeDataModel("Вантажівка"),
     ];
