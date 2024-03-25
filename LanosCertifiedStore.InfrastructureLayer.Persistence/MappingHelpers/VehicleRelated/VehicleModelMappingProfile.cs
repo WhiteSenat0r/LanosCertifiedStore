c182437c0958f8269dd2224443371f9b687905fd
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-25 12:37:59 +0200

    Renamed AvailableTypes property to VehicleType in VehicleModelMappingProfile.cs

--- LanosCertifiedStore.InfrastructureLayer.Persistence/MappingHelpers/VehicleRelated/VehicleModelMappingProfile.cs
+++ LanosCertifiedStore.InfrastructureLayer.Persistence/MappingHelpers/VehicleRelated/VehicleModelMappingProfile.cs
@@ -18,7 +18,7 @@ internal sealed class VehicleModelMappingProfile : Profile
         CreateMap<VehicleModel, VehicleModelDataModel>()
             .ForMember(d => d.VehicleBrandId, o => o.MapFrom(s => s.Brand.Id))
             .ForMember(model => model.VehicleBrand, options => options.Ignore())
-            .ForMember(d => d.AvailableTypes, o => o.MapFrom(s => MapAvailableTypes(s.AvailableTypes)));
+            .ForMember(d => d.VehicleType, o => o.MapFrom(s => MapAvailableTypes(s.AvailableTypes)));
 
     private void AddMappingProfileFromEntityToModel() =>
         CreateMap<VehicleModelDataModel, VehicleModel>()
