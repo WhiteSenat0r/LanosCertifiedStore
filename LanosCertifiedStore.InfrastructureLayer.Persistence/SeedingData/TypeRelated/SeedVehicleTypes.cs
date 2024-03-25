a23cb5cfc57d3400c2c5752bdcc10cccf200fad3
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-25 12:30:33 +0200

    Renamed SeedTypes.cs to SeedVehicleBodyTypes.cs

--- LanosCertifiedStore.InfrastructureLayer.Persistence/SeedingData/TypeRelated/SeedVehicleTypes.cs
+++ LanosCertifiedStore.InfrastructureLayer.Persistence/SeedingData/TypeRelated/SeedVehicleTypes.cs
@@ -1,10 +1,10 @@
 ﻿using Persistence.DataModels.VehicleRelated.TypeRelated;
 
-namespace Persistence.SeedingData;
+namespace Persistence.SeedingData.TypeRelated;
 
-internal static class SeedTypes
+internal static class SeedVehicleTypes
 {
-    public static List<VehicleTypeDataModel> GetTypes() =>
+    public static List<VehicleTypeDataModel> GetVehicleTypes() =>
     [
         new VehicleTypeDataModel("Легковик"),
         new VehicleTypeDataModel("Автобус"),
