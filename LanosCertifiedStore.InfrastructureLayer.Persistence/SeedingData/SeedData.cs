a62ab5302fef2f982873e94d4671d2516c788d47
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-25 12:38:34 +0200

    Temporarily commented EF-related code parts in SeedData.cs

--- LanosCertifiedStore.InfrastructureLayer.Persistence/SeedingData/SeedData.cs
+++ LanosCertifiedStore.InfrastructureLayer.Persistence/SeedingData/SeedData.cs
@@ -1,7 +1,8 @@
 ﻿using System.Text.Json;
-using Microsoft.EntityFrameworkCore;
 using Persistence.Contexts.ApplicationDatabaseContext;
 using Persistence.DataModels.VehicleRelated.LocationRelated;
+using Persistence.SeedingData.LocationRelated;
+using Persistence.SeedingData.TypeRelated;
 
 namespace Persistence.SeedingData;
 
@@ -13,32 +14,54 @@ public static class SeedData
     
     public static async Task Seed(ApplicationDatabaseContext context)
     {
-        var types = SeedTypes.GetTypes();
-        if (!await context.VehicleTypes.AnyAsync())
-            await context.VehicleTypes.AddRangeAsync(types);
+        var types = SeedVehicleTypes.GetVehicleTypes();
+        // if (!await context.VehicleTypes.AnyAsync())
+        //     await context.VehicleTypes.AddRangeAsync(types);
         
         var colors = SeedColors.GetColors();
-        if (!await context.VehiclesColors.AnyAsync())
-            await context.VehiclesColors.AddRangeAsync(colors);
+        // if (!await context.VehiclesColors.AnyAsync())
+        //     await context.VehiclesColors.AddRangeAsync(colors);
+
+        var bodyTypes = SeedVehicleBodyTypes.GetVehicleBodyTypes();
+        // if (!await context.VehicleBodyTypes.AnyAsync())
+        //     await context.VehicleBodyTypes.AddRangeAsync(bodyTypes);
+
+        var drivetrainTypes = SeedVehicleDrivetrainTypes.GetVehicleDrivetrainTypes();
+        // if (!await context.VehicleDrivetrainTypes.AnyAsync())
+        //     await context.VehicleDrivetrainTypes.AddRangeAsync(drivetrainTypes);
+        
+        var engineTypes = SeedVehicleEngineTypes.GetVehicleEngineTypes();
+        // if (!await context.VehicleEngineTypes.AnyAsync())
+        //     await context.VehicleEngineTypes.AddRangeAsync(engineTypes);
+        
+        var transmissionTypes = SeedVehicleTransmissionTypes.GetVehicleTransmissionTypes();
+        // if (!await context.VehicleTransmissionTypes.AnyAsync())
+        //     await context.VehicleTransmissionTypes.AddRangeAsync(transmissionTypes);
         
         var brands = SeedBrands.GetBrands();
-        if (!await context.VehiclesBrands.AnyAsync())
-            await context.VehiclesBrands.AddRangeAsync(brands);
+        // if (!await context.VehiclesBrands.AnyAsync())
+        //     await context.VehiclesBrands.AddRangeAsync(brands);
         
-        if (context.ChangeTracker.HasChanges())
-            await context.SaveChangesAsync();
+        // if (context.ChangeTracker.HasChanges())
+        //     await context.SaveChangesAsync();
 
         var locationsData = await GetLocationsData();
 
         var regions = SeedRegions.GetRegions(locationsData!.Keys);
+        // if (!await context.VehicleLocationRegions.AnyAsync())
+        //     await context.VehicleLocationRegions.AddRangeAsync(regions);
 
         var areaRegionDictionary = GetAreaRegionDictionary(regions, locationsData);
-
         var areas = SeedAreas.GetAreas(areaRegionDictionary, regions);
+        // if (!await context.VehicleLocationAreas.AnyAsync())
+        //     await context.VehicleLocationAreas.AddRangeAsync(areas);
 
         var towns = SeedTowns.GetTowns(regions, areas, locationsData);
+        // if (!await context.VehicleLocationTowns.AnyAsync())
+        //     await context.VehicleLocationTowns.AddRangeAsync(towns);
 
-        // var models = SeedModels.GetModels(brands, types);
+        var models = SeedModels.GetModels(
+            brands, types, engineTypes, bodyTypes, drivetrainTypes, transmissionTypes);
         // if (!await context.VehicleModels.AnyAsync()) 
         //     await context.VehicleModels.AddRangeAsync(models);
         //
