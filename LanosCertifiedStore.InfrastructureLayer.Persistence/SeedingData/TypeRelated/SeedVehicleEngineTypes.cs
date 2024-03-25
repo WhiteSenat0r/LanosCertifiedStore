ccee80c795ce5033b8624086ee6d1125bba6628f
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-25 12:32:57 +0200

    Added SeedVehicleEngineTypes.cs

---
+++ LanosCertifiedStore.InfrastructureLayer.Persistence/SeedingData/TypeRelated/SeedVehicleEngineTypes.cs
@@ -0,0 +1,19 @@
+﻿using Persistence.DataModels.VehicleRelated.TypeRelated;
+
+namespace Persistence.SeedingData.TypeRelated;
+
+internal static class SeedVehicleEngineTypes
+{
+    public static List<VehicleEngineTypeDataModel> GetVehicleEngineTypes() =>
+    [
+        new VehicleEngineTypeDataModel("Бензиновий"),
+        new VehicleEngineTypeDataModel("Бензиновий / газовий (метан)"),
+        new VehicleEngineTypeDataModel("Бензиновий / газовий (пропан-бутан)"),
+        new VehicleEngineTypeDataModel("Дизельний"),
+        new VehicleEngineTypeDataModel("Гібридний (HEV)"),
+        new VehicleEngineTypeDataModel("Гібридний (MHEV)"),
+        new VehicleEngineTypeDataModel("Гібридний (PHEV)"),
+        new VehicleEngineTypeDataModel("Газовий"),
+        new VehicleEngineTypeDataModel("Електро"),
+    ];
+}
