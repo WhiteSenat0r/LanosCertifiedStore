7d34e0ed2ecaa85e2e418865b18c1ee502cd81f8
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-27 19:36:44 +0200

    Added location-related mapping profiles

---
+++ LanosCertifiedStore.InfrastructureLayer.Persistence/MappingHelpers/VehicleRelated/LocationRelated/Common/Classes/LocationRelatedMappingProfileBase.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using Domain.Entities.VehicleRelated.Classes.Common.Classes;
+
+namespace Persistence.MappingHelpers.VehicleRelated.LocationRelated.Common.Classes;
+
+public abstract class LocationRelatedMappingProfileBase : Profile
+{
+    private protected ICollection<T> MapObjectRange<T>(IEnumerable<T> objects)
+        where T : NamedVehicleAspect, new() => 
+        objects.Select(t => new T { Id = t.Id }).ToList();
+}
---
+++ LanosCertifiedStore.InfrastructureLayer.Persistence/MappingHelpers/VehicleRelated/LocationRelated/RegionMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities.VehicleRelated.Classes.LocationRelated;
+using Persistence.DataModels.VehicleRelated.LocationRelated;
+using Persistence.MappingHelpers.VehicleRelated.LocationRelated.Common.Classes;
+
+namespace Persistence.MappingHelpers.VehicleRelated.LocationRelated;
+
+public sealed class RegionMappingProfile : LocationRelatedMappingProfileBase
+{
+    public RegionMappingProfile()
+    {
+        AddMappingProfileFromModelToEntity();
+        AddMappingProfileFromEntityToModel();
+    }
+    
+    private void AddMappingProfileFromModelToEntity() =>
+        CreateMap<VehicleLocationRegionDataModel, VehicleLocationRegion>();
+
+    private void AddMappingProfileFromEntityToModel() =>
+        CreateMap<VehicleLocationArea, VehicleLocationAreaDataModel>()
+            .ForMember(d => d.LocationRegionId, o => o.MapFrom(x => x.LocationRegion.Id))
+            .ForMember(d => d.LocationRegion, o => o.Ignore())
+            .ForMember(d => d.RelatedTowns, o => o.MapFrom(x => MapObjectRange(x.RelatedTowns)));
+}
