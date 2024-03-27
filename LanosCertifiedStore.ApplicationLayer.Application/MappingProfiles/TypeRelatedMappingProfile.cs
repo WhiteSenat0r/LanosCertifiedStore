614fe6a2ce1c02eb10056c46a32dea2f8ed66ad0
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-27 18:46:46 +0200

    Added a separate type-DTO mapping profile

---
+++ LanosCertifiedStore.ApplicationLayer.Application/MappingProfiles/TypeRelatedMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using Application.Dtos.TypeDtos;
+using AutoMapper;
+using Domain.Entities.VehicleRelated.Classes.TypeRelated;
+
+namespace Application.MappingProfiles;
+
+public sealed class TypeRelatedMappingProfile : Profile
+{
+    public TypeRelatedMappingProfile()
+    {
+        CreateMap<VehicleType, VehicleTypeDto>();
+        CreateMap<VehicleEngineType, VehicleEngineTypeDto>();
+        CreateMap<VehicleTransmissionType, VehicleTransmissionTypeDto>();
+        CreateMap<VehicleDrivetrainType, VehicleDrivetrainTypeDto>();
+        CreateMap<VehicleBodyType, VehicleBodyTypeDto>();
+    }
+}
