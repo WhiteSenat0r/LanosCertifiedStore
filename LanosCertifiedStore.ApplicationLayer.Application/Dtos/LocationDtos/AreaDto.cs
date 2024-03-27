ce045b986edba4f3d6e2afe8ff932474ae925e41
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-03-27 17:55:53 +0200

    Added location-related DTOs

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Dtos/LocationDtos/AreaDto.cs
@@ -0,0 +1,9 @@
+﻿using Application.Dtos.Common;
+
+namespace Application.Dtos.LocationDtos;
+
+public sealed record AreaDto : VehicleAspectDto
+{
+    public string? RelatedRegion { get; init; }
+    public ICollection<string>? RelatedTowns { get; init; }
+}
---
+++ LanosCertifiedStore.ApplicationLayer.Application/Dtos/LocationDtos/RegionDto.cs
@@ -0,0 +1,9 @@
+﻿using Application.Dtos.Common;
+
+namespace Application.Dtos.LocationDtos;
+
+public sealed record RegionDto : VehicleAspectDto
+{
+    public ICollection<string>? RelatedAreas { get; init; }
+    public ICollection<string>? RelatedTowns { get; init; }
+}
