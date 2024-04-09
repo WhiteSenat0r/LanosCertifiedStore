0df13119fb4651a03caa51b6e831de49547a9013
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-04-09 15:36:25 +0300

    Added IVehicleEngineTypeFilteringRequestParameters.cs and VehicleEngineTypeFilteringRequestParameters.cs

---
+++ LanosCertifiedStore.ApplicationLayer.Application/RequestParams/TypeRelated/VehicleEngineTypeFilteringRequestParameters.cs
@@ -0,0 +1,13 @@
+﻿using Application.RequestParams.Common.Classes;
+using Domain.Contracts.RequestParametersRelated.TypeRelated;
+using Domain.Entities.VehicleRelated.Classes.TypeRelated;
+using Domain.Enums.RequestParametersRelated.TypeRelated;
+
+namespace Application.RequestParams.TypeRelated;
+
+public sealed class VehicleEngineTypeFilteringRequestParameters : BaseFilteringRequestParameters<VehicleEngineType>,
+    IVehicleEngineTypeFilteringRequestParameters
+{
+    public string? Name { get; set; }
+    public VehicleEngineTypeSelectionProfile SelectionProfile { get; } = VehicleEngineTypeSelectionProfile.Default;
+}
