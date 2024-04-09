c655a49c8b951aa61b2f7830ef6d036a6d0fe86f
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-04-09 15:37:09 +0300

    Added VehicleModelRepository.cs and related classes

---
+++ LanosCertifiedStore.InfrastructureLayer.Persistence/Repositories/TypeRelated/VehicleEngineTypeRelated/QueryBuilderRelated/Common/Classes/VehicleEngineTypeSortingSettings.cs
@@ -0,0 +1,6 @@
+﻿using Persistence.DataModels.VehicleRelated.TypeRelated;
+using Persistence.QueryBuilder.Common;
+
+namespace Persistence.Repositories.TypeRelated.VehicleEngineTypeRelated.QueryBuilderRelated.Common.Classes;
+
+internal class VehicleEngineTypeSortingSettings : BaseSortingSettings<VehicleEngineTypeDataModel>;
