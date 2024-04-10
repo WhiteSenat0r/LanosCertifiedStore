75d9f9f65a762c6ff021314d9604cf1f54cc98f4
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-04-10 11:46:05 +0300

    Added IVehicleDrivetrainTypeFilteringRequestParameters.cs and VehicleDrivetrainTypeFilteringRequestParameters.cs

---
+++ LanosCertifiedStore.ApplicationLayer.Application/RequestParams/TypeRelated/VehicleDrivetrainTypeFilteringRequestParameters.cs
@@ -0,0 +1,14 @@
+﻿using Application.RequestParams.Common.Classes;
+using Domain.Contracts.RequestParametersRelated.TypeRelated;
+using Domain.Entities.VehicleRelated.Classes.TypeRelated;
+using Domain.Enums.RequestParametersRelated.TypeRelated;
+
+namespace Application.RequestParams.TypeRelated;
+
+public sealed class VehicleDrivetrainTypeFilteringRequestParameters : 
+    BaseFilteringRequestParameters<VehicleDrivetrainType>, IVehicleDrivetrainTypeFilteringRequestParameters
+{
+    public string? Name { get; set; }
+    public VehicleDrivetrainTypeSelectionProfile SelectionProfile { get; } =
+        VehicleDrivetrainTypeSelectionProfile.Default;
+}
