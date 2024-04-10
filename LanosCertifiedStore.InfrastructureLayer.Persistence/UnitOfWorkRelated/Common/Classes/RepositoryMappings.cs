996fad5016d2b3ca74cfa3a72a2b0ac3ba223242
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-04-10 12:45:48 +0300

    Updated repository mappings

--- LanosCertifiedStore.InfrastructureLayer.Persistence/UnitOfWorkRelated/Common/Classes/RepositoryMappings.cs
+++ LanosCertifiedStore.InfrastructureLayer.Persistence/UnitOfWorkRelated/Common/Classes/RepositoryMappings.cs
@@ -1,6 +1,10 @@
 ﻿using Domain.Contracts.RepositoryRelated.Common;
 using Domain.Entities.VehicleRelated.Classes;
 using Domain.Entities.VehicleRelated.Classes.TypeRelated;
+using Persistence.Repositories.TypeRelated.VehicleBodyTypeRelated;
+using Persistence.Repositories.TypeRelated.VehicleDrivetrainTypeRelated;
+using Persistence.Repositories.TypeRelated.VehicleEngineTypeRelated;
+using Persistence.Repositories.TypeRelated.VehicleTransmissionTypeRelated;
 using Persistence.Repositories.TypeRelated.VehicleTypeRelated;
 using Persistence.Repositories.VehicleBrandRelated;
 using Persistence.Repositories.VehicleColorRelated;
@@ -19,6 +23,10 @@ internal abstract class RepositoryMappings
         { typeof(IRepository<VehicleBrand>), typeof(VehicleBrandRepository) },
         { typeof(IRepository<VehicleModel>), typeof(VehicleModelRepository) },
         { typeof(IRepository<VehicleType>), typeof(VehicleTypeRepository) },
+        { typeof(IRepository<VehicleEngineType>), typeof(VehicleEngineTypeRepository) },
+        { typeof(IRepository<VehicleTransmissionType>), typeof(VehicleTransmissionTypeRepository) },
+        { typeof(IRepository<VehicleDrivetrainType>), typeof(VehicleDrivetrainTypeRepository) },
+        { typeof(IRepository<VehicleBodyType>), typeof(VehicleBodyTypeRepository) },
         { typeof(IRepository<VehicleColor>), typeof(VehicleColorRepository) },
         { typeof(IRepository<VehiclePrice>), typeof(VehiclePriceRepository) },
         { typeof(IRepository<VehicleImage>), typeof(VehicleImageRepository) },
