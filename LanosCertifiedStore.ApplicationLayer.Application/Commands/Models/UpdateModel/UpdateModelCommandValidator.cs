acdf8f51651de2331f495d6c7da8d85fbf4a3433
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-04-17 17:01:23 +0300

    Updated UpdateModelCommandValidator.cs

--- LanosCertifiedStore.ApplicationLayer.Application/Commands/Models/UpdateModel/UpdateModelCommandValidator.cs
+++ LanosCertifiedStore.ApplicationLayer.Application/Commands/Models/UpdateModel/UpdateModelCommandValidator.cs
@@ -22,5 +22,32 @@ internal sealed class UpdateModelCommandValidator : AbstractValidator<UpdateMode
         RuleFor(x => x.Name)
             .MustAsync(async (name, _) => await validationHelper.IsNameUniqueAsync(name))
             .WithMessage("Model with such name already exists! Model name must be unique");
+        
+        RuleFor(x => x.TypeId)
+            .NotEmpty();
+        
+        RuleFor(x => x.AvailableBodyTypeIds)
+            .NotNull()
+            .NotEmpty();
+        
+        RuleFor(x => x.AvailableEngineTypeIds)
+            .NotNull()
+            .NotEmpty();
+        
+        RuleFor(x => x.AvailableTransmissionTypeIds)
+            .NotNull()
+            .NotEmpty();
+        
+        RuleFor(x => x.AvailableDrivetrainTypeIds)
+            .NotNull()
+            .NotEmpty();
+        
+        RuleFor(x => x.MinimalProductionYear)
+            .LessThanOrEqualTo(x => x.MaximumProductionYear)
+            .When(x => x.MaximumProductionYear is not null);
+        
+        RuleFor(x => x.MaximumProductionYear)
+            .GreaterThanOrEqualTo(x => x.MinimalProductionYear)
+            .When(x => x.MaximumProductionYear is not null);
     }
 }
