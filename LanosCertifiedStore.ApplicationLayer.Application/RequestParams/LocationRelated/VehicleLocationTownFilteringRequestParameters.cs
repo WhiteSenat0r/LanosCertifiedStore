5dd3dc7f9fc8d2bd53541cabd9bc8e7926eb4b0c
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-04-23 11:12:16 +0300

    Added location-related selection profile classes

---
+++ LanosCertifiedStore.ApplicationLayer.Application/RequestParams/LocationRelated/VehicleLocationTownFilteringRequestParameters.cs
@@ -0,0 +1,14 @@
+﻿using Application.RequestParams.Common.Classes;
+using Domain.Contracts.RequestParametersRelated.LocationRelated;
+using Domain.Entities.VehicleRelated.Classes.LocationRelated;
+using Domain.Enums.RequestParametersRelated.LocationRelated;
+
+namespace Application.RequestParams.LocationRelated;
+
+public sealed class VehicleLocationTownFilteringRequestParameters : 
+    BaseFilteringRequestParameters<VehicleLocationTown>,
+    IVehicleLocationTownFilteringRequestParameters
+{
+    public string? Name { get; set; }
+    public VehicleLocationTownSelectionProfile SelectionProfile => VehicleLocationTownSelectionProfile.Default;
+}
