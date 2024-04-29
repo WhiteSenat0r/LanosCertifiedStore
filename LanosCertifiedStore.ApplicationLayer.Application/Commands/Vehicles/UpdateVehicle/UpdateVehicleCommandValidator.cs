b549506365bbb727fec31cab90caddb95fac8b9b
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-04-29 09:24:27 +0300

    Updated vehicle-related command validation

--- LanosCertifiedStore.ApplicationLayer.Application/Commands/Vehicles/UpdateVehicle/UpdateVehicleCommandValidator.cs
+++ LanosCertifiedStore.ApplicationLayer.Application/Commands/Vehicles/UpdateVehicle/UpdateVehicleCommandValidator.cs
@@ -1,12 +1,15 @@
-﻿using Domain.Contracts.RepositoryRelated.Common;
+﻿using Application.Helpers.ValidationRelated.Common.Contracts;
+using Domain.Contracts.RepositoryRelated.Common;
 using Domain.Entities.VehicleRelated.Classes;
+using Domain.Entities.VehicleRelated.Classes.LocationRelated;
+using Domain.Entities.VehicleRelated.Classes.TypeRelated;
 using FluentValidation;
 
 namespace Application.Commands.Vehicles.UpdateVehicle;
 
 internal sealed class UpdateVehicleCommandValidator : AbstractValidator<UpdateVehicleCommand>
 {
-    public UpdateVehicleCommandValidator(IUnitOfWork unitOfWork)
+    public UpdateVehicleCommandValidator(IUnitOfWork unitOfWork, IValidationHelper validationHelper)
     {
         RuleFor(x => x.Description)
             .NotEmpty()
@@ -17,31 +20,89 @@ internal sealed class UpdateVehicleCommandValidator : AbstractValidator<UpdateVe
             .NotEmpty()
             .GreaterThan(0);
         
-        VehicleModel? model = null;
+        RuleFor(x => x.Displacement)
+            .NotEmpty()
+            .GreaterThan(0);
 
         RuleFor(x => x.ModelId)
             .NotEmpty()
-            .MustAsync(async (modelId, _) =>
-            {
-                model = await unitOfWork.RetrieveRepository<VehicleModel>().GetEntityByIdAsync(modelId);
-
-                return model is not null;
-            })
+            .MustAsync(async (modelId, _) => 
+                await validationHelper.CheckMainAspectPresence<VehicleModel>(unitOfWork, modelId))
             .WithMessage("Such model doesn't exists!");
-
-        // TODO
-        // RuleFor(x => x.TypeId)
-        //     .NotEmpty()
-        //     .Must(typeId =>
-        //     {
-        //         if (model is not null)
-        //             return model.AvailableTypes
-        //                 .Select(x => x.Id)
-        //                 .ToList()
-        //                 .Contains(typeId);
-        //
-        //         return false;
-        //     })
-        //     .WithMessage("This type is not available for this model!");
+        
+        RuleFor(x => new { x.ModelId, x.BodyTypeId })
+            .NotEmpty()
+            .MustAsync(async (ids, _) => 
+                await validationHelper.CheckSecondaryAspectPresence<VehicleModel, VehicleBodyType>(
+                    unitOfWork, (ids.ModelId, ids.BodyTypeId), m => m.AvailableBodyTypes.Select(i => i.Id)))
+            .WithMessage("Such body type is not contained by the given model!");
+        
+        RuleFor(x => new { x.ModelId, x.EngineTypeId })
+            .NotEmpty()
+            .MustAsync(async (ids, _) => 
+                await validationHelper.CheckSecondaryAspectPresence<VehicleModel, VehicleEngineType>(
+                    unitOfWork, (ids.ModelId, ids.EngineTypeId), m => m.AvailableEngineTypes.Select(i => i.Id)))
+            .WithMessage("Such engine type is not contained by the given model!");
+        
+        RuleFor(x => new { x.ModelId, x.TransmissionTypeId })
+            .NotEmpty()
+            .MustAsync(async (ids, _) => 
+                await validationHelper.CheckSecondaryAspectPresence<VehicleModel, VehicleTransmissionType>(
+                    unitOfWork, (ids.ModelId, ids.TransmissionTypeId),
+                    m => m.AvailableTransmissionTypes.Select(i => i.Id)))
+            .WithMessage("Such transmission type is not contained by the given model!");
+        
+        RuleFor(x => new { x.ModelId, x.DrivetrainTypeId })
+            .NotEmpty()
+            .MustAsync(async (ids, _) => 
+                await validationHelper.CheckSecondaryAspectPresence<VehicleModel, VehicleDrivetrainType>(
+                    unitOfWork, (ids.ModelId, ids.DrivetrainTypeId), m => m.AvailableDrivetrainTypes.Select(i => i.Id)))
+            .WithMessage("Such drivetrain type is not contained by the given model!");
+        
+        RuleFor(x => x.LocationRegionId)
+            .NotEmpty()
+            .MustAsync(async (regionId, _) => 
+                await validationHelper.CheckMainAspectPresence<VehicleLocationRegion>(unitOfWork, regionId))
+            .WithMessage("Such region doesn't exists!");
+        
+        RuleFor(x => x.LocationAreaId)
+            .NotEmpty()
+            .MustAsync(async (areaId, _) => 
+                await validationHelper.CheckMainAspectPresence<VehicleLocationArea>(unitOfWork, areaId))
+            .WithMessage("Such area doesn't exists!");
+        
+        RuleFor(x => x.LocationTownId)
+            .NotEmpty()
+            .MustAsync(async (townId, _) => 
+                await validationHelper.CheckMainAspectPresence<VehicleLocationTown>(unitOfWork, townId))
+            .WithMessage("Such town doesn't exists!");
+        
+        RuleFor(x => new { x.LocationRegionId, x.LocationAreaId })
+            .NotEmpty()
+            .MustAsync(async (ids, _) => 
+                await validationHelper.CheckSecondaryAspectPresence<VehicleLocationRegion, VehicleLocationArea>(
+                    unitOfWork, (ids.LocationRegionId, ids.LocationAreaId), r => r.RelatedAreas.Select(i => i.Id)))
+            .WithMessage("Such area is not contained by the given region!");
+        
+        RuleFor(x => new { x.LocationRegionId, x.LocationTownId })
+            .NotEmpty()
+            .MustAsync(async (ids, _) => 
+                await validationHelper.CheckSecondaryAspectPresence<VehicleLocationRegion, VehicleLocationTown>(
+                    unitOfWork, (ids.LocationRegionId, ids.LocationTownId), r => r.RelatedTowns.Select(i => i.Id)))
+            .WithMessage("Such town is not contained by the given region!");
+        
+        RuleFor(x => new { x.LocationRegionId, x.LocationTownId })
+            .NotEmpty()
+            .MustAsync(async (ids, _) => 
+                await validationHelper.CheckSecondaryAspectPresence<VehicleLocationTown, VehicleLocationRegion>(
+                    unitOfWork, (ids.LocationRegionId, ids.LocationTownId), r => r.LocationRegion.Id))
+            .WithMessage("Such region is not related to the given town!");
+        
+        RuleFor(x => new { x.LocationTownId, x.LocationAreaId })
+            .NotEmpty()
+            .MustAsync(async (ids, _) => 
+                await validationHelper.CheckSecondaryAspectPresence<VehicleLocationTown, VehicleLocationRegion>(
+                    unitOfWork, (ids.LocationTownId, ids.LocationAreaId), r => r.LocationArea.Id))
+            .WithMessage("Such area is not related to the given town!");
     }
 }
