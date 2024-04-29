0738630560ece2c8559fbd8afffe494756a66be0
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-04-29 09:23:51 +0300

    Updated ActionVehicleCommandHandlerBase.cs and IActionVehicleCommandBase.cs

--- LanosCertifiedStore.ApplicationLayer.Application/Commands/Vehicles/Common/ActionVehicleCommandHandlerBase.cs
+++ LanosCertifiedStore.ApplicationLayer.Application/Commands/Vehicles/Common/ActionVehicleCommandHandlerBase.cs
@@ -2,6 +2,7 @@
 using Domain.Contracts.Common;
 using Domain.Contracts.RepositoryRelated.Common;
 using Domain.Entities.VehicleRelated.Classes;
+using Domain.Entities.VehicleRelated.Classes.LocationRelated;
 using Domain.Entities.VehicleRelated.Classes.TypeRelated;
 using Domain.Shared;
 
@@ -13,36 +14,69 @@ internal abstract class ActionVehicleCommandHandlerBase<TResult>(
 {
     private protected async Task<Result<Vehicle>> CreateVehicleInstance(IActionVehicleCommandBase request)
     {
-        var vehicleModel = await GetAspectDataAsync<VehicleModel>(request.ModelId);
-        if (vehicleModel is null) return GetFailureResult("Model");
+        var aspects = await GetVehicleAspects(request);
 
-        var vehicleColor = await GetAspectDataAsync<VehicleColor>(request.ColorId);
-        if (vehicleColor is null) return GetFailureResult("Color");
-
-        var vehicleType = await GetAspectDataAsync<VehicleType>(request.TypeId);
-        if (vehicleType is null) return GetFailureResult("Type");
+        foreach (var aspect in aspects.Where(aspect => aspect.Value is null))
+            return GetFailureResult(aspect.Key);
 
         var vehicle = GetVehicleInstanceByCommandData(
-            vehicleModel.Brand, vehicleModel, vehicleColor, vehicleType, request);
+            (aspects["Model"] as VehicleModel)!,
+            (aspects["Color"] as VehicleColor)!,
+            (aspects["Body type"] as VehicleBodyType)!,
+            (aspects["Engine type"] as VehicleEngineType)!,
+            (aspects["Transmission type"] as VehicleTransmissionType)!,
+            (aspects["Drivetrain type"] as VehicleDrivetrainType)!,
+            (aspects["Region"] as VehicleLocationRegion)!,
+            (aspects["Area"] as VehicleLocationArea)!,
+            (aspects["Town"] as VehicleLocationTown)!,
+            request);
 
         return Result<Vehicle>.Success(vehicle);
     }
 
+    private async Task<Dictionary<string, IIdentifiable<Guid>?>> GetVehicleAspects(
+        IActionVehicleCommandBase request) =>
+        new()
+        {
+            { "Model", await GetAspectDataAsync<VehicleModel>(request.ModelId) },
+            { "Color", await GetAspectDataAsync<VehicleColor>(request.ColorId) },
+            { "Body type", await GetAspectDataAsync<VehicleBodyType>(request.BodyTypeId) },
+            { "Engine type", await GetAspectDataAsync<VehicleEngineType>(request.EngineTypeId) },
+            { "Transmission type", await GetAspectDataAsync<VehicleTransmissionType>(request.TransmissionTypeId) },
+            { "Drivetrain type", await GetAspectDataAsync<VehicleDrivetrainType>(request.DrivetrainTypeId) },
+            { "Region", await GetAspectDataAsync<VehicleLocationRegion>(request.LocationRegionId) },
+            { "Area", await GetAspectDataAsync<VehicleLocationArea>(request.LocationAreaId) },
+            { "Town", await GetAspectDataAsync<VehicleLocationTown>(request.LocationTownId) },
+        };
+
     private Vehicle GetVehicleInstanceByCommandData(
-        VehicleBrand vehicleBrand, 
         VehicleModel vehicleModel,
-        VehicleColor vehicleColor, 
-        VehicleType vehicleType, 
+        VehicleColor vehicleColor,
+        VehicleBodyType vehicleBodyType,
+        VehicleEngineType vehicleEngineType,
+        VehicleTransmissionType vehicleTransmissionType,
+        VehicleDrivetrainType vehicleDrivetrainType,
+        VehicleLocationRegion vehicleLocationRegion,
+        VehicleLocationArea vehicleLocationArea,
+        VehicleLocationTown vehicleLocationTown,
         IActionVehicleCommandBase vehicleData) =>
         new(
-            // TODO
-            // brand: vehicleBrand,
-            // model: vehicleModel,
-            // color: vehicleColor,
-            // vehicleType: vehicleType,
-            // price: vehicleData.Price,
-            // displacement: vehicleData.Displacement,
-            // description: vehicleData.Description
+            brand: vehicleModel.Brand,
+            model: vehicleModel,
+            color: vehicleColor,
+            vehicleType: vehicleModel.VehicleType,
+            price: vehicleData.Price,
+            displacement: vehicleData.Displacement,
+            description: vehicleData.Description,
+            bodyType: vehicleBodyType,
+            engineType: vehicleEngineType,
+            transmissionType: vehicleTransmissionType,
+            drivetrainType: vehicleDrivetrainType,
+            locationArea: vehicleLocationArea,
+            locationRegion: vehicleLocationRegion,
+            locationTown: vehicleLocationTown,
+            mileage: vehicleData.Mileage,
+            productionYear: vehicleData.ProductionYear
         );
 
     private Task<TAspect?> GetAspectDataAsync<TAspect>(Guid id)
