f69f97265b4d5c9a73e9402eb7898f889ce367aa
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-05-25 21:11:51 +0300

    Added CountBodyTypesQuery and its handler

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Queries/Types/VehicleBodyTypeRelated/CountBodyTypesQueryRelated/CountBodyTypesQuery.cs
@@ -0,0 +1,11 @@
+﻿using Application.Dtos.Common;
+using Application.Queries.Common.CountItemsQueryRelated;
+using Domain.Contracts.RepositoryRelated.Common;
+using Domain.Entities.VehicleRelated.Classes.TypeRelated;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Queries.Types.VehicleBodyTypeRelated.CountBodyTypesQueryRelated;
+
+public sealed record CountBodyTypesQuery(IFilteringRequestParameters<VehicleEngineType> RequestParameters) : 
+    CountItemsQueryBase<VehicleEngineType>(RequestParameters), IRequest<Result<ItemsCountDto>>;
---
+++ LanosCertifiedStore.ApplicationLayer.Application/Queries/Types/VehicleBodyTypeRelated/CountBodyTypesQueryRelated/CountBodyTypesQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using Application.Dtos.Common;
+using Application.Queries.Common.CountItemsQueryRelated;
+using Application.Queries.Types.VehicleEngineTypeRelated.CountEngineTypesQueryRelated;
+using Domain.Contracts.RepositoryRelated.Common;
+using Domain.Entities.VehicleRelated.Classes.TypeRelated;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Queries.Types.VehicleBodyTypeRelated.CountBodyTypesQueryRelated;
+
+internal sealed class CountBodyTypesQueryHandler(IUnitOfWork unitOfWork) : 
+    CountItemsQueryHandlerBase<VehicleEngineType>(unitOfWork),
+    IRequestHandler<CountEngineTypesQuery, Result<ItemsCountDto>>
+{
+    public Task<Result<ItemsCountDto>> Handle(
+        CountEngineTypesQuery request, CancellationToken cancellationToken) =>
+        base.Handle(request, cancellationToken);
+}
