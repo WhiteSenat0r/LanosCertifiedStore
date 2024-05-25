61f89d5cde8f7552cb54ab3b9284695c00ff647e
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-05-25 21:10:50 +0300

    Added CountEngineTypesQuery and its handler

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Queries/Types/VehicleEngineTypeRelated/CountEngineTypesQueryRelated/CountEngineTypesQuery.cs
@@ -0,0 +1,11 @@
+﻿using Application.Dtos.Common;
+using Application.Queries.Common.CountItemsQueryRelated;
+using Domain.Contracts.RepositoryRelated.Common;
+using Domain.Entities.VehicleRelated.Classes.TypeRelated;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Queries.Types.VehicleEngineTypeRelated.CountEngineTypesQueryRelated;
+
+public sealed record CountEngineTypesQuery(IFilteringRequestParameters<VehicleEngineType> RequestParameters) : 
+    CountItemsQueryBase<VehicleEngineType>(RequestParameters), IRequest<Result<ItemsCountDto>>;
