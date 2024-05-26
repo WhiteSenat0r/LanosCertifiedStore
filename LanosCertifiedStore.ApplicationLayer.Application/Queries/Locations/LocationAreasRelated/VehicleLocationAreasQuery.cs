45242b26a9d5f093cfdb49dd7d11108c218bc1dd
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-05-26 12:11:20 +0300

    Added VehicleLocationAreasQuery and its handler

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Queries/Locations/LocationAreasRelated/VehicleLocationAreasQuery.cs
@@ -0,0 +1,13 @@
+﻿using Application.Core.Results;
+using Application.Dtos.LocationDtos;
+using Application.Queries.Common.QueryRelated;
+using Domain.Contracts.RepositoryRelated.Common;
+using Domain.Entities.VehicleRelated.Classes.LocationRelated;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Queries.Locations.LocationAreasRelated;
+
+public sealed record VehicleLocationAreasQuery(IFilteringRequestParameters<VehicleLocationArea> RequestParameters) :
+    ListQueryBase<VehicleLocationArea, IFilteringRequestParameters<VehicleLocationArea>>(RequestParameters),
+    IRequest<Result<PaginationResult<AreaDto>>>;
