0febfa2911136af8b47d06cd54d897204324be98
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-05-26 12:10:39 +0300

    Added CountTownsQuery and its handler

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Queries/Locations/LocationTownsRelated/CountAreasQueryRelated/CountTownsQuery.cs
@@ -0,0 +1,11 @@
+﻿using Application.Dtos.Common;
+using Application.Queries.Common.CountItemsQueryRelated;
+using Domain.Contracts.RepositoryRelated.Common;
+using Domain.Entities.VehicleRelated.Classes.LocationRelated;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Queries.Locations.LocationTownsRelated.CountAreasQueryRelated;
+
+public sealed record CountTownsQuery(IFilteringRequestParameters<VehicleLocationTown> RequestParameters) : 
+    CountItemsQueryBase<VehicleLocationTown>(RequestParameters), IRequest<Result<ItemsCountDto>>;
---
+++ LanosCertifiedStore.ApplicationLayer.Application/Queries/Locations/LocationTownsRelated/CountAreasQueryRelated/CountTownsQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using Application.Dtos.Common;
+using Application.Queries.Common.CountItemsQueryRelated;
+using Domain.Contracts.RepositoryRelated.Common;
+using Domain.Entities.VehicleRelated.Classes.LocationRelated;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Queries.Locations.LocationTownsRelated.CountAreasQueryRelated;
+
+internal sealed class CountTownsQueryHandler(IUnitOfWork unitOfWork) : 
+    CountItemsQueryHandlerBase<VehicleLocationTown>(unitOfWork),
+    IRequestHandler<CountTownsQuery, Result<ItemsCountDto>>
+{
+    public Task<Result<ItemsCountDto>> Handle(
+        CountTownsQuery request, CancellationToken cancellationToken) =>
+        base.Handle(request, cancellationToken);
+}
