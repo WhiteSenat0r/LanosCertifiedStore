a1e2589fbcca28c478e8eaf3927bcce34158c38a
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-05-26 10:09:02 +0300

    Added VehicleDrivetrainTypesQuery and its handler

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Queries/Types/VehicleDrivetrainTypeRelated/VehicleDrivetrainTypesQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Application.Core.Results;
+using Application.Dtos.TypeDtos;
+using Application.Queries.Common.QueryRelated;
+using AutoMapper;
+using Domain.Contracts.RepositoryRelated.Common;
+using Domain.Entities.VehicleRelated.Classes.TypeRelated;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Queries.Types.VehicleDrivetrainTypeRelated;
+
+internal sealed class VehicleDrivetrainTypesQueryHandler(IUnitOfWork unitOfWork, IMapper mapper) :
+    ListQueryHandlerBase<VehicleDrivetrainType,
+        IFilteringRequestParameters<VehicleDrivetrainType>,
+        VehicleDrivetrainTypeDto>(unitOfWork, mapper),
+    IRequestHandler<VehicleDrivetrainTypesQuery, Result<PaginationResult<VehicleDrivetrainTypeDto>>>
+{
+    public Task<Result<PaginationResult<VehicleDrivetrainTypeDto>>> Handle(VehicleDrivetrainTypesQuery request,
+        CancellationToken cancellationToken) =>
+        base.Handle(request, cancellationToken);
+}
