3811e4336c9dd664e5f7068801f24c7179b93986
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-05-26 12:36:32 +0300

    Added vehicle body type related commands

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Commands/Types/VehicleBodyTypesRelated/CreateBodyType/CreateBodyTypeCommand.cs
@@ -0,0 +1,6 @@
+﻿using Domain.Shared;
+using MediatR;
+
+namespace Application.Commands.Types.VehicleBodyTypesRelated.CreateBodyType;
+
+public sealed record CreateBodyTypeCommand(string Name) : IRequest<Result<Unit>>;
---
+++ LanosCertifiedStore.ApplicationLayer.Application/Commands/Types/VehicleBodyTypesRelated/UpdateBodyType/UpdateBodyTypeCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using Application.Commands.Common;
+using Application.Commands.Types.VehicleTypesRelated.UpdateType;
+using Domain.Contracts.RepositoryRelated.Common;
+using Domain.Entities.VehicleRelated.Classes.TypeRelated;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Commands.Types.VehicleBodyTypesRelated.UpdateBodyType;
+
+internal sealed class UpdateBodyTypeCommandHandler : 
+    CommandHandlerBase<Unit>, IRequestHandler<UpdateTypeCommand, Result<Unit>>
+{
+    public UpdateBodyTypeCommandHandler(IUnitOfWork unitOfWork) : base(unitOfWork)
+    {
+        PossibleErrors =
+        [
+            new Error("UpdateBodyTypeError", "Saving the updated body type was not successful!"),
+            new Error("UpdateBodyTypeError", "Error occured during the body type update!")
+        ];
+    }
+
+    public async Task<Result<Unit>> Handle(UpdateTypeCommand request, CancellationToken cancellationToken)
+    {
+        var bodyTypeRepository = GetRequiredRepository<VehicleBodyType>();
+        var existingBodyType = await bodyTypeRepository.GetEntityByIdAsync(request.Id);
+
+        if (existingBodyType is null) return Result<Unit>.Failure(Error.NotFound);
+
+        UpdateBodyType(request.UpdatedName, existingBodyType, bodyTypeRepository);
+
+        return await TrySaveChanges(cancellationToken);
+    }
+
+    private void UpdateBodyType(string updatedName, 
+        VehicleBodyType existingType, IRepository<VehicleBodyType> bodyTypeRepository)
+    {
+        existingType.Name = updatedName;
+
+        bodyTypeRepository.UpdateExistingEntityAsync(existingType);
+    }
+}
