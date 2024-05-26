cee41ccbea7992dbaa4498c39c167c0e79b5ebc4
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-05-26 12:11:29 +0300

    Added CountAreasQuery and its handler

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Queries/Locations/LocationAreasRelated/CountAreasQueryRelated/CountAreasQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using Application.Dtos.Common;
+using Application.Queries.Common.CountItemsQueryRelated;
+using Domain.Contracts.RepositoryRelated.Common;
+using Domain.Entities.VehicleRelated.Classes.LocationRelated;
+using Domain.Shared;
+using MediatR;
+
+namespace Application.Queries.Locations.LocationAreasRelated.CountAreasQueryRelated;
+
+internal sealed class CountAreasQueryHandler(IUnitOfWork unitOfWork) : 
+    CountItemsQueryHandlerBase<VehicleLocationArea>(unitOfWork),
+    IRequestHandler<CountAreasQuery, Result<ItemsCountDto>>
+{
+    public Task<Result<ItemsCountDto>> Handle(
+        CountAreasQuery request, CancellationToken cancellationToken) =>
+        base.Handle(request, cancellationToken);
+}
