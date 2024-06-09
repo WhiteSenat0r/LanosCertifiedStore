a51b9b76c11a6343af1ab0af421912703175394b
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-09 21:50:43 +0300

    Added location-related controllers

---
+++ LanosCertifiedStore.PresentationLayer.API/Controllers/LocationRelated/VehicleLocationAreasController.cs
@@ -0,0 +1,35 @@
+﻿using API.Controllers.Common;
+using API.Responses;
+using Application.Core.Results;
+using Application.Dtos.Common;
+using Application.Dtos.LocationDtos;
+using Application.Queries.Locations.LocationAreasRelated;
+using Application.Queries.Locations.LocationAreasRelated.CountAreasQueryRelated;
+using Application.RequestParams.LocationRelated;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.LocationRelated;
+
+public sealed class VehicleLocationAreasController : BaseEntityRelatedApiController
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(PaginationResult<AreaDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<PaginationResult<AreaDto>>> GetAreas(
+        [FromQuery] VehicleLocationAreaFilteringRequestParameters requestParameters)
+    {
+        return HandleResult(await Mediator.Send(new VehicleLocationAreasQuery(requestParameters)));
+    }
+    
+    [HttpGet("countItems")]
+    [ProducesResponseType(typeof(ItemsCountDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<ItemsCountDto>> GetItemsCount(
+        [FromQuery] VehicleLocationAreaFilteringRequestParameters requestParameters)
+    {
+        return HandleResult(await Mediator.Send(new CountAreasQuery(requestParameters)));
+    }
+    
+    // TODO Maybe add full CRUD in the future
+}
