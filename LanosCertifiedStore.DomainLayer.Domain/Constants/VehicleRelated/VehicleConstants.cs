fc2723d7a184e9f35667b2b5a52a277afc136078
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-14 16:26:33 +0300

    Added VehicleConstants.cs

---
+++ LanosCertifiedStore.DomainLayer.Domain/Constants/VehicleRelated/VehicleConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Constants.VehicleRelated;
+
+public static class VehicleConstants
+{
+    public const double MinimalDisplacementValue = 0;
+    public const int MinimalMileageValue = 0;
+    public const decimal MinimalPriceValue = 0;
+    public const int MinimalDescriptionLength = 20;
+    public const int MaximumDescriptionLength = 2048;
+}
