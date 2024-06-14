aa738313f307616c4b0d77e02b983e2f668788ac
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-14 18:16:05 +0300

    Added VehicleBrandConstants.cs

---
+++ LanosCertifiedStore.DomainLayer.Domain/Constants/VehicleRelated/VehicleBrandConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Constants.VehicleRelated;
+
+public static class VehicleBrandConstants
+{
+    public const int MinimalNameLength = 2;
+    public const int MaximumNameLength = 64;
+}
