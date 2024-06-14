a13afa9b572b2f487a6d6fa90af2c086a9c8e626
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-14 18:16:11 +0300

    Added VehicleColorConstants.cs

---
+++ LanosCertifiedStore.DomainLayer.Domain/Constants/VehicleRelated/VehicleColorConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Constants.VehicleRelated;
+
+public static class VehicleColorConstants
+{
+    public const int MinimalNameLength = 2;
+    public const int MaximumNameLength = 64;
+    public const int HexValueLength = 7;
+}
