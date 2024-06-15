0f2bfc69c8d8724bbc700940454487d55edf6070
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-15 10:59:53 +0300

    Added VehicleLocationRegionConstants.cs

---
+++ LanosCertifiedStore.DomainLayer.Domain/Constants/LocationRelated/VehicleLocationRegionConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Constants.LocationRelated;
+
+public static class VehicleLocationRegionConstants
+{
+    public const int MinimalNameLength = 2;
+    public const int MaximumNameLength = 64;
+}
