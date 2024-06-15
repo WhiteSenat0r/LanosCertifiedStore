e4441a3c09df7bf4191c34706adfbfac2578ac8c
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-15 12:20:57 +0300

    Added VehicleBrandHandlerMessages.cs

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Commands/Brands/Shared/VehicleBrandHandlerMessages.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Commands.Brands.Shared;
+
+public static class VehicleBrandHandlerMessages
+{
+    public const string FailedSavingNewBrandProcess = "Saving a new vehicle brand was not successful!";
+    public const string FailedCreationProcess = "Error occured during a new vehicle brand creation!";
+    public const string FailedSavingUpdatedBrandProcess = "Saving an updated vehicle brand was not successful!";
+    public const string FailedUpdateProcess = "Error occured during the vehicle brand update!";
+    public const string FailedSavingRemovedBrandProcess = "Vehicle brand removal was not successful!";
+    public const string FailedRemovalProcess = "Error occured during the vehicle brand removal!";
+}
