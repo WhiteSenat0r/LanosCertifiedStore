9de953492dee3c2b2696898a518c541b8e1a6410
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-15 12:20:46 +0300

    Added VehicleBrandHandlerErrorNames.cs

---
+++ LanosCertifiedStore.ApplicationLayer.Application/Commands/Brands/Shared/VehicleBrandHandlerErrorNames.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Commands.Brands.Shared;
+
+public static class VehicleBrandHandlerErrorNames
+{
+    public const string CreationError = "CreateBrandError";
+    public const string UpdateError = "UpdateBrandError";
+    public const string DeleteError = "DeleteBrandError";
+}
