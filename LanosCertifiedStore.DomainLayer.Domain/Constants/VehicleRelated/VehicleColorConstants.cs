e81427d8d5548eb23aa5655fcf17a414820dd9c6
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-18 11:16:33 +0300

    Replaced HexValueLength with HexPattern in VehicleColorConstants

--- LanosCertifiedStore.DomainLayer.Domain/Constants/VehicleRelated/VehicleColorConstants.cs
+++ LanosCertifiedStore.DomainLayer.Domain/Constants/VehicleRelated/VehicleColorConstants.cs
@@ -4,5 +4,5 @@ public static class VehicleColorConstants
 {
     public const int MinimalNameLength = 2;
     public const int MaximumNameLength = 64;
-    public const int HexValueLength = 7;
+    public const string HexPattern = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$";
 }
