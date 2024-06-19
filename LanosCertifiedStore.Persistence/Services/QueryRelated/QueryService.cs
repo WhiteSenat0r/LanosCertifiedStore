0be718b7c4f70bb505ba6d1e44708730997bf8f4
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-19 21:54:15 +0300

    Added QueryService.cs as a temporary template

---
+++ LanosCertifiedStore.Persistence/Services/QueryRelated/QueryService.cs
@@ -0,0 +1,19 @@
+﻿using Application.Contracts.RequestRelated;
+using Application.Contracts.ServicesRelated.RequestRelated;
+using Domain.Contracts.Common;
+using Persistence.Services.QueryRelated.Common.Contracts;
+
+namespace Persistence.Services.QueryRelated;
+
+internal sealed class QueryService(IQueryMappings queryMappings) : IQueryService
+{
+    public Task<TQueryResult> GetResult<TModel, TQueryResult, TRequestResult>(
+        IQueryRequest<TModel, TQueryResult, TRequestResult> queryRequest,
+        CancellationToken cancellationToken)
+        where TModel : IIdentifiable<Guid>
+        where TQueryResult : notnull
+        where TRequestResult : notnull
+    {
+        throw new NotImplementedException();
+    }
+}
