5deb4158990ea729a97188d83872b768c8f36015
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-19 21:56:31 +0300

    Added QueryPaginator.cs

---
+++ LanosCertifiedStore.Persistence/Queries/Common/Classes/QueryPaginator.cs
@@ -0,0 +1,16 @@
+﻿using Application.Contracts.RepositoryRelated.Common;
+using Domain.Contracts.Common;
+using Persistence.Queries.Common.Contracts;
+
+namespace Persistence.Queries.Common.Classes;
+
+internal sealed class QueryPaginator : IQueryPaginator
+{
+    public IQueryable<TEntity> ExecutePagination<TEntity, TModel>(
+        IQueryable<TEntity> queryable, IFilteringRequestParameters<TModel> filteringRequestParameters)
+        where TEntity : class, IIdentifiable<Guid>
+        where TModel : class, IIdentifiable<Guid> =>
+        queryable
+            .Skip((int)filteringRequestParameters.ItemQuantity * (filteringRequestParameters.PageIndex - 1))
+            .Take((int)filteringRequestParameters.ItemQuantity);
+}
