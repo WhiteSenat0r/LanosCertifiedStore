340812810f9782ec4e8021ed1d58ab9232b779e4
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-19 21:52:18 +0300

    Added request-related contracts

---
+++ LanosCertifiedStore.Application/Contracts/RequestRelated/ICommandRequest.cs
@@ -0,0 +1,9 @@
+﻿using Application.Shared;
+using Domain.Contracts.Common;
+using MediatR;
+
+namespace Application.Contracts.RequestRelated;
+
+public interface ICommandRequest<TModel, TResult> : IRequest<Result<TResult>>
+    where TModel : IIdentifiable<Guid>
+    where TResult : notnull;
---
+++ LanosCertifiedStore.Application/Contracts/RequestRelated/IQueryRequest.cs
@@ -0,0 +1,15 @@
+﻿using Application.Contracts.RepositoryRelated.Common;
+using Application.Shared;
+using Domain.Contracts.Common;
+using MediatR;
+
+namespace Application.Contracts.RequestRelated;
+
+public interface IQueryRequest<TModel, TQueryResult, TRequestResult> : IRequest<Result<TRequestResult>>
+    where TModel : IIdentifiable<Guid>
+    where TQueryResult : notnull
+    where TRequestResult : notnull
+{
+    IFilteringRequestParameters<TModel> FilteringParameters { get; }
+    bool IsTracked { get; }
+}
