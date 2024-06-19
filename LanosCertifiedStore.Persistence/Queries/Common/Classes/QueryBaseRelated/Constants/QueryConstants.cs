a7d5cba04ff097d6415117e5ae03607c5a355dae
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-19 21:56:21 +0300

    Added QueryConstants.cs

---
+++ LanosCertifiedStore.Persistence/Queries/Common/Classes/QueryBaseRelated/Constants/QueryConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Persistence.Queries.Common.Classes.QueryBaseRelated.Constants;
+
+internal static class QueryConstants
+{
+    public const string QueryExecutionErrorCode = "QueryExecution";
+    public const string QueryExecutionErrorMessage = "Error occured during the query execution!";
+}
