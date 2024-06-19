0e7edd86107258e682bbbe3ecc25a6b77d6bf132
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-19 21:53:42 +0300

    Added IQueryService.cs

---
+++ LanosCertifiedStore.Application/Contracts/ServicesRelated/RequestRelated/IQueryService.cs
@@ -0,0 +1,14 @@
+﻿using Application.Contracts.RequestRelated;
+using Domain.Contracts.Common;
+
+namespace Application.Contracts.ServicesRelated.RequestRelated;
+
+public interface IQueryService
+{
+    Task<TQueryResult> GetResult<TModel, TQueryResult, TRequestResult>(
+        IQueryRequest<TModel, TQueryResult, TRequestResult> queryRequest,
+        CancellationToken cancellationToken)
+        where TModel : IIdentifiable<Guid>
+        where TQueryResult : notnull
+        where TRequestResult : notnull;
+}
