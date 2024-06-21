bc7d67c21b4e211ce74649307dfe626f2542f89d
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-21 17:36:59 +0300

    Added IQueryFactory.cs and its implementation

---
+++ LanosCertifiedStore.Persistence/Factories/QueryRelated/Common/Classes/Common/Classes/Constants/QueryFactoryConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace Persistence.Factories.QueryRelated.Common.Classes.Common.Classes.Constants;
+
+internal static class QueryFactoryConstants
+{
+    public const string NotFoundQuery = "Relevant query for the provided query request was not found!";
+}
---
+++ LanosCertifiedStore.Persistence/Factories/QueryRelated/Common/Contracts/IQueryFactory.cs
@@ -0,0 +1,14 @@
+﻿using Application.Contracts.RequestRelated;
+using Domain.Contracts.Common;
+using Persistence.Queries.Common.Contracts;
+
+namespace Persistence.Factories.QueryRelated.Common.Contracts;
+
+internal interface IQueryFactory
+{
+    IQuery<TModel, TResult> GetQuery<TModel, TResult, TQueryResult>(
+        IQueryRequest<TModel, TResult, TQueryResult> queryRequest)
+        where TModel : class, IIdentifiable<Guid>
+        where TResult : notnull
+        where TQueryResult : notnull;
+}
