ea98baff58c309ad71aea6f51610ad348062f828
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-21 17:53:44 +0300

    Updated PersistenceServiceCollectionExtensions.cs

--- LanosCertifiedStore.Persistence/Extensions/PersistenceServiceCollectionExtensions.cs
+++ LanosCertifiedStore.Persistence/Extensions/PersistenceServiceCollectionExtensions.cs
@@ -1,7 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Application.Contracts.ServicesRelated.RequestRelated;
+using Domain.Models.VehicleRelated.Classes;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Persistence.Contexts.ApplicationDatabaseContext;
+using Persistence.Entities.VehicleRelated;
+using Persistence.Factories.QueryRelated.Common.Classes;
+using Persistence.Factories.QueryRelated.Common.Contracts;
+using Persistence.Queries.Common.Classes;
+using Persistence.Queries.Common.Contracts;
+using Persistence.Queries.VehicleBrandRelated;
+using Persistence.Services.QueryRelated;
 
 namespace Persistence.Extensions;
 
@@ -14,9 +23,44 @@ public static class PersistenceServiceCollectionExtensions
             option.UseNpgsql(config.GetConnectionString("PostgreSqlConnection"),
                 o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery));
         });
-        
-        // services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        AddVehicleBrandQueriesRelatedServices(services);
+        AddQueryRelatedServices(services);
 
         return services;
+    }
+
+    private static void AddQueryRelatedServices(IServiceCollection services)
+    {
+        services.AddScoped<IQueryPaginator, QueryPaginator>();
+        services.AddScoped<IQueryFactory, QueryFactory>(provider => InitializeQueryFactory(services, provider));
+        services.AddScoped<IQueryService, QueryService>();
+    }
+
+    private static void AddVehicleBrandQueriesRelatedServices(IServiceCollection services)
+    {
+        services.AddTransient<IQuerySortingSettingsSelector<VehicleBrand, VehicleBrandEntity>,
+            VehicleBrandsSortingSettingsSelector>();
+        services.AddTransient<IQueryProjectionProfileSelector<VehicleBrand, VehicleBrandEntity>,
+            VehicleBrandsProjectionProfileSelector>();
+        services.AddTransient<IQueryFilteringCriteriaSelector<VehicleBrand, VehicleBrandEntity>,
+            VehicleBrandsFilteringCriteriaSelector>();
+        services.AddTransient<IQuery<VehicleBrand, IReadOnlyCollection<VehicleBrand>>, VehicleBrandsQuery>();
+    }
+
+    private static QueryFactory InitializeQueryFactory(IServiceCollection services, IServiceProvider provider)
+    {
+        var queryDescriptors = services.Where(
+            serviceDescriptor => serviceDescriptor.ServiceType.IsGenericType &&
+                                 serviceDescriptor.ServiceType.GetGenericTypeDefinition() == typeof(IQuery<,>))
+            .ToList();
+
+        var serviceTypes = queryDescriptors.Select(d => d.ServiceType);
+
+        var queries = serviceTypes.Select(type => provider.GetService(type)!).ToList();
+
+        var queryFactory = new QueryFactory(queries);
+
+        return queryFactory;
     }
 }
