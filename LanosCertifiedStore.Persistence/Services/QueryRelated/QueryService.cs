bb64de30584cde4b2ab976ec3d5edd12377217ac
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-21 18:19:22 +0300

    Added QueryService implementation

--- LanosCertifiedStore.Persistence/Services/QueryRelated/QueryService.cs
+++ LanosCertifiedStore.Persistence/Services/QueryRelated/QueryService.cs
@@ -1,19 +1,33 @@
-﻿using Application.Contracts.RequestRelated;
+﻿using Application.Constants;
+using Application.Contracts.RequestRelated;
 using Application.Contracts.ServicesRelated.RequestRelated;
+using Application.Shared;
 using Domain.Contracts.Common;
-using Persistence.Services.QueryRelated.Common.Contracts;
+using Persistence.Factories.QueryRelated.Common.Contracts;
 
 namespace Persistence.Services.QueryRelated;
 
-internal sealed class QueryService(IQueryMappings queryMappings) : IQueryService
+internal sealed class QueryService(IQueryFactory queryFactory) : IQueryService
 {
-    public Task<TQueryResult> GetResult<TModel, TQueryResult, TRequestResult>(
+    public async Task<Result<TQueryResult>> GetResult<TModel, TQueryResult, TRequestResult>(
         IQueryRequest<TModel, TQueryResult, TRequestResult> queryRequest,
         CancellationToken cancellationToken)
-        where TModel : IIdentifiable<Guid>
+        where TModel : class, IIdentifiable<Guid>
         where TQueryResult : notnull
         where TRequestResult : notnull
     {
-        throw new NotImplementedException();
+        try
+        {
+            var query = queryFactory.GetQuery(queryRequest);
+            var result = await query.Execute(queryRequest, cancellationToken);
+
+            return result;
+        }
+        catch (Exception)
+        {
+            return Result<TQueryResult>.Failure(new Error(
+                QueryServiceConstants.NonProcessableQueryRequestCode,
+                QueryServiceConstants.NonProcessableQueryMessage));
+        }
     }
 }
