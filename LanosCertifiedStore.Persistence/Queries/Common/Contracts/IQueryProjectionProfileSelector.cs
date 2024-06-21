3f76fcab54651a1f9aa7ea567758eeda26d04d0e
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-21 17:55:33 +0300

    Renamed IQueryProjectionProfileSelector.cs

---
+++ LanosCertifiedStore.Persistence/Queries/Common/Contracts/IQueryProjectionProfileSelector.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+using Application.Contracts.RepositoryRelated.Common;
+using Domain.Contracts.Common;
+
+namespace Persistence.Queries.Common.Contracts;
+
+internal interface IQueryProjectionProfileSelector<TModel, TEntity>
+    where TEntity : IIdentifiable<Guid>
+    where TModel : class, IIdentifiable<Guid>
+{
+    Expression<Func<TEntity, TEntity>> GetProjectionProfile(
+        IFilteringRequestParameters<TModel>? filteringRequestParameters = null!);
+}
