988a8e53334abd0ca0e27d7f72fa807ee6850cfa
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-21 18:11:23 +0300

    Added ProjectionProfileSelectorBase.cs

---
+++ LanosCertifiedStore.Persistence/Queries/Common/Classes/QueryRelated/ProjectionProfileSelectorBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+using Application.Contracts.RepositoryRelated.Common;
+using Domain.Contracts.Common;
+using Persistence.Queries.Common.Contracts;
+
+namespace Persistence.Queries.Common.Classes.QueryRelated;
+
+internal abstract class ProjectionProfileSelectorBase<TModel, TEntity> : 
+    IQueryProjectionProfileSelector<TModel, TEntity> 
+    where TModel : class, IIdentifiable<Guid>
+    where TEntity : class, IIdentifiable<Guid>
+{
+    public Expression<Func<TEntity, TEntity>> GetProjectionProfile(
+        IFilteringRequestParameters<TModel>? filteringRequestParameters = null!)
+    {
+        return filteringRequestParameters is null 
+            ? GetDefaultProfile() 
+            : GetRelevantProfile(filteringRequestParameters);
+    }
+
+    private protected abstract Expression<Func<TEntity, TEntity>> GetDefaultProfile();
+
+    private protected abstract Expression<Func<TEntity, TEntity>> GetRelevantProfile(
+        IFilteringRequestParameters<TModel> filteringRequestParameters);
+}
