ffcdb466a9be43eb299da51cdbef62b719f10b60
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-23 11:03:51 +0300

    Added QueriesServiceCollectionExtensions.cs to separate query-related services registration

--- LanosCertifiedStore.Persistence/Extensions/PersistenceServiceCollectionExtensions.cs
+++ LanosCertifiedStore.Persistence/Extensions/PersistenceServiceCollectionExtensions.cs
@@ -1,16 +1,8 @@
-﻿using Application.Contracts.ServicesRelated.RequestRelated;
-using Domain.Models.VehicleRelated.Classes;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Persistence.Contexts.ApplicationDatabaseContext;
-using Persistence.Entities.VehicleRelated;
-using Persistence.Factories.QueryRelated.Common.Classes;
-using Persistence.Factories.QueryRelated.Common.Contracts;
-using Persistence.Queries.Common.Classes;
-using Persistence.Queries.Common.Contracts;
-using Persistence.Queries.VehicleBrandRelated;
-using Persistence.Services.QueryRelated;
+using Persistence.Extensions.QueryRelated;
 
 namespace Persistence.Extensions;
 
@@ -24,43 +16,8 @@ public static class PersistenceServiceCollectionExtensions
                 o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery));
         });
 
-        AddVehicleBrandQueriesRelatedServices(services);
-        AddQueryRelatedServices(services);
+        services.AddQueryRelatedServices();
 
         return services;
-    }
-
-    private static void AddQueryRelatedServices(IServiceCollection services)
-    {
-        services.AddScoped<IQueryPaginator, QueryPaginator>();
-        services.AddScoped<IQueryFactory, QueryFactory>(provider => InitializeQueryFactory(services, provider));
-        services.AddScoped<IQueryService, QueryService>();
-    }
-
-    private static void AddVehicleBrandQueriesRelatedServices(IServiceCollection services)
-    {
-        services.AddTransient<IQuerySortingSettingsSelector<VehicleBrand, VehicleBrandEntity>,
-            VehicleBrandsSortingSettingsSelector>();
-        services.AddTransient<IQueryProjectionProfileSelector<VehicleBrand, VehicleBrandEntity>,
-            VehicleBrandsProjectionProfileSelector>();
-        services.AddTransient<IQueryFilteringCriteriaSelector<VehicleBrand, VehicleBrandEntity>,
-            VehicleBrandsFilteringCriteriaSelector>();
-        services.AddTransient<IQuery<VehicleBrand, IReadOnlyCollection<VehicleBrand>>, VehicleBrandsQuery>();
-    }
-
-    private static QueryFactory InitializeQueryFactory(IServiceCollection services, IServiceProvider provider)
-    {
-        var queryDescriptors = services.Where(
-            serviceDescriptor => serviceDescriptor.ServiceType.IsGenericType &&
-                                 serviceDescriptor.ServiceType.GetGenericTypeDefinition() == typeof(IQuery<,>))
-            .ToList();
-
-        var serviceTypes = queryDescriptors.Select(d => d.ServiceType);
-
-        var queries = serviceTypes.Select(type => provider.GetService(type)!).ToList();
-
-        var queryFactory = new QueryFactory(queries);
-
-        return queryFactory;
     }
 }
---
+++ LanosCertifiedStore.Persistence/Extensions/QueryRelated/QueriesServiceCollectionExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Application.Contracts.ServicesRelated.RequestRelated;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Factories.QueryRelated.Common.Classes;
+using Persistence.Factories.QueryRelated.Common.Contracts;
+using Persistence.Queries.Common.Classes;
+using Persistence.Queries.Common.Contracts;
+using Persistence.Services.QueryRelated;
+
+namespace Persistence.Extensions.QueryRelated;
+
+internal static class QueriesServiceCollectionExtensions
+{
+    public static IServiceCollection AddQueryRelatedServices(this IServiceCollection services)
+    {
+        AddCommonServices(services);
+        
+        services.AddVehicleBrandQueriesRelatedServices();
+
+        return services;
+    }
+
+    private static void AddCommonServices(IServiceCollection services)
+    {
+        services.AddScoped<IQueryPaginator, QueryPaginator>();
+        services.AddScoped<IQueryFactory, QueryFactory>(provider => InitializeQueryFactory(services, provider));
+        services.AddScoped<IQueryService, QueryService>();
+    }
+
+    private static QueryFactory InitializeQueryFactory(IServiceCollection services, IServiceProvider provider)
+    {
+        var queryDescriptors = services.Where(
+                serviceDescriptor => serviceDescriptor.ServiceType.IsGenericType &&
+                                     serviceDescriptor.ServiceType.GetGenericTypeDefinition() == typeof(IQuery<,>))
+            .ToList();
+
+        var serviceTypes = queryDescriptors.Select(d => d.ServiceType);
+
+        var queries = serviceTypes.Select(type => provider.GetService(type)!).ToList();
+
+        var queryFactory = new QueryFactory(queries);
+
+        return queryFactory;
+    }
+}
