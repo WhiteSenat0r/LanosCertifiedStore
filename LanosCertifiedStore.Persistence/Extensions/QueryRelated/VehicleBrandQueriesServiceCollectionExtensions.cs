95450b804a9b7b702200cbeea008421aee532d75
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-23 11:03:08 +0300

    Added VehicleBrandQueriesServiceCollectionExtensions.cs

---
+++ LanosCertifiedStore.Persistence/Extensions/QueryRelated/VehicleBrandQueriesServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Models.VehicleRelated.Classes;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Entities.VehicleRelated;
+using Persistence.Queries.Common.Contracts;
+using Persistence.Queries.VehicleBrandRelated;
+
+namespace Persistence.Extensions.QueryRelated;
+
+internal static class VehicleBrandQueriesServiceCollectionExtensions
+{
+    public static IServiceCollection AddVehicleBrandQueriesRelatedServices(this IServiceCollection services)
+    {
+        services.AddTransient<IQuerySortingSettingsSelector<VehicleBrand, VehicleBrandEntity>,
+            VehicleBrandsSortingSettingsSelector>();
+        services.AddTransient<IQueryProjectionProfileSelector<VehicleBrand, VehicleBrandEntity>,
+            VehicleBrandsProjectionProfileSelector>();
+        services.AddTransient<IQueryFilteringCriteriaSelector<VehicleBrand, VehicleBrandEntity>,
+            VehicleBrandsFilteringCriteriaSelector>();
+        
+        services.AddTransient<IQuery<VehicleBrand, IReadOnlyCollection<VehicleBrand>>, VehicleBrandsCollectionQuery>();
+        services.AddTransient<IQuery<VehicleBrand, VehicleBrand>, SingleVehicleBrandQuery>();
+
+        return services;
+    }
+}
