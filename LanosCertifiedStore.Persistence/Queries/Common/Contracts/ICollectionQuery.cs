a6b5b06b23436f6a24905bbc13d96f879d3e4b90
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-23 11:09:28 +0300

    Added ICollectionQuery.cs and ISingleQuery.cs

---
+++ LanosCertifiedStore.Persistence/Queries/Common/Contracts/ICollectionQuery.cs
@@ -0,0 +1,15 @@
+﻿using Application.Contracts.RequestRelated.QueryRelated;
+using Application.Shared;
+using Domain.Contracts.Common;
+
+namespace Persistence.Queries.Common.Contracts;
+
+internal interface ICollectionQuery<TModel, TResult> : IQuery<TModel, TResult>
+    where TModel : class, IIdentifiable<Guid>
+    where TResult : IReadOnlyCollection<TModel>
+{
+    Task<Result<TResult>> Execute<TRequestResult>(
+        IQueryRequest<TModel, IReadOnlyCollection<TModel>, TRequestResult> queryRequest,
+        CancellationToken cancellationToken)
+        where TRequestResult : notnull;
+}
---
+++ LanosCertifiedStore.Persistence/Queries/Common/Contracts/ISingleQuery.cs
@@ -0,0 +1,9 @@
+﻿using Application.Shared;
+using Domain.Contracts.Common;
+
+namespace Persistence.Queries.Common.Contracts;
+
+internal interface ISingleQuery<TModel, TQueryResult, TRequestResult, TDto> : IQuery<TModel, TQueryResult>
+    where TModel : class, IIdentifiable<Guid>
+    where TQueryResult : TModel
+    where TRequestResult : Result<TDto>;
