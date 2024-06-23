efa59bb3c4f818dfe96e383dc79e1cc4f4a7e919
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-23 11:00:25 +0300

    Added SingleVehicleBrandQueryRequest.cs and its handler

---
+++ LanosCertifiedStore.Application/Queries/VehicleBrandsRelated/SingleVehicleBrandQueryRelated/SingleVehicleBrandQueryRequest.cs
@@ -0,0 +1,17 @@
+﻿using Application.Dtos.BrandDtos;
+using Application.Enums.RequestParametersRelated;
+using Application.Queries.Common.SingleQueryRelated;
+using Application.RequestParams;
+using Application.Shared;
+using Domain.Models.VehicleRelated.Classes;
+
+namespace Application.Queries.VehicleBrandsRelated.SingleVehicleBrandQueryRelated;
+
+public sealed record SingleVehicleBrandQueryRequest(Guid Id, bool IsTracked) : 
+    SingleQueryRequestBase<VehicleBrand, VehicleBrand, Result<VehicleBrandDto>, VehicleBrandDto>(
+        new VehicleBrandFilteringRequestParameters
+        {
+            ProjectionProfile = VehicleBrandProjectionProfile.Single
+        }, 
+        IsTracked,
+        Id);
---
+++ LanosCertifiedStore.Application/Queries/VehicleBrandsRelated/SingleVehicleBrandQueryRelated/SingleVehicleBrandQueryRequestHandler.cs
@@ -0,0 +1,18 @@
+﻿using Application.Contracts.ServicesRelated.RequestRelated;
+using Application.Dtos.BrandDtos;
+using Application.Queries.Common.SingleQueryRelated;
+using Application.Shared;
+using AutoMapper;
+using Domain.Models.VehicleRelated.Classes;
+
+namespace Application.Queries.VehicleBrandsRelated.SingleVehicleBrandQueryRelated;
+
+internal sealed class SingleVehicleBrandQueryRequestHandler(
+    IQueryService queryService,
+    IMapper mapper) :
+    SingleQueryRequestHandlerBase<
+        SingleVehicleBrandQueryRequest,
+        VehicleBrand,
+        VehicleBrand,
+        Result<VehicleBrandDto>,
+        VehicleBrandDto>(queryService, mapper);
