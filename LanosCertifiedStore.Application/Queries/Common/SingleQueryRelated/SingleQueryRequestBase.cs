74eb3630e3901ab01cd2456425f006e3397fa4ad
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-23 10:55:23 +0300

    Added SingleQueryRequestBase.cs and SingleQueryRequestHandlerBase.cs

---
+++ LanosCertifiedStore.Application/Queries/Common/SingleQueryRelated/SingleQueryRequestBase.cs
@@ -0,0 +1,15 @@
+﻿using Application.Contracts.RepositoryRelated.Common;
+using Application.Contracts.RequestRelated.QueryRelated;
+using Application.Shared;
+using Domain.Contracts.Common;
+
+namespace Application.Queries.Common.SingleQueryRelated;
+
+public abstract record SingleQueryRequestBase<TModel, TQueryResult, TRequestResult, TDto>(
+    IFilteringRequestParameters<TModel> FilteringParameters,
+    bool IsTracked,
+    Guid ItemId) : ISingleQueryRequest<TModel, TQueryResult, TRequestResult, TDto> 
+    where TModel : class, IIdentifiable<Guid>
+    where TQueryResult : TModel
+    where TRequestResult : Result<TDto> 
+    where TDto : class;
---
+++ LanosCertifiedStore.Application/Queries/Common/SingleQueryRelated/SingleQueryRequestHandlerBase.cs
@@ -0,0 +1,29 @@
+﻿using Application.Contracts.ServicesRelated.RequestRelated;
+using Application.Shared;
+using AutoMapper;
+using Domain.Contracts.Common;
+using MediatR;
+
+namespace Application.Queries.Common.SingleQueryRelated;
+
+internal abstract class SingleQueryRequestHandlerBase<TRequest, TModel, TQueryResult, TRequestResult, TDto>(
+    IQueryService queryService,
+    IMapper mapper) : IRequestHandler<TRequest, TRequestResult>
+    where TRequest : SingleQueryRequestBase<TModel, TQueryResult, TRequestResult, TDto>
+    where TModel : class, IIdentifiable<Guid>
+    where TQueryResult : TModel
+    where TRequestResult : Result<TDto>
+    where TDto : class
+{
+    public async Task<TRequestResult> Handle(TRequest request, CancellationToken cancellationToken)
+    {
+        var queryResult = await queryService.GetResult(request, cancellationToken);
+
+        if (!queryResult.IsSuccess)
+            return (Result<TDto>.Failure(queryResult.Error!) as TRequestResult)!;
+
+        var mappedItem = mapper.Map<TQueryResult, TDto>(queryResult.Value!);
+
+        return (Result<TDto>.Success(mappedItem) as TRequestResult)!;
+    }
+}
