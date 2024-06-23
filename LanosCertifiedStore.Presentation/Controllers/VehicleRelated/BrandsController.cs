586e2152e1d9c36f037a471b12c586f7da23aee1
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-23 11:02:49 +0300

    Added GetBrand method in BrandsController.cs

--- LanosCertifiedStore.Presentation/Controllers/VehicleRelated/BrandsController.cs
+++ LanosCertifiedStore.Presentation/Controllers/VehicleRelated/BrandsController.cs
@@ -6,8 +6,9 @@ using Application.Commands.Brands.UpdateBrand;
 using Application.Core.Results;
 using Application.Dtos.BrandDtos;
 using Application.Dtos.Common;
-using Application.Queries.Brands.BrandQueryRelated;
-using Application.Queries.Brands.CountBrandsQueryRelated;
+using Application.Queries.VehicleBrandsRelated.CountBrandsQueryRelated;
+using Application.Queries.VehicleBrandsRelated.ListVehicleBrandsQueryRelated;
+using Application.Queries.VehicleBrandsRelated.SingleVehicleBrandQueryRelated;
 using Application.RequestParams;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -22,7 +23,17 @@ public sealed class BrandsController : BaseEntityRelatedApiController
     public async Task<ActionResult<PaginationResult<VehicleBrandDto>>> GetBrands(
         [FromQuery] VehicleBrandFilteringRequestParameters requestParameters, bool isTracked)
     {
-        return HandleResult(await Mediator.Send(new ListVehicleBrandsQueryRequest(requestParameters, isTracked)));
+        return HandleResult(
+            await Mediator.Send(new CollectionVehicleBrandsQueryRequest(requestParameters, isTracked)));
+    }
+    
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(typeof(VehicleBrandDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<VehicleBrandDto>> GetBrand(
+        Guid id, [FromQuery] bool isTracked)
+    {
+        return HandleResult(await Mediator.Send(new SingleVehicleBrandQueryRequest(id, isTracked)));
     }
     
     [HttpGet("countItems")]
