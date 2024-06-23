e6c1596ecdf946b98dc58a12f982db2f398b33ac
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-23 11:12:24 +0300

    Added SingleVehicleBrandQuery.cs

---
+++ LanosCertifiedStore.Persistence/Queries/VehicleBrandRelated/SingleVehicleBrandQuery.cs
@@ -0,0 +1,17 @@
+﻿using Application.Dtos.BrandDtos;
+using AutoMapper;
+using Domain.Models.VehicleRelated.Classes;
+using Persistence.Contexts.ApplicationDatabaseContext;
+using Persistence.Entities.VehicleRelated;
+using Persistence.Queries.Common.Classes.QueryBaseRelated;
+using Persistence.Queries.Common.Contracts;
+
+namespace Persistence.Queries.VehicleBrandRelated;
+
+internal sealed class SingleVehicleBrandQuery(
+    ApplicationDatabaseContext context,
+    IQueryProjectionProfileSelector<VehicleBrand, VehicleBrandEntity> projectionProfileSelector,
+    IMapper mapper) : SingleQueryBase<VehicleBrand, VehicleBrandEntity, VehicleBrandDto>(
+    context,
+    projectionProfileSelector,
+    mapper);
