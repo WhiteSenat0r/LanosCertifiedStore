fb886059abb4593757e845c4756a7cd17c57fd19
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-23 11:01:34 +0300

    Added ICollectionQueryRequest.cs.cs and ISingleQueryRequest.cs

---
+++ LanosCertifiedStore.Application/Contracts/RequestRelated/QueryRelated/ICollectionQueryRequest.cs
@@ -0,0 +1,12 @@
+﻿using Application.Core.Results;
+using Application.Shared;
+using Domain.Contracts.Common;
+
+namespace Application.Contracts.RequestRelated.QueryRelated;
+
+public interface ICollectionQueryRequest<TModel, TQueryResult, TRequestResult, TDto> :
+    IQueryRequest<TModel, TQueryResult, TRequestResult>
+    where TModel : IIdentifiable<Guid>
+    where TQueryResult : IReadOnlyCollection<TModel>
+    where TRequestResult : Result<PaginationResult<TDto>>
+    where TDto : class;
---
+++ LanosCertifiedStore.Application/Contracts/RequestRelated/QueryRelated/ISingleQueryRequest.cs
@@ -0,0 +1,14 @@
+﻿using Application.Shared;
+using Domain.Contracts.Common;
+
+namespace Application.Contracts.RequestRelated.QueryRelated;
+
+public interface ISingleQueryRequest<TModel, TQueryResult, TRequestResult, TDto> : 
+    IQueryRequest<TModel, TQueryResult, TRequestResult>
+    where TModel : class, IIdentifiable<Guid>
+    where TQueryResult : TModel
+    where TRequestResult : Result<TDto>
+    where TDto : class
+{
+    Guid ItemId { get; }
+}
