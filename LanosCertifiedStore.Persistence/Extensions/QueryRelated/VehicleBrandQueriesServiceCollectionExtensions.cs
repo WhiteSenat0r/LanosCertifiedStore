386328205027decf165ebd7c79cf17254ae09c03
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-26 17:00:11 +0300

    Registered CountVehicleBrandsQuery in VehicleBrandQueriesServiceCollectionExtensions.cs

--- LanosCertifiedStore.Persistence/Extensions/QueryRelated/VehicleBrandQueriesServiceCollectionExtensions.cs
+++ LanosCertifiedStore.Persistence/Extensions/QueryRelated/VehicleBrandQueriesServiceCollectionExtensions.cs
@@ -2,7 +2,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using Persistence.Entities.VehicleRelated;
 using Persistence.Queries.Common.Contracts;
-using Persistence.Queries.VehicleBrandRelated;
+using Persistence.Queries.VehicleBrandRelated.QueryRelated;
+using Persistence.Queries.VehicleBrandRelated.SelectorRelated;
 
 namespace Persistence.Extensions.QueryRelated;
 
@@ -19,6 +20,7 @@ internal static class VehicleBrandQueriesServiceCollectionExtensions
         
         services.AddTransient<IQuery<VehicleBrand, IReadOnlyCollection<VehicleBrand>>, CollectionVehicleBrandsQuery>();
         services.AddTransient<IQuery<VehicleBrand, VehicleBrand>, SingleVehicleBrandQuery>();
+        services.AddTransient<IQuery<VehicleBrand, Tuple<int, int>>, CountVehicleBrandsQuery>();
 
         return services;
     }
