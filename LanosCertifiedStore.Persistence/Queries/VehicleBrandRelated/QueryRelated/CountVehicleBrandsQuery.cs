582bc27b99de21620f05fa58f0328598b45548b5
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-26 17:07:12 +0300

    Added CountVehicleBrandsQuery.cs

---
+++ LanosCertifiedStore.Persistence/Queries/VehicleBrandRelated/QueryRelated/CountVehicleBrandsQuery.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models.VehicleRelated.Classes;
+using Persistence.Contexts.ApplicationDatabaseContext;
+using Persistence.Entities.VehicleRelated;
+using Persistence.Queries.Common.Classes.QueryBaseRelated;
+using Persistence.Queries.Common.Contracts;
+
+namespace Persistence.Queries.VehicleBrandRelated.QueryRelated;
+
+internal sealed class CountVehicleBrandsQuery(
+    ApplicationDatabaseContext context,
+    IQueryFilteringCriteriaSelector<VehicleBrand, VehicleBrandEntity> filteringCriteriaSelector) : 
+    CountQueryBase<VehicleBrand, VehicleBrandEntity>(context, filteringCriteriaSelector);
