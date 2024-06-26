3d5ad5bae2d52cf131bdc88d13d4b58916e1d5f5
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-26 16:59:32 +0300

    Added CountVehicleBrandsQueryRequest and its handler

---
+++ LanosCertifiedStore.Application/Queries/VehicleBrandsRelated/CountVehicleBrandsQueryRelated/CountVehicleBrandsQueryRequest.cs
@@ -0,0 +1,11 @@
+﻿using Application.Contracts.RequestParametersRelated;
+using Application.Dtos.Common;
+using Application.Queries.Common.CountItemsQueryRelated;
+using Application.Shared;
+using Domain.Models.VehicleRelated.Classes;
+
+namespace Application.Queries.VehicleBrandsRelated.CountVehicleBrandsQueryRelated;
+
+public sealed record CountVehicleBrandsQueryRequest(
+    IVehicleBrandFilteringRequestParameters RequestParameters) : 
+    CountItemsQueryRequestBase<VehicleBrand, Tuple<int, int>, Result<ItemsCountDto>>(RequestParameters, false);
---
+++ LanosCertifiedStore.Application/Queries/VehicleBrandsRelated/CountVehicleBrandsQueryRelated/CountVehicleBrandsQueryRequestHandler.cs
@@ -0,0 +1,14 @@
+﻿using Application.Contracts.ServicesRelated.RequestRelated;
+using Application.Dtos.Common;
+using Application.Queries.Common.CountItemsQueryRelated;
+using Application.Shared;
+using Domain.Models.VehicleRelated.Classes;
+
+namespace Application.Queries.VehicleBrandsRelated.CountVehicleBrandsQueryRelated;
+
+internal sealed class CountVehicleBrandsQueryRequestRequestHandler(IQueryService queryService) : 
+    CountItemsQueryRequestHandlerBase<
+        VehicleBrand,
+        CountVehicleBrandsQueryRequest,
+        Tuple<int, int>,
+        Result<ItemsCountDto>>(queryService);
