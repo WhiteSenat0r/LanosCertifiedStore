011d58fa5d5c6eccb7c360cb5ea0addb431d88fc
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-26 16:58:02 +0300

    Added ICountQueryRequest.cs

---
+++ LanosCertifiedStore.Application/Contracts/RequestRelated/QueryRelated/ICountQueryRequest.cs
@@ -0,0 +1,11 @@
+﻿using Application.Dtos.Common;
+using Application.Shared;
+using Domain.Contracts.Common;
+
+namespace Application.Contracts.RequestRelated.QueryRelated;
+
+public interface ICountQueryRequest<TModel, TQueryResult, TRequestResult> : 
+    IQueryRequest<TModel, TQueryResult, TRequestResult>
+    where TModel : class, IIdentifiable<Guid>
+    where TQueryResult : Tuple<int, int>
+    where TRequestResult : Result<ItemsCountDto>;
