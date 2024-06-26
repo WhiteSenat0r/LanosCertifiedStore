ffb926fc7902826ad36617b8aedc7caa1293395c
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-26 16:58:49 +0300

    Updated CountItemsQueryRequestBase.cs and its handler

---
+++ LanosCertifiedStore.Application/Queries/Common/CountItemsQueryRelated/CountItemsQueryRequestHandlerBase.cs
@@ -0,0 +1,27 @@
+﻿using Application.Contracts.ServicesRelated.RequestRelated;
+using Application.Dtos.Common;
+using Application.Shared;
+using Domain.Contracts.Common;
+using MediatR;
+
+namespace Application.Queries.Common.CountItemsQueryRelated;
+
+internal abstract class CountItemsQueryRequestHandlerBase<TModel, TRequest, TQueryResult, TRequestResult>(
+    IQueryService queryService) : IRequestHandler<TRequest, TRequestResult>
+    where TRequest : CountItemsQueryRequestBase<TModel, TQueryResult, TRequestResult>
+    where TModel : class, IIdentifiable<Guid>
+    where TQueryResult : Tuple<int, int>
+    where TRequestResult : Result<ItemsCountDto>
+{
+    public async Task<TRequestResult> Handle(TRequest request, CancellationToken cancellationToken)
+    {
+        var queryResult = await queryService.GetResult(request, cancellationToken);
+
+        if (!queryResult.IsSuccess)
+            return (Result<ItemsCountDto>.Failure(queryResult.Error!) as TRequestResult)!;
+
+        var counts = new ItemsCountDto(queryResult.Value!.Item1, queryResult.Value.Item2);
+        
+        return (Result<ItemsCountDto>.Success(counts) as TRequestResult)!;
+    }
+}
--- LanosCertifiedStore.ApplicationLayer.Application/Queries/Common/CountItemsQueryRelated/CountItemsQueryBase.cs
+++
@@ -1,8 +0,0 @@
-﻿using Application.Contracts.RepositoryRelated.Common;
-using Domain.Contracts.Common;
-
-namespace Application.Queries.Common.CountItemsQueryRelated;
-
-public abstract record CountItemsQueryBase<TEntity>(
-    IFilteringRequestParameters<TEntity>? RequestParameters)
-    where TEntity : class, IIdentifiable<Guid>;
