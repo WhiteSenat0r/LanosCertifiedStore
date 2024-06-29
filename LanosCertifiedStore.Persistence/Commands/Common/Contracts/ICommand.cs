cc9a72240042720c0eea390e1536a890b9a51b9b
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-29 16:21:02 +0300

    Added ICommand.cs

---
+++ LanosCertifiedStore.Persistence/Commands/Common/Contracts/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using Application.Contracts.RequestRelated;
+using Application.Shared.ResultRelated;
+using Domain.Contracts.Common;
+
+namespace Persistence.Commands.Common.Contracts;
+
+internal interface ICommand<TModel, TRequest, TRequestResult>
+    where TModel : class, IIdentifiable<Guid>
+    where TRequest : ICommandRequest<TModel, TRequestResult>
+    where TRequestResult : notnull
+{
+    Task<Result<TModel>> Execute(TRequest commandRequest, CancellationToken cancellationToken);
+}
