5d9c61e72f04f63261a6fb99147961dd064efacc
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-29 16:20:52 +0300

    Added CommandBase.cs and CommandConstants.cs

---
+++ LanosCertifiedStore.Persistence/Commands/Common/Classes/CommandBase.cs
@@ -0,0 +1,15 @@
+﻿using Application.Contracts.RequestRelated;
+using Application.Shared.ResultRelated;
+using Domain.Contracts.Common;
+using Persistence.Commands.Common.Contracts;
+
+namespace Persistence.Commands.Common.Classes;
+
+internal abstract class CommandBase<TModel, TRequest, TRequestResult> : 
+    ICommand<TModel, TRequest, TRequestResult>
+    where TModel : class, IIdentifiable<Guid> 
+    where TRequest : ICommandRequest<TModel, TRequestResult>
+    where TRequestResult : notnull
+{
+    public abstract Task<Result<TModel>> Execute(TRequest commandRequest, CancellationToken cancellationToken);
+}
