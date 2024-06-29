501641a571143f82492d72a29f7282208c3449f5
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-29 15:57:19 +0300

    Removed responses from Presentation layer

--- LanosCertifiedStore.Presentation/Responses/ApiException.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace API.Responses;
-
-internal class ApiException(int statusCode, string message = null!, string details = null!)
-    : ApiResponse(statusCode, message)
-{
-    public string Details { get; init; } = details;
-}
