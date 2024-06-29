49aa49b3a7b373623f0588b7b41d47246f35433e
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-29 16:13:14 +0300

    Added TransactionService.cs

---
+++ LanosCertifiedStore.Persistence/Services/TransactionService.cs
@@ -0,0 +1,19 @@
+﻿using Application.Contracts.ServicesRelated.RequestRelated;
+using Persistence.Contexts.ApplicationDatabaseContext;
+
+namespace Persistence.Services;
+
+internal sealed class TransactionService(ApplicationDatabaseContext context) : ITransactionService
+{
+    public async Task BeginTransaction(CancellationToken cancellationToken = default) =>
+        await context.Database.BeginTransactionAsync(cancellationToken);
+
+    public async Task CommitTransaction(CancellationToken cancellationToken = default) => 
+        await context.Database.CommitTransactionAsync(cancellationToken);
+
+    public async Task RollbackTransaction(CancellationToken cancellationToken = default) => 
+        await context.Database.RollbackTransactionAsync(cancellationToken);
+
+    public async Task SavePerformedChanges(CancellationToken cancellationToken = default) =>
+        await context.SaveChangesAsync(cancellationToken);
+}
