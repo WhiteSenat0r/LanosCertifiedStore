8344d1a734170dea82e09c6356ded9531c2cca5d
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-29 16:17:39 +0300

    Added ICommandFactory.cs

---
+++ LanosCertifiedStore.Persistence/Factories/CommandRelated/Common/Contracts/ICommandFactory.cs
@@ -0,0 +1,14 @@
+﻿using Application.Contracts.RequestRelated;
+using Domain.Contracts.Common;
+using Persistence.Commands.Common.Contracts;
+
+namespace Persistence.Factories.CommandRelated.Common.Contracts;
+
+internal interface ICommandFactory
+{
+    ICommand<TModel, TRequest, TRequestResult> GetCommand<TModel, TRequest, TRequestResult>(
+        TRequest commandRequest)
+        where TModel : class, IIdentifiable<Guid>
+        where TRequest : ICommandRequest<TModel, TRequestResult>
+        where TRequestResult : notnull;
+}
