b2b235d2d72c71b3c47715811b3af49f6f495f2f
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-29 15:18:41 +0300

    Added ExceptionHandlerBase.cs

---
+++ LanosCertifiedStore.Presentation/Middlewares/ExceptionRelated/Common/Classes/ExceptionHandlerBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Middlewares.ExceptionRelated.Common.Classes;
+
+internal abstract class ExceptionHandlerBase<TException>(
+    ILogger<ExceptionHandlerBase<TException>> logger,
+    string title,
+    string detail,
+    HttpStatusCode statusCode) : IExceptionHandler
+    where TException : Exception
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        if (exception is not TException) return false;
+        
+        logger.LogError(
+            exception, "Exception occurred: {Message}", exception.Message);
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = title,
+            Detail = detail
+        };
+
+        httpContext.Response.StatusCode = (int)statusCode;
+
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
