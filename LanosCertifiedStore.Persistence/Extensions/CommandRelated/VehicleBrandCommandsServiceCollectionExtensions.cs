9a28cf12026ae412268a8478a6b3337117b0403c
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-06-29 16:17:14 +0300

    Added VehicleBrandCommandsServiceCollectionExtensions.cs

---
+++ LanosCertifiedStore.Persistence/Extensions/CommandRelated/VehicleBrandCommandsServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Application.Commands.VehicleBrandsRelated.CreateVehicleBrandRelated;
+using Application.Dtos.BrandDtos;
+using Domain.Models.VehicleRelated.Classes;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Commands.Common.Contracts;
+using Persistence.Commands.VehicleBrandRelated;
+
+namespace Persistence.Extensions.CommandRelated;
+
+internal static class VehicleBrandCommandsServiceCollectionExtensions
+{
+    public static IServiceCollection AddVehicleBrandCommandsRelatedServices(this IServiceCollection services)
+    {
+        services.AddTransient<
+            ICommand<VehicleBrand, CreateVehicleBrandCommandRequest, VehicleBrandDto>,
+            CreateVehicleBrandCommand>();
+        
+        return services;
+    }
+}
