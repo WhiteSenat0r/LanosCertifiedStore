81b17bcae3d528747a52f2f0d280c1a346a03538
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-02 22:15:45 +0300

    Removed QueryServiceConstants.cs

--- LanosCertifiedStore.Application/Constants/QueryServiceConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Application.Constants;
-
-public static class QueryServiceConstants
-{
-    public const string NonProcessableQueryRequestCode = "NonProcessableQueryRequest";
-    public const string NonProcessableQueryMessage = "The provided query request can not be processed!";
-}
