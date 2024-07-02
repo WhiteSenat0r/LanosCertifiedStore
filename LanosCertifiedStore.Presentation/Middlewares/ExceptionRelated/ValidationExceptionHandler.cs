bfb39477534ce9f296f00819910919413f1969b0
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-02 21:58:49 +0300

    Added ValidationExceptionHandler.cs

---
+++ LanosCertifiedStore.Presentation/Middlewares/ExceptionRelated/ValidationExceptionHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using API.Middlewares.ExceptionRelated.Common.Classes;
+using Application.Shared.ExceptionRelated;
+
+namespace API.Middlewares.ExceptionRelated;
+
+internal sealed class ValidationExceptionHandler(
+    ILogger<ValidationExceptionHandler> logger) : ExceptionHandlerBase<ValidationException>(
+    logger,
+    HttpStatusCode.BadRequest,
+    "ValidationError");
