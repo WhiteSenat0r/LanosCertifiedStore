f3852a4aad5db82f3338ce99cf2b1c7348051f7f
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-02 22:01:09 +0300

    Removed factories related to queries and commands

--- LanosCertifiedStore.Persistence/Factories/CommandRelated/Common/Classes/Common/Classes/Constants/CommandFactoryConstants.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Persistence.Factories.CommandRelated.Common.Classes.Common.Classes.Constants;
-
-internal static class CommandFactoryConstants
-{
-    public const string NotFoundCommand = "Relevant command for the provided command request was not found!";
-}
