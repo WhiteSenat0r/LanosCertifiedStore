780bb9011411411d2e889730509dd43c4f27c6f3
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-03 22:42:25 +0300

    Refactored VehicleBrandFilteringRequestParameters.cs

--- LanosCertifiedStore.Application/RequestParameters/VehicleBrandFilteringRequestParameters.cs
+++ LanosCertifiedStore.Application/RequestParameters/VehicleBrandFilteringRequestParameters.cs
@@ -1,14 +1,6 @@
-﻿using Application.Contracts.RequestParametersRelated;
-using Application.Enums.RequestParametersRelated;
-using Application.RequestParameters.Common.Classes;
+﻿using Application.RequestParameters.Common.Classes;
 using Domain.Entities.VehicleRelated;
 
 namespace Application.RequestParameters;
 
-public sealed class VehicleBrandFilteringRequestParameters : BaseFilteringRequestParameters<VehicleBrand>,
-    IVehicleBrandFilteringRequestParameters
-{
-    public string? Name { get; set; }
-    public string? ContainedModelName { get; set; }
-    public VehicleBrandProjectionProfile ProjectionProfile { get; set; } = VehicleBrandProjectionProfile.Default;
-}
+public sealed class VehicleBrandFilteringRequestParameters : BaseFilteringRequestParameters<VehicleBrand>;
