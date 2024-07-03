db47d689f52212d097676947cc79fe0a99e245ce
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-03 21:46:45 +0300

    IVehicleColorService added

---
+++ LanosCertifiedStore.Application/Contracts/ServicesRelated/IVehicleColorService.cs
@@ -0,0 +1,10 @@
+﻿using Application.Contracts.RequestParametersRelated;
+using Application.Dtos.ColorDtos;
+
+namespace Application.Contracts.ServicesRelated;
+
+public interface IVehicleColorService
+{
+    Task<VehicleColorDto> GetVehicleColors(IVehicleColorFilteringRequestParameters filteringRequestParameters,
+        CancellationToken cancellationToken);
+}
