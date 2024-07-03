84e98ec212590133d03db9be5656dea91084c313
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-03 22:58:06 +0300

    Commented unused code

--- LanosCertifiedStore.Persistence/Repositories/VehicleBrandRelated/QueryBuilderRelated/Common/Classes/VehicleBrandFilteringCriteria.cs
+++ LanosCertifiedStore.Persistence/Repositories/VehicleBrandRelated/QueryBuilderRelated/Common/Classes/VehicleBrandFilteringCriteria.cs
@@ -1,46 +1,45 @@
-﻿using System.Linq.Expressions;
-using Application.Contracts.RepositoryRelated.Common;
-using Application.Contracts.RequestParametersRelated;
-using Domain.Entities.VehicleRelated;
-using Persistence.QueryBuilder.Common;
-
-namespace Persistence.Repositories.VehicleBrandRelated.QueryBuilderRelated.Common.Classes;
-
-internal sealed class VehicleBrandFilteringCriteria 
-    : BaseFilteringCriteria<VehicleBrand, VehicleBrand, IVehicleBrandFilteringRequestParameters>
-{ 
-    internal override Expression<Func<VehicleBrand, bool>> GetCriteria(
-        IFilteringRequestParameters<VehicleBrand>? filteringRequestParameters)
-    {
-        if (filteringRequestParameters is not IVehicleBrandFilteringRequestParameters requestParameters)
-        {
-            return GetPredicate();
-        }
-
-        AddPredicateMethodsToList(requestParameters);
-
-        return GetPredicate(requestParameters);
-    }
-
-    private protected override void AddPredicateMethodsToList(
-        IVehicleBrandFilteringRequestParameters requestParameters)
-    {
-        if (!string.IsNullOrEmpty(requestParameters.Name))
-        {
-            PredicateDelegates.Add(GetBrandNamePredicate);
-        }
-
-        if (!string.IsNullOrEmpty(requestParameters.ContainedModelName))
-        {
-            PredicateDelegates.Add(GetContainedModelNamePredicate);
-        }
-    }
-
-    private Expression<Func<VehicleBrand, bool>> GetContainedModelNamePredicate(
-        IVehicleBrandFilteringRequestParameters requestParameters) =>
-        vehicleBrand => vehicleBrand.Models.Any(model => model.Name.Equals(requestParameters.ContainedModelName));
-
-    private Expression<Func<VehicleBrand, bool>> GetBrandNamePredicate(
-        IVehicleBrandFilteringRequestParameters requestParameters) =>
-        vehicleBrand => vehicleBrand.Name.Equals(requestParameters.Name);
-}
+﻿// using System.Linq.Expressions;
+// using Application.Contracts.RepositoryRelated.Common;
+// using Domain.Entities.VehicleRelated;
+// using Persistence.QueryBuilder.Common;
+//
+// namespace Persistence.Repositories.VehicleBrandRelated.QueryBuilderRelated.Common.Classes;
+//
+// internal sealed class VehicleBrandFilteringCriteria 
+//     : BaseFilteringCriteria<VehicleBrand, VehicleBrand, IVehicleBrandFilteringRequestParameters>
+// { 
+//     internal override Expression<Func<VehicleBrand, bool>> GetCriteria(
+//         IFilteringRequestParameters<VehicleBrand>? filteringRequestParameters)
+//     {
+//         if (filteringRequestParameters is not IVehicleBrandFilteringRequestParameters requestParameters)
+//         {
+//             return GetPredicate();
+//         }
+//
+//         AddPredicateMethodsToList(requestParameters);
+//
+//         return GetPredicate(requestParameters);
+//     }
+//
+//     private protected override void AddPredicateMethodsToList(
+//         IVehicleBrandFilteringRequestParameters requestParameters)
+//     {
+//         if (!string.IsNullOrEmpty(requestParameters.Name))
+//         {
+//             PredicateDelegates.Add(GetBrandNamePredicate);
+//         }
+//
+//         if (!string.IsNullOrEmpty(requestParameters.ContainedModelName))
+//         {
+//             PredicateDelegates.Add(GetContainedModelNamePredicate);
+//         }
+//     }
+//
+//     private Expression<Func<VehicleBrand, bool>> GetContainedModelNamePredicate(
+//         IVehicleBrandFilteringRequestParameters requestParameters) =>
+//         vehicleBrand => vehicleBrand.Models.Any(model => model.Name.Equals(requestParameters.ContainedModelName));
+//
+//     private Expression<Func<VehicleBrand, bool>> GetBrandNamePredicate(
+//         IVehicleBrandFilteringRequestParameters requestParameters) =>
+//         vehicleBrand => vehicleBrand.Name.Equals(requestParameters.Name);
+// }
--- LanosCertifiedStore.Persistence/Repositories/VehicleBrandRelated/QueryBuilderRelated/VehicleBrandQueryBuilder.cs
+++ LanosCertifiedStore.Persistence/Repositories/VehicleBrandRelated/QueryBuilderRelated/VehicleBrandQueryBuilder.cs
@@ -1,46 +1,39 @@
-﻿using Application.Contracts.RepositoryRelated.Common;
-using Application.Contracts.RequestParametersRelated;
-using Application.Enums.RequestParametersRelated;
-using Domain.Entities.VehicleRelated;
-using Persistence.QueryBuilder;
-using Persistence.Repositories.VehicleBrandRelated.QueryBuilderRelated.Common.Classes;
+﻿namespace Persistence.Repositories.VehicleBrandRelated.QueryBuilderRelated;
 
-namespace Persistence.Repositories.VehicleBrandRelated.QueryBuilderRelated;
-
-internal sealed class VehicleBrandQueryBuilder(
-    VehicleBrandSelectionProfiles selectionProfiles,
-    VehicleBrandFilteringCriteria brandFilteringCriteria)
-    : BaseQueryBuilder<VehicleBrandProjectionProfile,
-        VehicleBrand,
-        VehicleBrand,
-        IVehicleBrandFilteringRequestParameters>(
-        selectionProfiles,
-        brandFilteringCriteria)
-{
-    private protected override VehicleBrandSortingSettings GetQuerySortingSettings(
-        IFilteringRequestParameters<VehicleBrand>? filteringRequestParameters)
-    {
-        if (string.IsNullOrEmpty(filteringRequestParameters!.SortingType))
-        {
-            return new VehicleBrandSortingSettings
-            {
-                OrderByAscendingExpression = VehicleBrandSortingTypes.Options["default"]
-            };
-        }
-
-        var settings = new VehicleBrandSortingSettings();
-
-        if (filteringRequestParameters.SortingType.Contains("-asc"))
-        {
-            settings.OrderByAscendingExpression = VehicleBrandSortingTypes.Options
-                [filteringRequestParameters.SortingType];
-        }
-        else if (filteringRequestParameters.SortingType.Contains("-desc"))
-        {
-            settings.OrderByDescendingExpression = VehicleBrandSortingTypes.Options
-                [filteringRequestParameters.SortingType];
-        }
-
-        return settings;
-    }
-}
+// internal sealed class VehicleBrandQueryBuilder(
+//     VehicleBrandSelectionProfiles selectionProfiles,
+//     VehicleBrandFilteringCriteria brandFilteringCriteria)
+//     : BaseQueryBuilder<VehicleBrandProjectionProfile,
+//         VehicleBrand,
+//         VehicleBrand,
+//         IVehicleBrandFilteringRequestParameters>(
+//         selectionProfiles,
+//         brandFilteringCriteria)
+// {
+//     private protected override VehicleBrandSortingSettings GetQuerySortingSettings(
+//         IFilteringRequestParameters<VehicleBrand>? filteringRequestParameters)
+//     {
+//         if (string.IsNullOrEmpty(filteringRequestParameters!.SortingType))
+//         {
+//             return new VehicleBrandSortingSettings
+//             {
+//                 OrderByAscendingExpression = VehicleBrandSortingTypes.Options["default"]
+//             };
+//         }
+//
+//         var settings = new VehicleBrandSortingSettings();
+//
+//         if (filteringRequestParameters.SortingType.Contains("-asc"))
+//         {
+//             settings.OrderByAscendingExpression = VehicleBrandSortingTypes.Options
+//                 [filteringRequestParameters.SortingType];
+//         }
+//         else if (filteringRequestParameters.SortingType.Contains("-desc"))
+//         {
+//             settings.OrderByDescendingExpression = VehicleBrandSortingTypes.Options
+//                 [filteringRequestParameters.SortingType];
+//         }
+//
+//         return settings;
+//     }
+// }
