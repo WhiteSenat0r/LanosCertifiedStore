b583a176e96fac92dc921ca88c292e37ae5ae4a2
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-03 16:36:04 +0300

    Added SingleVehicleBrandDto.cs

---
+++ LanosCertifiedStore.Application/Dtos/BrandDtos/SingleVehicleBrandDto.cs
@@ -0,0 +1,6 @@
+﻿using Application.Dtos.Common;
+using Application.Dtos.ModelDtos;
+
+namespace Application.Dtos.BrandDtos;
+
+public sealed record SingleVehicleBrandDto(IEnumerable<ModelDto>? Models) : VehicleAspectDto;
