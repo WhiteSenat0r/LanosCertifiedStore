b9c5fb258e9c85baa1bf8ff276bf56cad3b93237
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-03 20:41:07 +0300

    CommandBase removed

--- LanosCertifiedStore.Persistence/Commands/Common/Classes/Constants/CommandConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Persistence.Commands.Common.Classes.Constants;
-
-internal static class CommandConstants
-{
-    public const string CommandExecutionErrorCode = "CommandExecution";
-    public const string CommandExecutionErrorMessage = "Error occured during the command execution!";
-}
