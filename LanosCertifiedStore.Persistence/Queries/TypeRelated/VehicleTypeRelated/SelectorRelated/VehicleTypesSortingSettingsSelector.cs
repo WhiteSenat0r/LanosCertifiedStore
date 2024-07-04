559e5d7970438fc063d2d2ac532e5d9d970defe3
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-04 14:06:41 +0300

    Added VehicleTypesSortingSettingsSelector.cs

---
+++ LanosCertifiedStore.Persistence/Queries/TypeRelated/VehicleTypeRelated/SelectorRelated/VehicleTypesSortingSettingsSelector.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+using Domain.Entities.VehicleRelated.TypeRelated;
+using Persistence.Queries.Common.Classes.SelectorBaseRelated;
+
+namespace Persistence.Queries.TypeRelated.VehicleTypeRelated.SelectorRelated;
+
+internal sealed class VehicleTypesSortingSettingsSelector : 
+    QuerySortingSettingsSelectorBase<VehicleType>
+{
+    private protected override IReadOnlyDictionary<string, Expression<Func<VehicleType, object>>> 
+        GetMappedSortingExpressions()
+    {
+        Expression<Func<VehicleType, object>> expression = vehicleBrand => vehicleBrand.Name;
+
+        return new Dictionary<string, Expression<Func<VehicleType, object>>>
+        {
+            { "name-asc", expression },
+            { "name-desc", expression },
+            { string.Empty, expression }
+        };
+    }
+}
