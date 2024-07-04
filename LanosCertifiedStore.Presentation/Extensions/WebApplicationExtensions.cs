e20fd59da849da32b0d0ef6505d6f0450022dd8b
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-04 16:59:53 +0300

    Added WebApplicationExtensions.cs

---
+++ LanosCertifiedStore.Presentation/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence.Contexts.ApplicationDatabaseContext;
+using Persistence.SeedingData;
+
+namespace API.Extensions;
+
+internal static class WebApplicationExtensions
+{
+    public static async Task ExecuteMigration(this WebApplication application)
+    {
+        if (application.Environment.IsDevelopment())
+        {
+            return;
+        }
+            
+        using var scope = application.Services.CreateScope();
+        var services = scope.ServiceProvider;
+
+        try
+        {
+            var context = services.GetRequiredService<ApplicationDatabaseContext>();
+            await context.Database.MigrateAsync();
+            await SeedData.Seed(context);
+        }
+
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occured during migration!");
+        }
+    }
+}
