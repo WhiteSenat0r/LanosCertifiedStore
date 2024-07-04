2cf924f14b47be9e9348cf904b224c053ffe0af5
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-04 14:09:08 +0300

    Removed vehicle type-related commands

--- LanosCertifiedStore.Application/CommandRequests/Types/VehicleTypeRelated/CreateType/CreateTypeCommand.cs
+++
@@ -1,6 +0,0 @@
-﻿using Application.Shared.ResultRelated;
-using MediatR;
-
-namespace Application.CommandRequests.Types.VehicleTypeRelated.CreateType;
-
-public sealed record CreateTypeCommand(string Name) : IRequest<Result<Unit>>;
--- LanosCertifiedStore.Application/CommandRequests/Types/VehicleTypeRelated/DeleteType/DeleteTypeCommand.cs
+++
@@ -1,6 +0,0 @@
-﻿using Application.Shared.ResultRelated;
-using MediatR;
-
-namespace Application.CommandRequests.Types.VehicleTypeRelated.DeleteType;
-
-public sealed record DeleteTypeCommand(Guid Id) : IRequest<Result<Unit>>;
