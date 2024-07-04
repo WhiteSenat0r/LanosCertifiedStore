7071271ce85a0d31b3a5b40e91b27ba6f50724d5
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-04 14:06:57 +0300

    Added CollectionVehicleTypesQuery.cs and CountVehicleTypesQuery.cs

---
+++ LanosCertifiedStore.Persistence/Queries/TypeRelated/VehicleTypeRelated/QueryRelated/CollectionVehicleTypesQuery.cs
@@ -0,0 +1,30 @@
+﻿using Application.Contracts.RequestRelated.QueryRelated;
+using Application.Dtos.TypeDtos;
+using AutoMapper;
+using Domain.Entities.VehicleRelated.TypeRelated;
+using Persistence.Contexts.ApplicationDatabaseContext;
+using Persistence.Queries.Common.Classes.QueryBaseRelated;
+using Persistence.Queries.Common.Contracts;
+
+namespace Persistence.Queries.TypeRelated.VehicleTypeRelated.QueryRelated;
+
+public sealed class CollectionVehicleTypesQuery(
+    ApplicationDatabaseContext context,
+    IQuerySortingSettingsSelector<VehicleType> sortingSettingsSelector,
+    IQueryPaginator queryPaginator,
+    IMapper mapper) : CollectionQueryBase<VehicleType, VehicleTypeDto>
+{
+    public override async Task<IReadOnlyCollection<VehicleTypeDto>> Execute<TRequestResult>(
+        IQueryRequest<VehicleType, TRequestResult> queryRequest, CancellationToken cancellationToken)
+    {
+        var queryable = GetDatabaseQueryable(context);
+
+        queryable = GetSortedQueryable(queryRequest, queryable, sortingSettingsSelector);
+
+        queryable = GetPaginatedQueryable(queryRequest, queryable, queryPaginator);
+
+        var executionResult = await GetQueryResult(queryable, mapper, cancellationToken);
+
+        return executionResult;
+    }
+}
