2ab18857da8917a89e52033b9b4adb5766f4f550
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-06 11:41:52 +0300

    Added CountVehicleTransmissionTypesQueryRequest.cs and its handler

---
+++ LanosCertifiedStore.Application/QueryRequests/TypesRelated/VehicleTransmissionTypeRelated/CountTransmissionTypesQueryRelated/CountVehicleTransmissionTypesQueryRequest.cs
@@ -0,0 +1,9 @@
+﻿using Application.Contracts.Common;
+using Application.Contracts.RequestRelated.QueryRelated;
+using Domain.Entities.VehicleRelated.TypeRelated;
+
+namespace Application.QueryRequests.TypesRelated.VehicleTransmissionTypeRelated.CountTransmissionTypesQueryRelated;
+
+public sealed record CountVehicleTransmissionTypesQueryRequest(
+    IFilteringRequestParameters<VehicleTransmissionType> FilteringParameters) : 
+    ICountQueryRequest<VehicleTransmissionType>;
---
+++ LanosCertifiedStore.Application/QueryRequests/TypesRelated/VehicleTransmissionTypeRelated/CountTransmissionTypesQueryRelated/CountVehicleTransmissionTypesQueryRequestHandler.cs
@@ -0,0 +1,20 @@
+﻿using Application.Contracts.ServicesRelated;
+using Application.Dtos.Common;
+using Application.Shared.ResultRelated;
+using MediatR;
+
+namespace Application.QueryRequests.TypesRelated.VehicleTransmissionTypeRelated.CountTransmissionTypesQueryRelated;
+
+internal sealed class CountVehicleTransmissionTypesQueryRequestHandler(
+    IVehicleTransmissionTypeService vehicleTransmissionTypeService) : 
+    IRequestHandler<CountVehicleTransmissionTypesQueryRequest, Result<ItemsCountDto>>
+{
+    public async Task<Result<ItemsCountDto>> Handle(CountVehicleTransmissionTypesQueryRequest request,
+        CancellationToken cancellationToken)
+    {
+        var itemsCountDto = await vehicleTransmissionTypeService
+            .GetVehicleTransmissionTypesCount(request, cancellationToken);
+
+        return Result<ItemsCountDto>.Success(itemsCountDto);
+    }
+}
