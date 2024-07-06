c4775cf7df27a2249d21135a5f762b9bbcad94fe
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-06 11:42:44 +0300

    Added IVehicleTransmissionTypeService.cs and its implementation

---
+++ LanosCertifiedStore.Application/Contracts/ServicesRelated/IVehicleTransmissionTypeService.cs
@@ -0,0 +1,17 @@
+﻿using Application.Dtos.Common;
+using Application.Dtos.TypeDtos;
+using Application.QueryRequests.TypesRelated.VehicleTransmissionTypeRelated.CollectionVehicleTransmissionTypesQueryRelated;
+using Application.QueryRequests.TypesRelated.VehicleTransmissionTypeRelated.CountTransmissionTypesQueryRelated;
+
+namespace Application.Contracts.ServicesRelated;
+
+public interface IVehicleTransmissionTypeService
+{
+    Task<IReadOnlyCollection<VehicleTransmissionTypeDto>> GetVehicleTransmissionTypeCollection(
+        CollectionVehicleTransmissionTypesQueryRequest queryRequest,
+        CancellationToken cancellationToken);
+
+    Task<ItemsCountDto> GetVehicleTransmissionTypesCount(
+        CountVehicleTransmissionTypesQueryRequest queryRequest,
+        CancellationToken cancellationToken);
+}
---
+++ LanosCertifiedStore.Infrastructure/Services/VehicleTransmissionTypeService.cs
@@ -0,0 +1,27 @@
+﻿using Application.Contracts.ServicesRelated;
+using Application.Dtos.Common;
+using Application.Dtos.TypeDtos;
+using Application.QueryRequests.TypesRelated.VehicleTransmissionTypeRelated.CollectionVehicleTransmissionTypesQueryRelated;
+using Application.QueryRequests.TypesRelated.VehicleTransmissionTypeRelated.CountTransmissionTypesQueryRelated;
+using Persistence.Queries.TypeRelated.VehicleTransmissionTypeRelated.QueryRelated;
+
+namespace LanosCertifiedStore.InfrastructureLayer.Services.Services;
+
+internal sealed class VehicleTransmissionTypeService(
+    CollectionVehicleTransmissionTypesQuery collectionVehicleTransmissionTypesQuery,
+    CountVehicleTransmissionTypesQuery countVehicleTransmissionTypesQuery) : IVehicleTransmissionTypeService
+{
+    public async Task<IReadOnlyCollection<VehicleTransmissionTypeDto>> GetVehicleTransmissionTypeCollection(
+        CollectionVehicleTransmissionTypesQueryRequest queryRequest,
+        CancellationToken cancellationToken)
+    {
+        return await collectionVehicleTransmissionTypesQuery.Execute(queryRequest, cancellationToken);
+    }
+
+    public async Task<ItemsCountDto> GetVehicleTransmissionTypesCount(
+        CountVehicleTransmissionTypesQueryRequest queryRequest,
+        CancellationToken cancellationToken)
+    {
+        return await countVehicleTransmissionTypesQuery.Execute(queryRequest, cancellationToken);
+    }
+}
