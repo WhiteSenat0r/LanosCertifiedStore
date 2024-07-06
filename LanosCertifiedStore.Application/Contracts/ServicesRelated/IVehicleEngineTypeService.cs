4f22f45e202dbc426a951cc4ecb39c6989be3e0b
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-06 12:18:09 +0300

    Added IVehicleEngineTypeService.cs

---
+++ LanosCertifiedStore.Application/Contracts/ServicesRelated/IVehicleEngineTypeService.cs
@@ -0,0 +1,17 @@
+﻿using Application.Dtos.Common;
+using Application.Dtos.TypeDtos;
+using Application.QueryRequests.TypesRelated.VehicleEngineTypeRelated.CollectionVehicleEngineTypesQueryRelated;
+using Application.QueryRequests.TypesRelated.VehicleEngineTypeRelated.CountVehicleEngineTypesQueryRelated;
+
+namespace Application.Contracts.ServicesRelated;
+
+public interface IVehicleEngineTypeService
+{
+    Task<IReadOnlyCollection<VehicleEngineTypeDto>> GetVehicleEngineTypeCollection(
+        CollectionVehicleEngineTypesQueryRequest queryRequest,
+        CancellationToken cancellationToken);
+
+    Task<ItemsCountDto> GetVehicleEngineTypesCount(
+        CountVehicleEngineTypesQueryRequest queryRequest,
+        CancellationToken cancellationToken);
+}
