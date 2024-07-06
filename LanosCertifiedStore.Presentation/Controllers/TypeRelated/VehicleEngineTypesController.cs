72d06a41b6aa354ea223b1bcb1783ac09d52d3bc
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-06 12:21:58 +0300

    Updated VehicleEngineTypesController.cs

--- LanosCertifiedStore.Presentation/Controllers/TypeRelated/VehicleEngineTypesController.cs
+++ LanosCertifiedStore.Presentation/Controllers/TypeRelated/VehicleEngineTypesController.cs
@@ -1,49 +1,36 @@
-﻿namespace API.Controllers.TypeRelated;
+﻿using API.Controllers.Common;
+using Application.Core.Results;
+using Application.Dtos.Common;
+using Application.Dtos.TypeDtos;
+using Application.QueryRequests.TypesRelated.VehicleEngineTypeRelated.CollectionVehicleEngineTypesQueryRelated;
+using Application.QueryRequests.TypesRelated.VehicleEngineTypeRelated.CountVehicleEngineTypesQueryRelated;
+using Application.RequestParameters.TypeRelated;
+using Microsoft.AspNetCore.Mvc;
 
-// public sealed class VehicleEngineTypesController : BaseModelRelatedApiController
-// {
-//     [HttpGet]
-//     [ProducesResponseType(typeof(PaginationResult<VehicleEngineTypeDto>), StatusCodes.Status200OK)]
-//     [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
-//     public async Task<ActionResult<PaginationResult<VehicleEngineTypeDto>>> GetTypes(
-//         [FromQuery] VehicleEngineTypeFilteringRequestParameters requestParameters)
-//     {
-//         return HandleResult(await Mediator.Send(new VehicleEngineTypesQuery(requestParameters)));
-//     }
-//     
-//     [HttpGet("countItems")]
-//     [ProducesResponseType(typeof(ItemsCountDto), StatusCodes.Status200OK)]
-//     [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
-//     [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
-//     public async Task<ActionResult<ItemsCountDto>> GetItemsCount(
-//         [FromQuery] VehicleEngineTypeFilteringRequestParameters requestParameters)
-//     {
-//         return HandleResult(await Mediator.Send(new CountEngineTypesQueryRequest(requestParameters)));
-//     }
-//
-//     [HttpPost]
-//     [ProducesResponseType(typeof(Unit), StatusCodes.Status200OK)]
-//     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
-//     [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
-//     public async Task<ActionResult> CreateVehicleTypeRelated([FromBody] CreateEngineTypeCommand createCommand)
-//     {
-//         return HandleResult(await Mediator.Send(createCommand));
-//     }
-//
-//     [HttpPut]
-//     [ProducesResponseType(typeof(Unit), StatusCodes.Status200OK)]
-//     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
-//     [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
-//     public async Task<ActionResult> UpdateVehicleTypeRelated([FromBody] UpdateEngineTypeCommand updateCommand)
-//     {
-//         return HandleResult(await Mediator.Send(updateCommand));
-//     }
-//
-//     [HttpDelete]
-//     [ProducesResponseType(typeof(Unit), StatusCodes.Status200OK)]
-//     [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
-//     public async Task<ActionResult> DeleteType([FromBody] DeleteEngineTypeCommand deleteCommand)
-//     {
-//         return HandleResult(await Mediator.Send(deleteCommand));
-//     }
-// }
+namespace API.Controllers.TypeRelated;
+
+[Route("api/EngineTypes")]
+public sealed class VehicleEngineTypesController : BaseApiController
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(PaginationResult<VehicleEngineTypeDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<PaginationResult<VehicleEngineTypeDto>>> GetTypes(
+        [FromQuery] VehicleEngineTypeFilteringRequestParameters requestParameters)
+    {
+        var result = await Mediator.Send(new CollectionVehicleEngineTypesQueryRequest(requestParameters));
+
+        return Ok(result.Value);
+    }
+
+    [HttpGet("CountItems")]
+    [ProducesResponseType(typeof(ItemsCountDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<ItemsCountDto>> GetItemsCount(
+        [FromQuery] VehicleEngineTypeFilteringRequestParameters requestParameters)
+    {
+        var result = await Mediator.Send(new CountVehicleEngineTypesQueryRequest(requestParameters));
+
+        return Ok(result.Value);
+    }
+}
