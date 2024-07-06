6c8ce092da6fcb35ac5dc40117bc493c18bb84cc
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-06 12:19:21 +0300

    Removed unused files

--- LanosCertifiedStore.Application/QueryRequests/TypesRelated/VehicleEngineTypeRelated/CountEngineTypesQueryRelated/CountEngineTypesQueryRequest.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace Application.QueryRequests.TypesRelated.VehicleEngineTypeRelated.CountEngineTypesQueryRelated;
-
-// public sealed record CountEngineTypesQueryRequest(IFilteringRequestParameters<VehicleEngineType> RequestParameters) : 
-//     CountItemsQueryRequestBase<VehicleEngineType>(RequestParameters), IRequest<Result<ItemsCountDto>>;
--- LanosCertifiedStore.Application/QueryRequests/TypesRelated/VehicleEngineTypeRelated/VehicleEngineTypesQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Application.QueryRequests.TypesRelated.VehicleEngineTypeRelated;
-
-// TODO
-// public sealed record VehicleEngineTypesQuery(IFilteringRequestParameters<VehicleEngineType> RequestParameters) :
-//     CollectionQueryRequestBase<VehicleEngineType, IFilteringRequestParameters<VehicleEngineType>>(RequestParameters),
-//     IRequest<Result<PaginationResult<VehicleEngineTypeDto>>>;
