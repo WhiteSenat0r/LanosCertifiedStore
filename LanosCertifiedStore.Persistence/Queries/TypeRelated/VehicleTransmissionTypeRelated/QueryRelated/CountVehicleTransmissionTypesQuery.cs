8cde94d3f1bd866ab1aabf926776153f4e1c24a5
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-06 11:45:30 +0300

    Added vehicle transmission type-related queries and selectors

---
+++ LanosCertifiedStore.Persistence/Queries/TypeRelated/VehicleTransmissionTypeRelated/QueryRelated/CountVehicleTransmissionTypesQuery.cs
@@ -0,0 +1,21 @@
+﻿using Application.Contracts.RequestRelated.QueryRelated;
+using Application.Dtos.Common;
+using Domain.Entities.VehicleRelated.TypeRelated;
+using Persistence.Contexts.ApplicationDatabaseContext;
+using Persistence.Queries.Common.Classes.QueryBaseRelated;
+
+namespace Persistence.Queries.TypeRelated.VehicleTransmissionTypeRelated.QueryRelated;
+
+public sealed class CountVehicleTransmissionTypesQuery(
+    ApplicationDatabaseContext context) : CountQueryBase<VehicleTransmissionType>
+{
+    public override async Task<ItemsCountDto> Execute<TRequestResult>(
+        IQueryRequest<VehicleTransmissionType, TRequestResult> queryRequest, CancellationToken cancellationToken)
+    {
+        var queryable = GetDatabaseQueryable(context);
+
+        var executionResult = await GetQueryResult(queryable, cancellationToken);
+
+        return executionResult;
+    }
+}
---
+++ LanosCertifiedStore.Persistence/Queries/TypeRelated/VehicleTransmissionTypeRelated/SelectorRelated/VehicleTransmissionTypesSortingSettingsSelector.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+using Domain.Entities.VehicleRelated.TypeRelated;
+using Persistence.Queries.Common.Classes.SelectorBaseRelated;
+
+namespace Persistence.Queries.TypeRelated.VehicleTransmissionTypeRelated.SelectorRelated;
+
+internal sealed class VehicleTransmissionTypesSortingSettingsSelector : 
+    QuerySortingSettingsSelectorBase<VehicleTransmissionType>
+{
+    private protected override IReadOnlyDictionary<string, Expression<Func<VehicleTransmissionType, object>>>
+        GetMappedSortingExpressions()
+    {
+        Expression<Func<VehicleTransmissionType, object>> expression = 
+            vehicleTransmissionType => vehicleTransmissionType.Name;
+
+        return new Dictionary<string, Expression<Func<VehicleTransmissionType, object>>>
+        {
+            { "name-asc", expression },
+            { "name-desc", expression },
+            { string.Empty, expression }
+        };
+    }
+}
