d1d542b2d17aec10bbcdfb4826dd30971c8fa38b
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-08 12:57:06 +0300

    Added CollectionLocationRegionsQueryRequest.cs and its handler

---
+++ LanosCertifiedStore.Application/QueryRequests/LocationsRelated/LocationRegionsRelated/CollectionLocationRegionsQueryRequestRelated/CollectionLocationRegionsQueryRequest.cs
@@ -0,0 +1,11 @@
+﻿using Application.Contracts.Common;
+using Application.Contracts.RequestRelated.QueryRelated;
+using Application.Core.Results;
+using Application.Dtos.LocationDtos;
+using Domain.Entities.VehicleRelated.LocationRelated;
+
+namespace Application.QueryRequests.LocationsRelated.LocationRegionsRelated.CollectionLocationRegionsQueryRequestRelated;
+
+public sealed record CollectionLocationRegionsQueryRequest(
+    IFilteringRequestParameters<VehicleLocationRegion> FilteringParameters)
+    : ICollectionQueryRequest<VehicleLocationRegion, PaginationResult<LocationRegionDto>, LocationRegionDto>;
