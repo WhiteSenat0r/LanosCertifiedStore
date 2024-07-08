4df857ea662b50b0baeb29c953bb47ad36908988
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-08 17:14:52 +0300

    Added CountLocationTownsQueryRequest.cs and its handler

---
+++ LanosCertifiedStore.Application/QueryRequests/LocationsRelated/LocationTownsRelated/CountLocationTownsQueryRelated/CountLocationTownsQueryRequestHandler.cs
@@ -0,0 +1,18 @@
+﻿using Application.Contracts.ServicesRelated.LocationRelated;
+using Application.Dtos.Common;
+using Application.Shared.ResultRelated;
+using MediatR;
+
+namespace Application.QueryRequests.LocationsRelated.LocationTownsRelated.CountLocationTownsQueryRelated;
+
+internal sealed class CountVehicleLocationTownsQueryRequestRequestHandler(ILocationTownService locationTownService) :
+    IRequestHandler<CountLocationTownsQueryRequest, Result<ItemsCountDto>>
+{
+    public async Task<Result<ItemsCountDto>> Handle(
+        CountLocationTownsQueryRequest request, CancellationToken cancellationToken)
+    {
+        var count = await locationTownService.GetLocationTownsCount(request, cancellationToken);
+
+        return Result<ItemsCountDto>.Success(count);
+    }
+}
