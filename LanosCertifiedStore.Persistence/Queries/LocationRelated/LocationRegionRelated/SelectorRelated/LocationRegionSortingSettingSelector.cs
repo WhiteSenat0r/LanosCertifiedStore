8915c571809f7bdd9bd4dd75fef1a25eefa19725
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-08 12:58:24 +0300

    Added CollectionLocationRegionsQuery.cs and its selector

---
+++ LanosCertifiedStore.Persistence/Queries/LocationRelated/LocationRegionRelated/SelectorRelated/LocationRegionSortingSettingSelector.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using Domain.Entities.VehicleRelated.LocationRelated;
+using Persistence.Queries.Common.Classes.SelectorBaseRelated;
+
+namespace Persistence.Queries.LocationRelated.LocationRegionRelated.SelectorRelated;
+
+internal sealed class LocationRegionSortingSettingSelector : QuerySortingSettingsSelectorBase<VehicleLocationRegion>
+{
+    private protected override IReadOnlyDictionary<string, Expression<Func<VehicleLocationRegion, object>>>
+        GetMappedSortingExpressions()
+    {
+        Expression<Func<VehicleLocationRegion, object>> expression = locationRegion => locationRegion.Name;
+
+        return new Dictionary<string, Expression<Func<VehicleLocationRegion, object>>>
+        {
+            { "name-asc", expression },
+            { "name-desc", expression },
+            { string.Empty, expression }
+        };
+    }
+}
