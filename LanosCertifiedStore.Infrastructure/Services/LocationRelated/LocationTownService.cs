9faca5b68591f75cb029e924ee764b9435e2fbe3
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-08 17:17:21 +0300

    Added LocationTownService.cs

---
+++ LanosCertifiedStore.Infrastructure/Services/LocationRelated/LocationTownService.cs
@@ -0,0 +1,25 @@
+﻿using Application.Contracts.ServicesRelated.LocationRelated;
+using Application.Dtos.Common;
+using Application.Dtos.LocationDtos;
+using Application.QueryRequests.LocationsRelated.LocationTownsRelated.CollectionLocationTownsQueryRequestRelated;
+using Application.QueryRequests.LocationsRelated.LocationTownsRelated.CountLocationTownsQueryRelated;
+using Persistence.Queries.LocationRelated.LocationTownRelated.QueryRelated;
+
+namespace LanosCertifiedStore.InfrastructureLayer.Services.Services.LocationRelated;
+
+internal sealed class LocationTownService(
+    CollectionLocationTownsQuery collectionQuery,
+    CountLocationTownsQuery countQuery) : ILocationTownService
+{
+    public async Task<IReadOnlyCollection<LocationTownDto>> GetLocationTownCollection(
+        CollectionLocationTownsQueryRequest request, CancellationToken cancellationToken)
+    {
+        return await collectionQuery.Execute(request, cancellationToken);
+    }
+
+    public async Task<ItemsCountDto> GetLocationTownsCount(
+        CountLocationTownsQueryRequest request, CancellationToken cancellationToken)
+    {
+        return await countQuery.Execute(request, cancellationToken);
+    }
+}
