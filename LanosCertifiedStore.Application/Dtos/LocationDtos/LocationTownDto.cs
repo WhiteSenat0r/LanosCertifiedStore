9aacf585bc946db2b1bac798d6894377c5eb50d5
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-08 16:55:42 +0300

    Updated LocationTownDto.cs

--- LanosCertifiedStore.Application/Dtos/LocationDtos/LocationTownDto.cs
+++ LanosCertifiedStore.Application/Dtos/LocationDtos/LocationTownDto.cs
@@ -2,8 +2,9 @@
 
 namespace Application.Dtos.LocationDtos;
 
-public sealed record TownDto : VehicleAspectDto
+public sealed record LocationTownDto : VehicleAspectDto
 {
     public string? RelatedRegion { get; init; }
     public string? RelatedArea { get; init; }
+    public string? TownType { get; init; }
 }
