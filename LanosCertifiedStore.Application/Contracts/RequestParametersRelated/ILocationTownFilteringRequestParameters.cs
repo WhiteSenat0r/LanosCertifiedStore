590061cb48f99adc787cf49914c82dc7c2316ade
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-08 16:57:25 +0300

    Added ILocationTownFilteringRequestParameters.cs

---
+++ LanosCertifiedStore.Application/Contracts/RequestParametersRelated/ILocationTownFilteringRequestParameters.cs
@@ -0,0 +1,10 @@
+﻿using Application.Contracts.Common;
+using Domain.Entities.VehicleRelated.LocationRelated;
+
+namespace Application.Contracts.RequestParametersRelated;
+
+public interface ILocationTownFilteringRequestParameters : IFilteringRequestParameters<VehicleLocationTown>
+{
+    Guid? LocationRegionId { get; }
+    Guid? LocationTownTypeId { get; }
+}
