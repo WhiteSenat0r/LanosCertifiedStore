b15fcf3c32428c6c32e633b39ea93a3597f567e1
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-08 17:18:33 +0300

    Added location town-related queries and required selectors

---
+++ LanosCertifiedStore.Persistence/Queries/LocationRelated/LocationTownRelated/QueryRelated/CollectionLocationTownsQuery.cs
@@ -0,0 +1,32 @@
+﻿using Application.Contracts.RequestRelated.QueryRelated;
+using Application.Dtos.LocationDtos;
+using AutoMapper;
+using Domain.Entities.VehicleRelated.LocationRelated;
+using Persistence.Contexts.ApplicationDatabaseContext;
+using Persistence.Queries.Common.Classes.QueryBaseRelated;
+using Persistence.Queries.Common.Contracts;
+
+namespace Persistence.Queries.LocationRelated.LocationTownRelated.QueryRelated;
+
+public sealed class CollectionLocationTownsQuery(
+    ApplicationDatabaseContext context,
+    IQueryFilteringCriteriaSelector<VehicleLocationTown> filteringCriteriaSelector,
+    IQuerySortingSettingsSelector<VehicleLocationTown> sortingSettingsSelector,
+    IQueryPaginator queryPaginator,
+    IMapper mapper) : CollectionQueryBase<VehicleLocationTown, LocationTownDto>
+{
+    public override async Task<IReadOnlyCollection<LocationTownDto>> Execute<TRequestResult>(
+        IQueryRequest<VehicleLocationTown, TRequestResult> queryRequest,
+        CancellationToken cancellationToken)
+    {
+        var queryable = GetDatabaseQueryable(context);
+
+        queryable = GetFilteredQueryable(queryRequest, queryable, filteringCriteriaSelector);
+        queryable = GetSortedQueryable(queryRequest, queryable, sortingSettingsSelector);
+        queryable = GetPaginatedQueryable(queryRequest, queryable, queryPaginator);
+
+        var executionResult = await GetQueryResult(queryable, mapper, cancellationToken);
+
+        return executionResult;
+    }
+}
---
+++ LanosCertifiedStore.Persistence/Queries/LocationRelated/LocationTownRelated/QueryRelated/CountLocationTownsQuery.cs
@@ -0,0 +1,25 @@
+﻿using Application.Contracts.RequestRelated.QueryRelated;
+using Application.Dtos.Common;
+using Domain.Entities.VehicleRelated.LocationRelated;
+using Persistence.Contexts.ApplicationDatabaseContext;
+using Persistence.Queries.Common.Classes.QueryBaseRelated;
+using Persistence.Queries.Common.Contracts;
+
+namespace Persistence.Queries.LocationRelated.LocationTownRelated.QueryRelated;
+
+public sealed class CountLocationTownsQuery(
+    ApplicationDatabaseContext context,
+    IQueryFilteringCriteriaSelector<VehicleLocationTown> filteringCriteriaSelector) : CountQueryBase<VehicleLocationTown>
+{
+    public override async Task<ItemsCountDto> Execute<TRequestResult>(
+        IQueryRequest<VehicleLocationTown, TRequestResult> queryRequest, CancellationToken cancellationToken)
+    {
+        var queryable = GetDatabaseQueryable(context);
+
+        var filteredQueryable = GetFilteredQueryable(queryRequest, queryable, filteringCriteriaSelector);
+        
+        var executionResult = await GetQueryResult(queryable, cancellationToken, filteredQueryable);
+
+        return executionResult;
+    }
+}
