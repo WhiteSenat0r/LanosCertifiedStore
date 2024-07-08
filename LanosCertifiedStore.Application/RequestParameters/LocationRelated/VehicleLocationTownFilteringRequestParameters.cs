5350ff717da974b98fdaf5f881e9809f5ee8be65
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-08 17:03:04 +0300

    Removed unused files

--- LanosCertifiedStore.Application/RequestParameters/LocationRelated/VehicleLocationTownFilteringRequestParameters.cs
+++ LanosCertifiedStore.Application/RequestParameters/LocationRelated/VehicleLocationTownFilteringRequestParameters.cs
@@ -1,6 +1,12 @@
-﻿using Application.RequestParameters.Common.Classes;
+﻿using Application.Contracts.RequestParametersRelated;
+using Application.RequestParameters.Common.Classes;
 using Domain.Entities.VehicleRelated.LocationRelated;
 
 namespace Application.RequestParameters.LocationRelated;
 
-public sealed class VehicleLocationTownFilteringRequestParameters : BaseFilteringRequestParameters<VehicleLocationTown>;
+public sealed class VehicleLocationTownFilteringRequestParameters : 
+    BaseFilteringRequestParameters<VehicleLocationTown>, ILocationTownFilteringRequestParameters
+{
+    public Guid? LocationRegionId { get; init; }
+    public Guid? LocationTownTypeId { get; init; }
+}
