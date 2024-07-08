bef4871cd3582783726908e92e43b69ef3054138
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-08 12:56:20 +0300

    Updated LocationAreaDto.cs and LocationRegionDto.cs

--- LanosCertifiedStore.Application/Dtos/LocationDtos/LocationRegionDto.cs
+++ LanosCertifiedStore.Application/Dtos/LocationDtos/LocationRegionDto.cs
@@ -2,4 +2,4 @@
 
 namespace Application.Dtos.LocationDtos;
 
-public sealed record LocationAreaDto : VehicleAspectDto;
+public sealed record LocationRegionDto : VehicleAspectDto;
--- LanosCertifiedStore.Application/Dtos/LocationDtos/RegionDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Application.Dtos.Common;
-
-namespace Application.Dtos.LocationDtos;
-
-public sealed record RegionDto : VehicleAspectDto
-{
-    public ICollection<string>? RelatedAreas { get; init; }
-    public ICollection<string>? RelatedTowns { get; init; }
-}
