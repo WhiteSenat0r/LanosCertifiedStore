c6a4196e074af995a765b067ef775ad27b9e4720
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-09 16:10:20 +0300

    PersistenceServiceCOllectionExtensions refactored for automated registering services to DI

--- src/LanosCertifiedStore.Persistence/Extensions/PersistenceServiceCollectionExtensions.cs
+++ src/LanosCertifiedStore.Persistence/Extensions/PersistenceServiceCollectionExtensions.cs
@@ -1,11 +1,13 @@
-﻿using Application.Shared;
+﻿using System.Reflection;
+using Application.Shared;
 using Application.Shared.ValidationRelated;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Persistence.Contexts.ApplicationDatabaseContext;
-using Persistence.Extensions.CommandRelated;
-using Persistence.Extensions.QueryRelated;
+using Persistence.Queries.Common.Classes;
+using Persistence.Queries.Common.Contracts;
 using Persistence.Services;
 
 namespace Persistence.Extensions;
@@ -20,12 +22,68 @@ public static class PersistenceServiceCollectionExtensions
                 o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery));
         });
 
-        services.AddQueryRelatedServices();
-        services.AddCommandRelatedServices();
+        services.AddCommandsAndQueriesRelatedServices();
+        services.AddServicesForInterface(typeof(IQuerySortingSettingsSelector<>));
+        services.AddServicesForInterface(typeof(IQueryFilteringCriteriaSelector<>));
+
+        services.AddCommonServices();
 
         services.AddScoped<ITransactionService, TransactionService>();
         services.AddScoped<IValidationHelper, ValidationHelper>();
 
         return services;
+    }
+
+    private static IServiceCollection AddCommonServices(this IServiceCollection services)
+    {
+        services.AddScoped<IQueryPaginator, QueryPaginator>();
+        return services;
+    }
+
+    private static IServiceCollection AddServicesForInterface(this IServiceCollection services, Type interfaceType)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var requests = GetClassesImplementingInterface(assembly, interfaceType);
+
+        var serviceDescriptor = requests
+            .Select(x =>
+            {
+                var implementedInterfaceType = x.GetInterfaces().First();
+                return new ServiceDescriptor(implementedInterfaceType, x, ServiceLifetime.Transient);
+            });
+
+        services.TryAdd(serviceDescriptor);
+
+        return services;
+    }
+
+    private static IServiceCollection AddCommandsAndQueriesRelatedServices(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        var serviceDescriptors = assembly.ExportedTypes
+            .Where(type => !type.IsInterface && !type.IsAbstract)
+            .Where(type => type.Name.EndsWith("Query") || type.Name.EndsWith("Command"))
+            .Select(type => new ServiceDescriptor(type, type, ServiceLifetime.Transient));
+
+        services.TryAdd(serviceDescriptors);
+
+        return services;
+    }
+
+    private static List<Type> GetClassesImplementingInterface(Assembly assembly, Type typeToMatch)
+    {
+        return assembly.GetTypes()
+            .Where(type =>
+            {
+                var genericInterfaceTypes = type.GetInterfaces()
+                    .Where(x => x.IsGenericType)
+                    .ToList();
+
+                var implementRequestType = genericInterfaceTypes
+                    .Any(x => x.GetGenericTypeDefinition() == typeToMatch);
+
+                return !type.IsInterface && !type.IsAbstract && implementRequestType;
+            }).ToList();
     }
 }
