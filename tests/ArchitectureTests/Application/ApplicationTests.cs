fc0fcf2ec0dfbac109bb1546d52d18a585b42e7b
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-09 22:52:03 +0300

    Application and Persistence arch tests added

--- tests/ArchitectureTests/Application/ApplicationTests.cs
+++ tests/ArchitectureTests/Application/ApplicationTests.cs
@@ -1,23 +1,58 @@
 ﻿using System.Reflection;
 using Application.Shared.RequestRelated;
+using Application.Shared.RequestRelated.QueryRelated;
 using MediatR;
 
 namespace ArchitectureTests.Application;
 
 public sealed class ApplicationTests
 {
-    private static readonly Assembly DomainAssembly = typeof(ICommandRequest<>).Assembly;
+    private static readonly Assembly ApplicationAssembly = typeof(ICommandRequest<>).Assembly;
 
     [Fact]
     public void RequestHandlersNames_Should_EndWithRequestHandler()
     {
-        var handlerTypes = Types.InAssembly(DomainAssembly)
+        var handlerTypes = GetTypesImplementingInterfaceNotEndingWith(typeof(IRequestHandler<,>), "RequestHandler");
+
+        handlerTypes.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void RequestHandlers_Should_BeInternalAndSealed()
+    {
+        var types = Types.InAssembly(ApplicationAssembly)
             .That()
             .ImplementInterface(typeof(IRequestHandler<,>))
-            .GetTypes()
-            .Where(handlerType => !handlerType.Name.EndsWith("RequestHandler"));
+            .And().ArePublic()
+            .And().AreNotSealed()
+            .GetTypes();
 
-        handlerTypes.Should()
-            .BeEmpty();
+        types.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void CommandRequestsNames_Should_EndWithCommandRequest()
+    {
+        var handlerTypes = GetTypesImplementingInterfaceNotEndingWith(typeof(ICommandRequest<>), "CommandRequest");
+
+        handlerTypes.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void QueryRequestsNames_Should_EndWithQueryRequest()
+    {
+        var handlerTypes = GetTypesImplementingInterfaceNotEndingWith(typeof(IQueryRequest<,>), "QueryRequest");
+
+        handlerTypes.Should().BeEmpty();
+    }
+
+    private static IEnumerable<Type> GetTypesImplementingInterfaceNotEndingWith(Type interfaceType, string endsWith)
+    {
+        return Types.InAssembly(ApplicationAssembly)
+            .That()
+            .ImplementInterface(interfaceType)
+            .GetTypes()
+            .ThatAreClasses()
+            .Where(handlerType => !handlerType.Name.EndsWith(endsWith));
     }
 }
---
+++ tests/ArchitectureTests/Persistence/PersistenceTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Persistence.Contexts.ApplicationDatabaseContext;
+using Persistence.Queries.Common.Classes.QueryBaseRelated;
+
+namespace ArchitectureTests.Persistence;
+
+public sealed class PersistenceTests
+{
+    private static readonly Assembly PersistenceAssembly = typeof(ApplicationDatabaseContext).Assembly;
+
+    [Fact]
+    public void QueriesNames_Should_EndWithQuery()
+    {
+        var types = Types.InAssembly(PersistenceAssembly)
+            .That()
+            .Inherit(typeof(CollectionQueryBase<,>))
+            .Or()
+            .Inherit(typeof(CountQueryBase<>))
+            .Or()
+            .Inherit(typeof(SingleQueryBase<,>))
+            .GetTypes()
+            .Where(type => !type.Name.EndsWith("Query"));
+
+        types.Should()
+            .BeEmpty();
+    }
+}
