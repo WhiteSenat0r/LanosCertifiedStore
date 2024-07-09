ed17aebf1acf8025a2e754e0638b18dfce42ea6b
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-09 21:35:50 +0300

    RequestHandlersNames_Should_EndWithRequestHandler test added

---
+++ tests/ArchitectureTests/Application/ApplicationTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Application.Shared.RequestRelated;
+using MediatR;
+
+namespace ArchitectureTests.Application;
+
+public sealed class ApplicationTests
+{
+    private static readonly Assembly DomainAssembly = typeof(ICommandRequest<>).Assembly;
+
+    [Fact]
+    public void RequestHandlersNames_Should_EndWithRequestHandler()
+    {
+        var handlerTypes = Types.InAssembly(DomainAssembly)
+            .That()
+            .ImplementInterface(typeof(IRequestHandler<,>))
+            .GetTypes()
+            .Where(handlerType => !handlerType.Name.EndsWith("RequestHandler"));
+
+        handlerTypes.Should()
+            .BeEmpty();
+    }
+}
