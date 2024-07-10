5bf7cf7d4dc9bad110bd9386768a532aa43ef8fb
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-10 22:46:12 +0300

    Added vehicle model-related queries and selectors

---
+++ src/LanosCertifiedStore.Persistence/Queries/VehicleModelRelated/QueryRelated/CollectionBrandlessVehicleModelsQuery.cs
@@ -0,0 +1,20 @@
+﻿using Application.VehicleModels.Dtos;
+using AutoMapper;
+using Domain.Entities.VehicleRelated;
+using Persistence.Contexts.ApplicationDatabaseContext;
+using Persistence.Queries.Common.Contracts;
+using Persistence.Queries.VehicleModelRelated.QueryRelated.Common;
+
+namespace Persistence.Queries.VehicleModelRelated.QueryRelated;
+
+public sealed class CollectionBrandlessVehicleModelsQuery(
+    ApplicationDatabaseContext context,
+    IQueryFilteringCriteriaSelector<VehicleModel> filteringCriteriaSelector,
+    IQuerySortingSettingsSelector<VehicleModel> sortingSettingsSelector,
+    IQueryPaginator queryPaginator,
+    IMapper mapper) : CollectionVehicleModelsQueryBase<VehicleModelWithoutBrandNameDto>(
+    context, 
+    filteringCriteriaSelector,
+    sortingSettingsSelector,
+    queryPaginator,
+    mapper);
---
+++ src/LanosCertifiedStore.Persistence/Queries/VehicleModelRelated/QueryRelated/CollectionVehicleModelsQuery.cs
@@ -0,0 +1,20 @@
+﻿using Application.VehicleModels.Dtos;
+using AutoMapper;
+using Domain.Entities.VehicleRelated;
+using Persistence.Contexts.ApplicationDatabaseContext;
+using Persistence.Queries.Common.Contracts;
+using Persistence.Queries.VehicleModelRelated.QueryRelated.Common;
+
+namespace Persistence.Queries.VehicleModelRelated.QueryRelated;
+
+public sealed class CollectionVehicleModelsQuery(
+    ApplicationDatabaseContext context,
+    IQueryFilteringCriteriaSelector<VehicleModel> filteringCriteriaSelector,
+    IQuerySortingSettingsSelector<VehicleModel> sortingSettingsSelector,
+    IQueryPaginator queryPaginator,
+    IMapper mapper) : CollectionVehicleModelsQueryBase<VehicleModelDto>(
+    context, 
+    filteringCriteriaSelector,
+    sortingSettingsSelector,
+    queryPaginator,
+    mapper);
