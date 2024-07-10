b0f2178de88d9bf3f7942e404065082e61a79851
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-10 22:42:18 +0300

    Added VehicleModelValidatorMessages.cs

---
+++ src/LanosCertifiedStore.Application/VehicleModels/VehicleModelValidatorMessages.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.VehicleModels;
+
+internal static class VehicleModelValidatorMessages
+{
+    public const string InvalidNameValue = "Name must be greater than 2 characters and less than 64!";
+    public const string AlreadyExistingNameValue = "Vehicle brand with such name already exists!";
+    public const string InvalidMinimalProductionYearValue = "Minimal production year must be greater or equal 1900!";
+    public const string InvalidBrandIdValue = "Brand with such ID does not exist!";
+    public const string InvalidTypeIdValue = "Type with such ID does not exist!";
+    public const string TooBigMinimalProductionYearValue = "Minimal production year must not be greater than maximum one!";
+    public const string TooSmallMaximumProductionYearValue = "Maximum production year must not be lesser than minimum one!";
+}
