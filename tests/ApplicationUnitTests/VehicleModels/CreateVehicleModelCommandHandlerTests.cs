9639f9c9800c75aeddb3baa58386118d0d36dfcd
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-11 15:51:59 +0300

    CreateVehicleModelCommandRequest tests added

---
+++ tests/ApplicationUnitTests/VehicleModels/CreateVehicleModelCommandHandlerTests.cs
@@ -0,0 +1,51 @@
+﻿using Application.VehicleModels;
+using Application.VehicleModels.Commands.CreateVehicleModelRelated;
+
+namespace ApplicationUnitTests.VehicleModels;
+
+public sealed class CreateVehicleModelCommandHandlerTests
+{
+    private readonly IVehicleModelService _vehicleModelService = Substitute.For<IVehicleModelService>();
+    private readonly CreateVehicleModelCommandRequestHandler _requestHandler;
+    private readonly CreateVehicleModelCommandRequest _request = VehicleModelTestExemplars.Regular();
+
+    public CreateVehicleModelCommandHandlerTests()
+    {
+        _requestHandler = new CreateVehicleModelCommandRequestHandler(_vehicleModelService);
+    }
+
+    [Fact]
+    public async Task Handler_ShouldInvokeAddNewVehicleModel_FromVehicleModelService()
+    {
+        // Act
+        await _requestHandler.Handle(_request, default);
+
+        // Assert
+        await _vehicleModelService
+            .Received()
+            .AddNewVehicleModel(_request, default!);
+    }
+
+    [Fact]
+    public async Task Handler_ShouldReturnCreatedBrandId()
+    {
+        // Arrange
+        var expectedId = Guid.NewGuid();
+
+        _vehicleModelService.AddNewVehicleModel(_request, default)
+            .Returns(expectedId);
+
+        // Act
+        var result = await _requestHandler.Handle(_request, default);
+
+        // Assert
+        result.IsSuccess
+            .Should().BeTrue();
+
+        result.Value
+            .Should()
+            .Be(expectedId);
+    }
+
+
+}
