aa4b7fe7ca0fb730a1904f21d3f8497a42976233
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-11 16:02:50 +0300

    Added UpdateVehicleModelCommand.cs

---
+++ src/LanosCertifiedStore.Persistence/Commands/VehicleModelRelated/UpdateVehicleModelCommand.cs
@@ -0,0 +1,101 @@
+﻿using Application.VehicleModels.Commands.UpdateVehicleModelRelated;
+using Domain.Contracts.Common;
+using Domain.Entities.VehicleRelated;
+using Domain.Entities.VehicleRelated.TypeRelated;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Contexts.ApplicationDatabaseContext;
+
+namespace Persistence.Commands.VehicleModelRelated;
+
+public sealed class UpdateVehicleModelCommand(ApplicationDatabaseContext context)
+{
+    public async Task Execute(UpdateVehicleModelCommandRequest request)
+    {
+        var updatedModel = await context.Set<VehicleModel>()
+            .Include(model => model.AvailableDrivetrainTypes)
+            .Include(model => model.AvailableEngineTypes)
+            .Include(model => model.AvailableBodyTypes)
+            .Include(model => model.AvailableTransmissionTypes)
+            .SingleOrDefaultAsync(m => m.Id.Equals(request.Id));
+
+        if (updatedModel is null)
+        {
+            ThrowNotFoundException<VehicleModel>(request.Id);
+        }
+
+        await UpdateModelAspects(request, updatedModel!);
+
+        context.Set<VehicleModel>().Update(updatedModel!);
+    }
+
+    private async Task UpdateModelAspects(UpdateVehicleModelCommandRequest request, VehicleModel updatedModel)
+    {
+        await UpdateAspectCollection<VehicleBodyType>(
+            request.AvailableBodyTypesIds, updatedModel, nameof(updatedModel.AvailableBodyTypes));
+        
+        await UpdateAspectCollection<VehicleEngineType>(
+            request.AvailableEngineTypesIds, updatedModel, nameof(updatedModel.AvailableEngineTypes));
+        
+        await UpdateAspectCollection<VehicleDrivetrainType>(
+            request.AvailableDrivetrainTypesIds, updatedModel, nameof(updatedModel.AvailableDrivetrainTypes));
+        
+        await UpdateAspectCollection<VehicleTransmissionType>(
+            request.AvailableTransmissionTypesIds, updatedModel, nameof(updatedModel.AvailableTransmissionTypes));
+
+        if (IsNewMaximumProductionYearAssignable(request, updatedModel))
+        {
+            updatedModel.MaximumProductionYear = request.MaximumProductionYear;
+        }
+    }
+
+    private async Task UpdateAspectCollection<TEntity>(
+        IEnumerable<Guid> ids, VehicleModel updatedModel, string propertyName) 
+        where TEntity : class, IIdentifiable<Guid>
+    {
+        var aspects = await GetAspectCollection<TEntity>(ids);
+        
+        updatedModel.GetType().GetProperty(propertyName)!.SetValue(updatedModel, aspects);
+    }
+
+    private bool IsNewMaximumProductionYearAssignable(
+        UpdateVehicleModelCommandRequest request,
+        VehicleModel updatedModel)
+    {
+        var isMaximumYearGreaterThanMinimum = 
+            request.MaximumProductionYear!.Value >= updatedModel.MinimalProductionYear;
+        
+        if (request.MaximumProductionYear.HasValue && updatedModel.MaximumProductionYear.HasValue)
+        {
+            return !updatedModel.MaximumProductionYear.Value.Equals(request.MaximumProductionYear.Value) &&
+                   isMaximumYearGreaterThanMinimum;
+        }
+        
+        return isMaximumYearGreaterThanMinimum;
+    }
+
+    private async Task<ICollection<TEntity>> GetAspectCollection<TEntity>(IEnumerable<Guid> identifiers)
+        where TEntity : class, IIdentifiable<Guid>
+    {
+        var items = new List<TEntity>();
+        var set = context.Set<TEntity>();
+
+        foreach (var id in identifiers)
+        {
+            var item = await set.FindAsync(id);
+
+            if (item is null)
+            {
+                ThrowNotFoundException<TEntity>(id);
+            }
+            
+            items.Add(item!);
+        }
+
+        return items;
+    }
+    
+    private void ThrowNotFoundException<TEntity>(Guid id) where TEntity : class, IIdentifiable<Guid>
+    {
+        throw new KeyNotFoundException($"{typeof(TEntity).Name} with ID {id} was not found!");
+    }
+}
