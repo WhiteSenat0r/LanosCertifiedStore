1f52ef1d499920471cbb481f09f8530adf2fb153
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-12 20:12:25 +0300

    Added Collection attribute to IntegrationTestBase

--- tests/IntegrationTests/Common/IntegrationTestBase.cs
+++ tests/IntegrationTests/Common/IntegrationTestBase.cs
@@ -4,16 +4,24 @@ using Persistence.Contexts.ApplicationDatabaseContext;
 
 namespace IntegrationTests.Common;
 
-public abstract class IntegrationTestBase : IClassFixture<IntegrationTestsWebApplicationFactory>
+[Collection(CollectionName)]
+public abstract class IntegrationTestBase : IClassFixture<IntegrationTestsWebApplicationFactory>, IDisposable
 {
     private readonly IServiceScope _scope;
     private protected readonly ISender Sender;
     private protected readonly ApplicationDatabaseContext Context;
 
+    protected const string CollectionName = "Integration tests collection";
     private protected IntegrationTestBase(IntegrationTestsWebApplicationFactory factory)
     {
         _scope = factory.Services.CreateScope();
         Sender = _scope.ServiceProvider.GetRequiredService<ISender>();
         Context = _scope.ServiceProvider.GetRequiredService<ApplicationDatabaseContext>();
+    }
+
+    public void Dispose()
+    {
+        _scope.Dispose();
+        Context.Dispose();
     }
 }
