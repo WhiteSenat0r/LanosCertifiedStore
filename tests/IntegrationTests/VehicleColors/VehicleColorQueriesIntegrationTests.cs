144fa77b2fb9818e001d1f8d8e867001cf7223b7
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-12 22:51:41 +0300

    Added VehicleColorQueriesIntegrationTests.cs

---
+++ tests/IntegrationTests/VehicleColors/VehicleColorQueriesIntegrationTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using Application.Shared.RequestParamsRelated;
+using Application.VehicleColors;
+using Application.VehicleColors.Queries.CollectionVehicleColorsQueryRequestRelated;
+using IntegrationTests.Common;
+
+namespace IntegrationTests.VehicleColors;
+
+public sealed class VehicleColorQueriesIntegrationTests(
+    IntegrationTestsWebApplicationFactory factory) : IntegrationTestBase(factory)
+{
+    [Fact]
+    public async Task Send_CollectionRequest_Should_ReturnCollectionOfColors()
+    {
+        // Arrange
+        var filteringRequestParameters = new VehicleColorFilteringRequestParameters
+        {
+            ItemQuantity = ItemQuantitySelection.Ten,
+            SortingType = "name-asc",
+            PageIndex = 1
+        };
+        var queryRequest = new CollectionVehicleColorsQueryRequest(filteringRequestParameters);
+
+        // Act
+        var response = await Sender.Send(queryRequest);
+        var colors = response.Value!.Items;
+
+        // Assert
+        response.Error.Should().BeNull();
+        response.IsSuccess.Should().BeTrue();
+
+        colors.Should().BeInAscendingOrder(
+            b => b.Name, 
+            StringComparer.Create(new CultureInfo("uk-UA"), ignoreCase: true));
+        colors.Count.Should().Be((int)ItemQuantitySelection.Ten);
+    }
+}
