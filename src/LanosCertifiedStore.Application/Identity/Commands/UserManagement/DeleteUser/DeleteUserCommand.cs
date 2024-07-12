069e9bac28436bf9328e76ccded037477a61dd30
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-12 15:28:27 +0300

    Removed identity-related commands

--- src/LanosCertifiedStore.Application/Identity/Commands/UserManagement/DeleteUser/DeleteUserCommand.cs
+++
@@ -1,6 +0,0 @@
-﻿using Application.Shared.ResultRelated;
-using MediatR;
-
-namespace Application.Identity.Commands.UserManagement.DeleteUser;
-
-public sealed record DeleteUserCommand(Guid Id) : IRequest<Result<Unit>>;
