9f46c53f08f23f477714545c34de44dc21505db3
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-12 13:03:41 +0300

    Update BaseIntegrationTest.cs

--- tests/IntegrationTests/BaseIntegrationTest.cs
+++ tests/IntegrationTests/BaseIntegrationTest.cs
@@ -1,16 +1,18 @@
 ﻿using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts.ApplicationDatabaseContext;
 
 namespace IntegrationTests;
 
 public abstract class BaseIntegrationTest : IClassFixture<IntegrationTestsWebApplicationFactory>
 {
     private readonly IServiceScope _scope;
-    private protected readonly ISender Sender; 
+    private protected readonly ISender Sender;
+    private protected readonly ApplicationDatabaseContext Context;
     public BaseIntegrationTest(IntegrationTestsWebApplicationFactory factory)
     {
         _scope = factory.Services.CreateScope();
         Sender = _scope.ServiceProvider.GetRequiredService<ISender>();
-
+        Context = _scope.ServiceProvider.GetRequiredService<ApplicationDatabaseContext>();
     }
 }
