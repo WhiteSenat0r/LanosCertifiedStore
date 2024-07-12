2199ee23cdb275af2dde69a2b3104db14204ce83
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-12 16:48:30 +0300

    Added VehicleBrandCommandIntegrationTests.cs

---
+++ tests/IntegrationTests/VehicleBrands/VehicleBrandCommandIntegrationTests.cs
@@ -0,0 +1,66 @@
+﻿using Application.VehicleBrands.Commands.CreateVehicleBrandRelated;
+using Application.VehicleBrands.Commands.UpdateVehicleBrandRelated;
+using Domain.Entities.VehicleRelated;
+using FluentAssertions;
+using IntegrationTests.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntegrationTests.VehicleBrands;
+
+public sealed class VehicleBrandCommandIntegrationTests(
+    IntegrationTestsWebApplicationFactory factory) : IntegrationTestBase(factory)
+{
+    private const string NewBrandName = "Name";
+    
+    [Fact]
+    public async Task CreateVehicleBrandCommandRequestHandler_ShouldAddNewBrand_IfRequestIsValid()
+    {
+        // Arrange
+        var commandRequest = new CreateVehicleBrandCommandRequest(NewBrandName);
+
+        // Act
+        var response = await Sender.Send(commandRequest);
+        var createdBrand = await Context.FindAsync<VehicleBrand>(response.Value);
+
+        // Assert
+        response.Error.Should().BeNull();
+        response.IsSuccess.Should().BeTrue();
+        response.Value.Should().NotBeEmpty();
+        
+        createdBrand.Should().NotBeNull();
+        createdBrand!.Name.Should().Be(NewBrandName);
+    }
+    
+    [Fact]
+    public async Task UpdateVehicleBrandCommandRequestHandler_ShouldUpdateExistingBrand_IfRequestIsValid()
+    {
+        // Arrange
+        var brand = await Context.Set<VehicleBrand>().SingleAsync(b => b.Name.Equals(NewBrandName));
+        var commandRequest = new UpdateVehicleBrandCommandRequest(brand.Id, brand.Id.ToString());
+
+        // Act
+        var response = await Sender.Send(commandRequest);
+        var updatedBrand = await Context.FindAsync<VehicleBrand>(brand.Id);
+
+        // Assert
+        response.Error.Should().BeNull();
+        response.IsSuccess.Should().BeTrue();
+        
+        updatedBrand.Should().NotBeNull();
+        updatedBrand!.Name.Should().NotBe(NewBrandName);
+    }
+    
+    [Fact]
+    public async Task UpdateVehicleBrandCommandRequestHandler_ShouldNotUpdate_NonExistingBrand()
+    {
+        // Arrange
+        var commandRequest = new UpdateVehicleBrandCommandRequest(Guid.Empty, NewBrandName);
+
+        // Act
+        var response = await Sender.Send(commandRequest);
+
+        // Assert
+        response.Error.Should().NotBeNull();
+        response.IsSuccess.Should().NotBe(true);
+    }
+}
