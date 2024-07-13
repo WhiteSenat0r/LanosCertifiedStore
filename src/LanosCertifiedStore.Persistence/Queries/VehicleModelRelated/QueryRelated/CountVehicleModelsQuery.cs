c62116bd4a85b42c72065eaf5d615d19e38d5c6b
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-13 16:18:28 +0300

    added filtereqdQUeryable counting in CountVehicleModelsQuery

--- src/LanosCertifiedStore.Persistence/Queries/VehicleModelRelated/QueryRelated/CountVehicleModelsQuery.cs
+++ src/LanosCertifiedStore.Persistence/Queries/VehicleModelRelated/QueryRelated/CountVehicleModelsQuery.cs
@@ -3,17 +3,22 @@ using Application.Shared.RequestRelated.QueryRelated;
 using Domain.Entities.VehicleRelated;
 using Persistence.Contexts.ApplicationDatabaseContext;
 using Persistence.Queries.Common.Classes.QueryBaseRelated;
+using Persistence.Queries.Common.Contracts;
 
 namespace Persistence.Queries.VehicleModelRelated.QueryRelated;
 
-public sealed class CountVehicleModelsQuery(ApplicationDatabaseContext context) : CountQueryBase<VehicleModel>
+public sealed class CountVehicleModelsQuery(
+    ApplicationDatabaseContext context,
+    IQueryFilteringCriteriaSelector<VehicleModel> filteringCriteriaSelector) : CountQueryBase<VehicleModel>
 {
     public override async Task<ItemsCountDto> Execute<TRequestResult>(
         IQueryRequest<VehicleModel, TRequestResult> queryRequest, CancellationToken cancellationToken)
     {
         var totalQueryable = GetDatabaseQueryable(context);
 
-        var executionResult = await GetQueryResult(totalQueryable, cancellationToken);
+        var filteredQueryable = GetFilteredQueryable(queryRequest, totalQueryable, filteringCriteriaSelector);
+
+        var executionResult = await GetQueryResult(totalQueryable, cancellationToken, filteredQueryable);
 
         return executionResult;
     }
