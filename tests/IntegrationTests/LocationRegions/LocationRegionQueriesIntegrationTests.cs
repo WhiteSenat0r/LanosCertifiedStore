53eb97b1861f4b493ce74214b096aa7b3fe45903
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-13 11:16:57 +0300

    Added LocationRegionQueriesIntegrationTests.cs

---
+++ tests/IntegrationTests/LocationRegions/LocationRegionQueriesIntegrationTests.cs
@@ -0,0 +1,38 @@
+﻿using Application.LocationRegions;
+using Application.LocationRegions.Queries.CollectionLocationRegionsQueryRequestRelated;
+using Application.Shared.RequestParamsRelated;
+using IntegrationTests.Common;
+
+namespace IntegrationTests.LocationRegions;
+
+public sealed class LocationRegionQueriesIntegrationTests(
+    IntegrationTestsWebApplicationFactory factory) : IntegrationTestBase(factory)
+{
+    [Fact]
+    public async Task Send_CollectionRequest_Should_ReturnCollectionOfLocationRegions()
+    {
+        // Arrange
+        var filteringRequestParameters = new VehicleLocationRegionFilteringRequestParameters
+        {
+            ItemQuantity = ItemQuantitySelection.Ten,
+            SortingType = "name-asc",
+            PageIndex = 1
+        };
+        var queryRequest = new CollectionLocationRegionsQueryRequest(filteringRequestParameters);
+
+        // Act
+        var response = await Sender.Send(queryRequest);
+        var regions = response.Value!.Items;
+
+        // Assert
+        response.Error
+            .Should().BeNull();
+        response.IsSuccess
+            .Should().BeTrue();
+
+        regions
+            .Should().BeInAscendingOrder(b => b.Name);
+        regions.Count
+            .Should().BeLessOrEqualTo((int)ItemQuantitySelection.Ten);
+    }
+}
