fd63309bf4aa0925f33be9ebb53c1a4cb941991a
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-13 16:18:31 +0300

    Create VehicleModelCommandIntegrationTests.cs

---
+++ tests/IntegrationTests/VehicleModels/VehicleModelCommandIntegrationTests.cs
@@ -0,0 +1,81 @@
+﻿using Application.VehicleModels.Commands.CreateVehicleModelRelated;
+using Domain.Entities.VehicleRelated;
+using Domain.Entities.VehicleRelated.TypeRelated;
+using IntegrationTests.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntegrationTests.VehicleModels;
+
+public sealed class VehicleModelCommandIntegrationTests(
+    IntegrationTestsWebApplicationFactory factory) : IntegrationTestBase(factory)
+{
+    [Fact]
+    public async Task Send_CreateRequest_Should_AddNewBrandIfRequestIsValid()
+    {
+        // Arrange
+        var commandRequest = await InstantiateValidCreateRequest();
+
+        // Act
+        var response = await Sender.Send(commandRequest);
+        var createdModel = await Context.FindAsync<VehicleModel>(response.Value);
+
+        // Assert
+        response.Error
+            .Should().BeNull();
+        response.IsSuccess
+            .Should().BeTrue();
+        response.Value
+            .Should().NotBeEmpty();
+
+        createdModel
+            .Should().NotBeNull();
+        createdModel!.Name
+            .Should().Be(commandRequest.Name);
+    }
+
+    private async Task<CreateVehicleModelCommandRequest> InstantiateValidCreateRequest()
+    {
+        const string name = "test";
+        var brand = await Context.Set<VehicleBrand>().FirstAsync();
+        var type = await Context.Set<VehicleType>().FirstAsync();
+        const int minimalProductionYear = 2005;
+        const int maximumProductionYear = 2010;
+
+        IEnumerable<VehicleEngineType> availableEngineTypes =
+        [
+            await Context.Set<VehicleEngineType>().OrderBy(x => x.Name).FirstAsync(),
+            await Context.Set<VehicleEngineType>().OrderBy(x => x.Name).LastAsync()
+        ];
+
+        IEnumerable<VehicleTransmissionType> availableTransmissionTypes =
+        [
+            await Context.Set<VehicleTransmissionType>().OrderBy(x => x.Name).FirstAsync(),
+            await Context.Set<VehicleTransmissionType>().OrderBy(x => x.Name).LastAsync()
+        ];
+
+        IEnumerable<VehicleDrivetrainType> availableDrivetrainTypes =
+        [
+            await Context.Set<VehicleDrivetrainType>().OrderBy(x => x.Name).FirstAsync(),
+            await Context.Set<VehicleDrivetrainType>().OrderBy(x => x.Name).LastAsync()
+        ];
+
+        IEnumerable<VehicleBodyType> availableBodyTypes =
+        [
+            await Context.Set<VehicleBodyType>().OrderBy(x => x.Name).FirstAsync(),
+            await Context.Set<VehicleBodyType>().OrderBy(x => x.Name).LastAsync()
+        ];
+
+
+        return new CreateVehicleModelCommandRequest(
+            name,
+            brand.Id,
+            type.Id,
+            minimalProductionYear,
+            maximumProductionYear,
+            availableEngineTypes.Select(x => x.Id),
+            availableTransmissionTypes.Select(x => x.Id),
+            availableDrivetrainTypes.Select(x => x.Id),
+            availableBodyTypes.Select(x => x.Id)
+        );
+    }
+}
