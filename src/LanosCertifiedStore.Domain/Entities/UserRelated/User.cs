b30fe7fa15a2ce89469d3af2589897d5ac7cd3e7
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-16 18:43:41 +0300

    Added IdentityId property in User.cs

--- src/LanosCertifiedStore.Domain/Entities/UserRelated/User.cs
+++ src/LanosCertifiedStore.Domain/Entities/UserRelated/User.cs
@@ -10,6 +10,7 @@ public sealed class User : IIdentifiable<Guid>
     public string LastName { get; set; } = null!;
     public string PhoneNumber { get; set; } = null!;
     public string Email { get; set; } = null!;
+    public string IdentityId { get; set; } = null!;
     public ICollection<Vehicle> Vehicles { get; set; } = [];
 
     public User() { }
