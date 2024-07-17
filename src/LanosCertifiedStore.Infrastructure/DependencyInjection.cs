3cb65117cc5c77b2456f9b12343b343a7e5d9389
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-17 13:37:08 +0300

    Refactored DependencyInjection.cs

--- src/LanosCertifiedStore.Infrastructure/DependencyInjection.cs
+++ src/LanosCertifiedStore.Infrastructure/DependencyInjection.cs
@@ -16,6 +16,7 @@ using LanosCertifiedStore.InfrastructureLayer.Services.Vehicles;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace LanosCertifiedStore.InfrastructureLayer.Services;
 
@@ -23,37 +24,50 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddExternalServices(this IServiceCollection services, IConfiguration config)
     {
-        AddCloudinaryConfiguration(services, config);
+        AddImageRelatedServices(services);
+        AddVehicleRelatedServices(services);
+        AddTypeRelatedServices(services);
+        AddLocationRelatedServices(services);
+        AddAuthenticationRelatedServices(services);
 
+        return services;
+    }
+
+    private static void AddAuthenticationRelatedServices(IServiceCollection services)
+    {
+        services
+            .AddAuthentication()
+            .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme);
+        services.AddHttpContextAccessor();
+        services.ConfigureOptions<JwtBearerConfigureOptions>();
+    }
+
+    private static void AddImageRelatedServices(IServiceCollection services)
+    {
+        services.AddSingleton<IConfigureOptions<CloudinaryOptions>, CloudinaryConfigureOptions>();
+        services.ConfigureOptions<CloudinaryConfigureOptions>();
         services.AddScoped<IImageService, ImageService>();
+    }
+
+    private static void AddVehicleRelatedServices(IServiceCollection services)
+    {
         services.AddScoped<IVehicleBrandService, VehicleBrandService>();
         services.AddScoped<IVehicleColorService, VehicleColorService>();
+        services.AddScoped<IVehicleModelService, VehicleModelService>();
+    }
+
+    private static void AddTypeRelatedServices(IServiceCollection services)
+    {
         services.AddScoped<IVehicleTypeService, VehicleTypeService>();
         services.AddScoped<IVehicleBodyTypeService, VehicleBodyTypeService>();
         services.AddScoped<IVehicleDrivetrainTypeService, VehicleDrivetrainTypeService>();
         services.AddScoped<IVehicleTransmissionTypeService, VehicleTransmissionTypeService>();
         services.AddScoped<IVehicleEngineTypeService, VehicleEngineTypeService>();
-        services.AddScoped<ILocationRegionService, LocationRegionService>();
-        services.AddScoped<ILocationTownService, LocationTownService>();
-        services.AddScoped<IVehicleModelService, VehicleModelService>();
-
-        services
-            .AddAuthentication()
-            .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme);
-
-        services.Configure<AuthenticationOptions>(config.GetSection("Authentication"));
-        services.ConfigureOptions<JwtBearerOptionsSetup>();
-
-        return services;
     }
 
-    private static void AddCloudinaryConfiguration(IServiceCollection services, IConfiguration config)
+    private static void AddLocationRelatedServices(IServiceCollection services)
     {
-        services.Configure<CloudinarySettings>(cloudinarySettings =>
-        {
-            cloudinarySettings.ApiKey = config.GetSection("Cloudinary")["ApiKey"]!;
-            cloudinarySettings.ApiSecret = config.GetSection("Cloudinary")["ApiSecret"]!;
-            cloudinarySettings.CloudName = config.GetSection("Cloudinary")["CloudName"]!;
-        });
+        services.AddScoped<ILocationRegionService, LocationRegionService>();
+        services.AddScoped<ILocationTownService, LocationTownService>();
     }
 }
