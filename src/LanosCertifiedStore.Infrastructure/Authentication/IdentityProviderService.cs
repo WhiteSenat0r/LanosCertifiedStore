159e61c3ef7955c9379679207b4cd79976c8d241
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-24 23:12:16 +0300

    Temporarily removed Keycloak-related classes implementations

--- src/LanosCertifiedStore.Infrastructure/Authentication/IdentityProviderService.cs
+++ src/LanosCertifiedStore.Infrastructure/Authentication/IdentityProviderService.cs
@@ -1,5 +1,4 @@
-﻿using System.Net;
-using Application.Identity;
+﻿using Application.Identity;
 using Application.Identity.Commands.RegisterUserCommandRequestRelated;
 using Application.Shared.ResultRelated;
 using LanosCertifiedStore.InfrastructureLayer.Services.Authentication.KeyCloak;
@@ -11,33 +10,10 @@ internal sealed class IdentityProviderService(
     KeyCloakClient keyCloakClient,
     ILogger<IdentityProviderService> logger) : IIdentityProviderService
 {
-    private const string PasswordCredentialType = "Password";
-
     public async Task<Result<string>> RegisterAsync(
         RegisterUserCommandRequest commandRequest,
         CancellationToken cancellationToken = default)
     {
-        var userRepresentation = new UserRepresentation(
-            commandRequest.Email,
-            commandRequest.Email,
-            // commandRequest.PhoneNumber,
-            commandRequest.FirstName,
-            commandRequest.LastName,
-            EmailVerified: false,
-            Enabled: true,
-            [new CredentialRepresentation(PasswordCredentialType, commandRequest.Password, false)]);
-
-        try
-        {
-            var identityId = await keyCloakClient.RegisterUserAsync(userRepresentation, cancellationToken);
-
-            return Result<string>.Success(identityId);
-        }
-        catch (HttpRequestException e) when (e.StatusCode == HttpStatusCode.Conflict)
-        {
-            logger.LogError(e, "User registration failed");
-
-            return Result<string>.Failure(IdentityProviderErrors.EmailIsNotUnique);
-        }
+        throw new NotImplementedException();
     }
 }
--- src/LanosCertifiedStore.Infrastructure/Authentication/KeyCloak/KeyCloakClient.cs
+++ src/LanosCertifiedStore.Infrastructure/Authentication/KeyCloak/KeyCloakClient.cs
@@ -1,41 +1,6 @@
-﻿using System.Net.Http.Json;
-
-namespace LanosCertifiedStore.InfrastructureLayer.Services.Authentication.KeyCloak;
+﻿namespace LanosCertifiedStore.InfrastructureLayer.Services.Authentication.KeyCloak;
 
 internal sealed class KeyCloakClient(HttpClient httpClient)
 {
-    internal async Task<string> RegisterUserAsync(
-        UserRepresentation user,
-        CancellationToken cancellationToken = default)
-    {
-        var httpResponseMessage = await httpClient.PostAsJsonAsync(
-            "users",
-            user,
-            cancellationToken);
-
-        httpResponseMessage.EnsureSuccessStatusCode();
-
-        return ExtractIdentityIdFromLocationHeader(httpResponseMessage);
-    }
-
-    private static string ExtractIdentityIdFromLocationHeader(
-        HttpResponseMessage httpResponseMessage)
-    {
-        const string usersSegmentName = "users/";
-
-        var locationHeader = httpResponseMessage.Headers.Location?.PathAndQuery;
-
-        if (locationHeader is null)
-        {
-            throw new InvalidOperationException("Location header is null");
-        }
-
-        var userSegmentValueIndex = locationHeader.IndexOf(
-            usersSegmentName,
-            StringComparison.InvariantCultureIgnoreCase);
-
-        var identityId = locationHeader.Substring(userSegmentValueIndex + usersSegmentName.Length);
-
-        return identityId;
-    }
+    // TODO Add new implementation later
 }
