43837b9f92b2e99b2d5861933765529910e50c44
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-24 19:05:23 +0300

    RolePermission entity added

--- src/LanosCertifiedStore.Persistence/Contexts/ApplicationDatabaseContext/Configurations/IdentityRelated/PermissionEntityConfiguration.cs
+++ src/LanosCertifiedStore.Persistence/Contexts/ApplicationDatabaseContext/Configurations/IdentityRelated/PermissionEntityConfiguration.cs
@@ -29,54 +29,5 @@ internal sealed class PermissionEntityConfiguration : IEntityTypeConfiguration<P
             Permission.CreateModel,
             Permission.UpdateModel
         );
-
-        builder
-            .HasMany<UserRole>()
-            .WithMany()
-            .UsingEntity(joinBuilder =>
-                {
-                    joinBuilder.ToTable("RolePermissions", DatabaseSchemas.IdentitySchema);
-
-                    joinBuilder.HasData(
-                        // User
-                        CreateRolePermission(UserRole.User, Permission.GetUser),
-                        CreateRolePermission(UserRole.User, Permission.CreateVehicles),
-                        // Manager
-                        CreateRolePermission(UserRole.Manager, Permission.GetUser),
-                        CreateRolePermission(UserRole.Manager, Permission.ListUsers),
-                        CreateRolePermission(UserRole.Manager, Permission.CreateUser),
-                        CreateRolePermission(UserRole.Manager, Permission.UpdateUser),
-                        CreateRolePermission(UserRole.Manager, Permission.DeleteUser),
-                        CreateRolePermission(UserRole.Manager, Permission.CreateVehicles),
-                        CreateRolePermission(UserRole.Manager, Permission.UpdateVehicles),
-                        CreateRolePermission(UserRole.Manager, Permission.CreateBrands),
-                        CreateRolePermission(UserRole.Manager, Permission.UpdateBrands),
-                        CreateRolePermission(UserRole.Manager, Permission.CreateModel),
-                        CreateRolePermission(UserRole.Manager, Permission.UpdateModel),
-                        // Administrator
-                        CreateRolePermission(UserRole.Administrator, Permission.GetUser),
-                        CreateRolePermission(UserRole.Administrator, Permission.ListUsers),
-                        CreateRolePermission(UserRole.Administrator, Permission.CreateUser),
-                        CreateRolePermission(UserRole.Administrator, Permission.UpdateUser),
-                        CreateRolePermission(UserRole.Administrator, Permission.ChangeUserRole),
-                        CreateRolePermission(UserRole.Administrator, Permission.DeleteUser),
-                        CreateRolePermission(UserRole.Administrator, Permission.CreateVehicles),
-                        CreateRolePermission(UserRole.Administrator, Permission.UpdateVehicles),
-                        CreateRolePermission(UserRole.Administrator, Permission.CreateBrands),
-                        CreateRolePermission(UserRole.Administrator, Permission.UpdateBrands),
-                        CreateRolePermission(UserRole.Administrator, Permission.CreateModel),
-                        CreateRolePermission(UserRole.Administrator, Permission.UpdateModel)
-                    );
-                }
-            );
-    }
-
-    private static object CreateRolePermission(UserRole role, Permission permission)
-    {
-        return new
-        {
-            UserRoleName = role.Name,
-            PermissionCode = permission.Code
-        };
     }
 }
