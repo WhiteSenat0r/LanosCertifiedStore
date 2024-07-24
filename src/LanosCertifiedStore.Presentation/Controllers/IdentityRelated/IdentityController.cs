e16c04ee4c94b92aa7d2d570ab6790d9b187fb60
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-24 23:10:05 +0300

    Added dummy method for authorization testing

--- src/LanosCertifiedStore.Presentation/Controllers/IdentityRelated/IdentityController.cs
+++ src/LanosCertifiedStore.Presentation/Controllers/IdentityRelated/IdentityController.cs
@@ -2,6 +2,9 @@
 using API.Controllers.Common;
 using Application.Identity.Commands.RegisterUserCommandRequestRelated;
 using Application.Shared.ValidationRelated;
+using Domain.Entities.UserRelated;
+using LanosCertifiedStore.InfrastructureLayer.Services.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers.IdentityRelated;
@@ -9,6 +12,13 @@ namespace API.Controllers.IdentityRelated;
 [Route("api/identity")]
 public sealed class IdentityController : BaseApiController
 {
+    [HttpGet("dummy")]
+    [HasAccessPermission("users:read")]
+    public async Task<ActionResult<string>> Foo()
+    {
+        return Ok("NIGGER");
+    }
+    
     [HttpPost("register")]
     public async Task<ActionResult<Guid>> RegisterUser(RegisterUserCommandRequest registerUserCommandRequest)
     {
