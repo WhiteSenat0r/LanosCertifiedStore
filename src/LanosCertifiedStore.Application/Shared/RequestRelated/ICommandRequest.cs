0db6f81aa680adcb2394b4baf8e18d9f240dfb9f
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-26 15:21:06 +0300

    ICommandRequestBase and non generic ICommandRequest interfaces added

--- src/LanosCertifiedStore.Application/Shared/RequestRelated/ICommandRequest.cs
+++ src/LanosCertifiedStore.Application/Shared/RequestRelated/ICommandRequest.cs
@@ -3,4 +3,8 @@ using MediatR;
 
 namespace LanosCertifiedStore.Application.Shared.RequestRelated;
 
-public interface ICommandRequest<TResult> : IRequest<Result<TResult>> where TResult : notnull;
+public interface ICommandRequest<TResult> : ICommandRequestBase, IRequest<Result<TResult>> where TResult : notnull;
+
+public interface ICommandRequest : ICommandRequestBase, IRequest<Result>;
+
+public interface ICommandRequestBase;
