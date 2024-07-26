eb0d8033814e8b516fbb45ab71ea99481c23c4e3
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-26 14:00:14 +0300

    Removed PublicClientId in KeyCloakOptions.cs

--- src/LanosCertifiedStore.Infrastructure/Authentication/KeyCloak/KeycloakOptions.cs
+++ src/LanosCertifiedStore.Infrastructure/Authentication/KeyCloak/KeycloakOptions.cs
@@ -1,10 +1,9 @@
 ﻿namespace LanosCertifiedStore.InfrastructureLayer.Services.Authentication.KeyCloak;
 
-internal sealed class KeyCloakOptions
+internal sealed class KeycloakOptions
 {
     public string AdminUrl { get; set; }
     public string TokenUrl { get; set; }
     public string ConfidentialClientId { get; set; }
     public string ConfidentialClientSecret { get; set; }
-    public string PublicClientId { get; set; }
 }
