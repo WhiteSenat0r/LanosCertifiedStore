96ce9153a4cf57ca0a1b0181db48e6684b29c1e8
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-26 13:59:17 +0300

    Updated KeycloakAdminAuthToken.cs

--- src/LanosCertifiedStore.Infrastructure/Authentication/KeyCloak/KeycloakAdminAuthToken.cs
+++ src/LanosCertifiedStore.Infrastructure/Authentication/KeyCloak/KeycloakAdminAuthToken.cs
@@ -2,8 +2,8 @@ using System.Text.Json.Serialization;
 
 namespace LanosCertifiedStore.InfrastructureLayer.Services.Authentication.KeyCloak;
 
-internal sealed class AuthToken
+internal sealed class KeycloakAdminAuthToken
 {
     [JsonPropertyName("access_token")]
-    public string AcessToken { get; set; }
+    public string AccessToken { get; set; } = null!;
 }
