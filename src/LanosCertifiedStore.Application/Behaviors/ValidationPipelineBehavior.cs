02088f377876bc595574adb75dbcb494d0bf4c15
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-28 00:14:13 +0300

    fixed IndexOutOfRange exception issues when generating validation result for requests using non-generic Result version

--- src/LanosCertifiedStore.Application/Behaviors/ValidationPipelineBehavior.cs
+++ src/LanosCertifiedStore.Application/Behaviors/ValidationPipelineBehavior.cs
@@ -30,12 +30,12 @@ public class ValidationPipelineBehavior<TRequest, TResponse>(
         }
 
         var validationResult = CreateValidationResult<TResponse>(errors);
-        
+
         logger.LogError("Validation failures have occured: {@ValidationResult}", validationResult);
         return validationResult;
     }
 
-    private bool AreErrorsPresent(Error[] errors)
+    private static bool AreErrorsPresent(Error[] errors)
     {
         const int emptyValue = 0;
 
@@ -57,15 +57,20 @@ public class ValidationPipelineBehavior<TRequest, TResponse>(
             .ToArray();
     }
 
-    private TResult CreateValidationResult<TResult>(Error[] errors)
+    private static TResult CreateValidationResult<TResult>(Error[] errors)
         where TResult : Result
     {
+        if (typeof(TResult) == typeof(Result))
+        {
+            return (ValidationResult.WithErrors(errors) as TResult)!;
+        }
+
         const int genericTypeArgumentIndex = 0;
 
         var validationResult = typeof(ValidationResult<>)
             .GetGenericTypeDefinition()
             .MakeGenericType(typeof(TResult).GenericTypeArguments[genericTypeArgumentIndex])
-            .GetMethod(nameof(ValidationResult<TResult>.WithErrors))!
+            .GetMethod(nameof(ValidationResult.WithErrors))!
             .Invoke(null, [errors])!;
 
         return (TResult)validationResult;
--- src/LanosCertifiedStore.Application/Shared/ResultRelated/ValidationResult.cs
+++ src/LanosCertifiedStore.Application/Shared/ResultRelated/ValidationResult.cs
@@ -2,10 +2,25 @@
 
 namespace LanosCertifiedStore.Application.Shared.ResultRelated;
 
+public sealed class ValidationResult : Result, IValidationResult
+{
+    private ValidationResult(Error[] errors)
+        : base(false, IValidationResult.ValidationError)
+    {
+        Errors = errors;
+    }
+
+    public Error[] Errors { get; }
+
+    public static ValidationResult WithErrors(Error[] errors) => new(errors);
+}
+
 public sealed class ValidationResult<TValue> : Result<TValue>, IValidationResult
 {
-    private ValidationResult(Error[] errors) : base(default, false, IValidationResult.ValidationError) =>
+    private ValidationResult(Error[] errors) : base(default, false, IValidationResult.ValidationError)
+    {
         Errors = errors;
+    }
 
     public Error[] Errors { get; }
 
