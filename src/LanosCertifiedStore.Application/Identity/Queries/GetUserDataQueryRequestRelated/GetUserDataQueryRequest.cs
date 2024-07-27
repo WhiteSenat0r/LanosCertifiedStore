9f3e181044fa869958cd2b89a31ce5cdab0af625
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-27 10:58:41 +0300

    Added GetUserDataQueryRequest.cs and its handler

---
+++ src/LanosCertifiedStore.Application/Identity/Queries/GetUserDataQueryRequestRelated/GetUserDataQueryRequest.cs
@@ -0,0 +1,6 @@
+﻿using LanosCertifiedStore.Application.Shared.RequestRelated.QueryRelated;
+using LanosCertifiedStore.Application.Shared.ResultRelated;
+
+namespace LanosCertifiedStore.Application.Identity.Queries.GetUserDataQueryRequestRelated;
+
+public sealed record GetUserDataQueryRequest(Guid Id) : IQueryRequest<Result<UserDataDto>>;
---
+++ src/LanosCertifiedStore.Application/Identity/Queries/GetUserDataQueryRequestRelated/GetUserDataQueryRequestHandler.cs
@@ -0,0 +1,15 @@
+﻿using LanosCertifiedStore.Application.Shared.ResultRelated;
+using MediatR;
+
+namespace LanosCertifiedStore.Application.Identity.Queries.GetUserDataQueryRequestRelated;
+
+internal sealed class GetUserDataQueryRequestHandler(IIdentityProviderService identityProviderService) 
+    : IRequestHandler<GetUserDataQueryRequest, Result<UserDataDto>>
+{
+    public async Task<Result<UserDataDto>> Handle(GetUserDataQueryRequest request, CancellationToken cancellationToken)
+    {
+        var result = await identityProviderService.GetUserDataAsync(request, cancellationToken);
+
+        return result;
+    }
+}
