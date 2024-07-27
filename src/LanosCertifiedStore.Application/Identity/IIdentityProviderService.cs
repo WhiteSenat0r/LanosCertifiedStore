c3ea4aa6aeb97cadb9165ae68e02c8678e672e45
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-27 11:00:23 +0300

    Added GetUserDataAsync method in IdentityProviderService.cs

--- src/LanosCertifiedStore.Application/Identity/IIdentityProviderService.cs
+++ src/LanosCertifiedStore.Application/Identity/IIdentityProviderService.cs
@@ -1,10 +1,15 @@
 ﻿using LanosCertifiedStore.Application.Identity.Commands.UpdateUserDataCommandRequestRelated;
+using LanosCertifiedStore.Application.Identity.Queries.GetUserDataQueryRequestRelated;
 using LanosCertifiedStore.Application.Shared.ResultRelated;
 
 namespace LanosCertifiedStore.Application.Identity;
 
 public interface IIdentityProviderService
 {
+    Task<Result<UserDataDto>> GetUserDataAsync(
+        GetUserDataQueryRequest request,
+        CancellationToken cancellationToken = default);
+    
     Task<Result> UpdateUserDataAsync(
         UpdateUserDataCommandRequest request,
         CancellationToken cancellationToken = default);
--- src/LanosCertifiedStore.Infrastructure/Authentication/IdentityProviderService.cs
+++ src/LanosCertifiedStore.Infrastructure/Authentication/IdentityProviderService.cs
@@ -1,8 +1,10 @@
 ﻿using System.Net;
 using LanosCertifiedStore.Application.Identity;
 using LanosCertifiedStore.Application.Identity.Commands.UpdateUserDataCommandRequestRelated;
+using LanosCertifiedStore.Application.Identity.Queries.GetUserDataQueryRequestRelated;
 using LanosCertifiedStore.Application.Shared.ResultRelated;
-using LanosCertifiedStore.Infrastructure.Services.Authentication.Keycloak;
+using LanosCertifiedStore.Infrastructure.Services.Authentication.DataRepresentations;
+using LanosCertifiedStore.Infrastructure.Services.Authentication.KeyCloak;
 using Microsoft.Extensions.Logging;
 
 namespace LanosCertifiedStore.Infrastructure.Services.Authentication;
@@ -11,6 +13,23 @@ internal sealed class IdentityProviderService(
     KeycloakClient keycloakClient,
     ILogger<IdentityProviderService> logger) : IIdentityProviderService
 {
+    public async Task<Result<UserDataDto>> GetUserDataAsync(
+        GetUserDataQueryRequest request,
+        CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var result = await keycloakClient.GetUserDataAsync(request.Id, cancellationToken);
+            return Result<UserDataDto>.Success(result);
+        }
+        catch (HttpRequestException e) when (e.StatusCode is HttpStatusCode.NotFound)
+        {
+            const string userWithProvidedItWasNotFound = "User with provided it was not found!";
+            logger.LogError(e, userWithProvidedItWasNotFound);
+            return Result<UserDataDto>.Failure(new Error("IdentityDataReceival", userWithProvidedItWasNotFound));
+        }
+    }
+
     public async Task<Result> UpdateUserDataAsync(
         UpdateUserDataCommandRequest request,
         CancellationToken cancellationToken = default)
