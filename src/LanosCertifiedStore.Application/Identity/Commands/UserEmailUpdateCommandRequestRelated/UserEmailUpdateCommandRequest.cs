0f91adae4a80243074b6b939e6fcd5049f0b4698
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-28 23:07:03 +0300

    Added UserEmailUpdateCommandRequest and its handler

---
+++ src/LanosCertifiedStore.Application/Identity/Commands/UserEmailUpdateCommandRequestRelated/UserEmailUpdateCommandRequest.cs
@@ -0,0 +1,5 @@
+﻿using LanosCertifiedStore.Application.Shared.RequestRelated;
+
+namespace LanosCertifiedStore.Application.Identity.Commands.UserEmailUpdateCommandRequestRelated;
+
+public sealed record UserEmailUpdateCommandRequest(string NewEmail) : ICommandRequest;
---
+++ src/LanosCertifiedStore.Application/Identity/Commands/UserEmailUpdateCommandRequestRelated/UserEmailUpdateCommandRequestHandler.cs
@@ -0,0 +1,34 @@
+﻿using LanosCertifiedStore.Application.Shared.ResultRelated;
+using MediatR;
+
+namespace LanosCertifiedStore.Application.Identity.Commands.UserEmailUpdateCommandRequestRelated;
+
+internal sealed class UserEmailUpdateCommandRequestHandler(
+    IIdentityProviderService identityService,
+    IUserContext userContext) : IRequestHandler<UserEmailUpdateCommandRequest, Result>
+{
+    public async Task<Result> Handle(UserEmailUpdateCommandRequest request, CancellationToken cancellationToken)
+    {
+        var userId = userContext.UserId;
+        
+        var userDataResult = await identityService.GetUserDataAsync(userId, cancellationToken);
+        
+        if (!userDataResult.IsSuccess)
+        {
+            return userDataResult;
+        }
+
+        if (userDataResult.Value!.Email.Equals(request.NewEmail))
+        {
+            return Result.Create(
+                new Error("SameEmailUpdate", "Current email address is identical to the new one!"));
+        }
+        
+        var result = await identityService.UpdateUserEmailAsync(
+            userId,
+            request.NewEmail,
+            cancellationToken);
+        
+        return result;
+    }
+}
