66a916794dfccada70b0cd07340df70516cc7f8c
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-28 16:28:51 +0300

    Create UserDataRepresentation.cs

---
+++ src/LanosCertifiedStore.Infrastructure/Authentication/Keycloak/UserDataRepresentation.cs
@@ -0,0 +1,15 @@
+﻿using LanosCertifiedStore.Application.Identity;
+
+namespace LanosCertifiedStore.Infrastructure.Authentication.Keycloak;
+
+internal sealed record UserDataRepresentation(
+    string Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    Attributes Attributes,
+    bool EmailVerified,
+    List<FederatedIdentityDto> FederatedIdentities,
+    long CreatedTimestamp);
+
+internal sealed record Attributes(List<string> PhoneNumber);
