68bc1ea6eddfec227891127e65e8098756977a49
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-29 20:49:05 +0300

    Added KeycloakRequiredActions.cs

---
+++ src/LanosCertifiedStore.Infrastructure/Authentication/KeyCloak/KeycloakRequiredActions.cs
@@ -0,0 +1,14 @@
+﻿namespace LanosCertifiedStore.Infrastructure.Authentication.KeyCloak;
+
+internal static class KeycloakRequiredActions
+{
+    public static List<string> GetVerifyEmailCode()
+    {
+        return ["VERIFY_EMAIL"];
+    }
+    
+    public static List<string> GetUpdatePasswordCode()
+    {
+        return ["VERIFY_EMAIL"];
+    }
+}
