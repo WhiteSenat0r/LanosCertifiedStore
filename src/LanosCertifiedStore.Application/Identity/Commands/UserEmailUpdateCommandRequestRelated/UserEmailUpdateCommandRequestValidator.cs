135b0042cf8804d237be727a532b3ae883012832
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-30 15:10:49 +0300

    Create UserEmailUpdateCommandRequestValidator.cs

---
+++ src/LanosCertifiedStore.Application/Identity/Commands/UserEmailUpdateCommandRequestRelated/UserEmailUpdateCommandRequestValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace LanosCertifiedStore.Application.Identity.Commands.UserEmailUpdateCommandRequestRelated;
+
+internal sealed class UserEmailUpdateCommandRequestValidator : AbstractValidator<UserEmailUpdateCommandRequest>
+{
+    public UserEmailUpdateCommandRequestValidator()
+    {
+        RuleFor(x => x.NewEmail).EmailAddress();
+    }
+}
