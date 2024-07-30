bc357b90db54fa22f79d3edbb163c293a70df95c
Davyd Melnychuk <davyd.melnychuk@gmail.com>
2024-07-30 17:20:39 +0300

    Added PublicClientId property in KeycloakOptions.cs

--- src/LanosCertifiedStore.Infrastructure/Authentication/Keycloak/KeycloakOptions.cs
+++ src/LanosCertifiedStore.Infrastructure/Authentication/Keycloak/KeycloakOptions.cs
@@ -1,9 +1,10 @@
 ﻿namespace LanosCertifiedStore.Infrastructure.Authentication.KeyCloak;
 
-internal sealed class KeycloakOptions
+public sealed class KeycloakOptions
 {
     public string AdminUrl { get; set; }
     public string TokenUrl { get; set; }
+    public string PublicClientId { get; set; }
     public string ConfidentialClientId { get; set; }
     public string ConfidentialClientSecret { get; set; }
 }
