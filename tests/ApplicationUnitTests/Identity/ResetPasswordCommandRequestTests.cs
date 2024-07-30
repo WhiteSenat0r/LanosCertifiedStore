18d421fdc45b9125e1e54a2d853cc6b02d580241
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-30 15:39:25 +0300

    Create ResetPasswordCommandRequestTests.cs

---
+++ tests/ApplicationUnitTests/Identity/ResetPasswordCommandRequestTests.cs
@@ -0,0 +1,58 @@
+﻿using LanosCertifiedStore.Application.Identity;
+using LanosCertifiedStore.Application.Identity.Commands.ResetPasswordCommandRequestRelated;
+using LanosCertifiedStore.Application.Shared.ResultRelated;
+
+namespace ApplicationUnitTests.Identity;
+
+public sealed class ResetPasswordCommandRequestTests
+{
+    private readonly IIdentityProviderService _identityProviderService = Substitute.For<IIdentityProviderService>();
+    private readonly IUserContext _userContext = Substitute.For<IUserContext>();
+    private readonly Guid _userId = Guid.NewGuid();
+    private readonly ResetPasswordCommandRequest _request = new();
+
+    [Fact]
+    public async Task Handle_Should_CallIdentityProviderService()
+    {
+        // Arrange
+        _userContext.UserId
+            .Returns(_userId);
+        
+        _identityProviderService
+            .ResetUserPassword(_userId)
+            .Returns(Result.Create(Error.None));
+
+        var handler = new ResetPasswordCommandRequestHandler(_userContext, _identityProviderService);
+        
+        // Act
+        await handler.Handle(_request, default);
+        
+        // Assert
+        await _identityProviderService
+            .Received(1)
+            .ResetUserPassword(_userId);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnSameResultAsIdentityProviderService()
+    {
+        // Arrange
+        _userContext.UserId
+            .Returns(_userId);
+        
+        _identityProviderService
+            .ResetUserPassword(_userId)
+            .Returns(Result.Create(Error.None));
+
+        var handler = new ResetPasswordCommandRequestHandler(_userContext, _identityProviderService);
+        
+        // Act
+        var result = await handler.Handle(_request, default);
+
+        // Assert
+        result.IsSuccess
+            .Should().BeTrue();
+        result.Error
+            .Should().BeEquivalentTo(Error.None);
+    }
+}
