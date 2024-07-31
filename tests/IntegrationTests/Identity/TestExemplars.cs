ab44f7018bd0ebfc0c87798121fa18dbc9ad0d45
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-31 15:37:52 +0300

    added UserRepresentationWithPasswordAndId for integration testing purposes

---
+++ tests/IntegrationTests/Identity/TestExemplars.cs
@@ -0,0 +1,28 @@
+﻿using LanosCertifiedStore.Infrastructure.Authentication.KeyCloak;
+
+namespace IntegrationTests.Identity;
+
+internal static class TestExemplars
+{
+    private const string Email = "test@test.com";
+    private const string Password = "123";
+
+    internal static UserRepresentationWithPasswordAndId GetCorrectUserRepresentationWithPasswordAndId()
+    {
+        var attributes = new Dictionary<string, string>
+        {
+            { "phoneNumber", "+380123456789" }
+        };
+
+        return new UserRepresentationWithPasswordAndId(
+            null,
+            Email,
+            Email,
+            true,
+            true,
+            attributes,
+            "test",
+            "test",
+            [new CredentialRepresentation("password", Password, false)]);
+    }
+}
