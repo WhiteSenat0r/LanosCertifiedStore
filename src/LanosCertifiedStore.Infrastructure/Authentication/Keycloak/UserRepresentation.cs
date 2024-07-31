7e88cf8e44b47379dd6bb9115afb56aa4cd5410a
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-31 15:43:43 +0300

    UserRepresentationWithPasswordAndId for integration testing purposes added

--- src/LanosCertifiedStore.Infrastructure/Authentication/Keycloak/UserRepresentation.cs
+++ src/LanosCertifiedStore.Infrastructure/Authentication/Keycloak/UserRepresentation.cs
@@ -8,3 +8,26 @@ internal record UserRepresentation(
     string FirstName,
     string LastName,
     List<string> RequiredActions = null!);
+
+
+/// <summary>
+/// Represents a user with an additional ID and password for registration purposes.
+/// </summary>
+/// <remarks>
+/// This record is used only for integration testing and includes fields for user information and credentials.
+/// </remarks>
+/// <param name="Id">The unique identifier for the user, which may be null if not set.</param>
+/// <param name="Enabled">Specifies whether the user account is enabled.</param>
+/// <param name="Credentials">A list of credential representations for the user, such as passwords or other authentication methods.</param>
+internal sealed record UserRepresentationWithPasswordAndId(
+    Guid? Id,
+    string Username,
+    string Email,
+    bool EmailVerified,
+    bool Enabled,
+    Dictionary<string, string> Attributes,
+    string FirstName,
+    string LastName,
+    List<CredentialRepresentation> Credentials,
+    List<string> RequiredActions = null!)
+    : UserRepresentation(Username, Email, EmailVerified, Attributes, FirstName, LastName, RequiredActions);
