d511be4653a6d5403d17328a1f9dd510aa81c1cf
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-07-31 15:44:03 +0300

    Send_Should_ReturnSuccessResult_IfUserExists implemented

--- tests/IntegrationTests/Identity/GetUserDataQueryRequestTests.cs
+++ tests/IntegrationTests/Identity/GetUserDataQueryRequestTests.cs
@@ -1,7 +1,4 @@
-﻿using System.Net.Http.Headers;
-using System.Text;
-using System.Text.Json;
-using IntegrationTests.Common;
+﻿using IntegrationTests.Common;
 using LanosCertifiedStore.Application.Identity.Queries.GetUserDataQueryRequestRelated;
 using LanosCertifiedStore.Application.Shared.ResultRelated;
 using LanosCertifiedStore.Infrastructure.Authentication.KeyCloak;
@@ -11,9 +8,6 @@ namespace IntegrationTests.Identity;
 public sealed class GetUserDataQueryRequestTests(
     IntegrationTestsWebApplicationFactory factory) : IntegrationTestBase(factory)
 {
-    private const string Email = "test@test.com";
-    private const string Password = "123";
-
     [Fact]
     public async Task Send_Should_ReturnFailureResult_IfUserDoesNotExist()
     {
@@ -34,11 +28,10 @@ public sealed class GetUserDataQueryRequestTests(
     public async Task Send_Should_ReturnSuccessResult_IfUserExists()
     {
         // Arrange
-        var userId = Guid.NewGuid();
-        var user = await RegisterNewUser(userId);
+        var user = await RegisterNewUser();
 
         // Act
-        var result = await Sender.Send(new GetUserDataQueryRequest(userId));
+        var result = await Sender.Send(new GetUserDataQueryRequest((Guid)user.Id!));
 
         // Assert
         result.Error
@@ -46,54 +39,19 @@ public sealed class GetUserDataQueryRequestTests(
         result.Value
             .Should().NotBeNull();
         result.Value!.Email
-            .Should().Be(Email);
+            .Should().Be(user.Email);
     }
-    
 
-    private async Task<UserRepresentationWithPassword> RegisterNewUser(Guid id)
+
+    private async Task<UserRepresentationWithPasswordAndId> RegisterNewUser()
     {
-        var user = new UserRepresentationWithPassword(
-            id.ToString(),
-            Email,
-            Email,
-            true,
-            null!,
-            "test",
-            "test",
-            new CredentialRepresentation("password", Password, false));
+        var user = TestExemplars.GetCorrectUserRepresentationWithPasswordAndId();
 
-        var uri = KeycloakOptions.AdminUrl + "users";
-        using var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, uri);
+        var userId = Guid.Parse(await KeycloakClient.RegisterUserAsync(user));
 
-        httpRequestMessage.Headers.Authorization = new AuthenticationHeaderValue(
-            "Bearer",
-            await GetConfidentialAccessTokenAsync());
-
-        var jsonContent = JsonSerializer.Serialize(user);
-        httpRequestMessage.Content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
-
-        using var responseMessage = await HttpClient.SendAsync(httpRequestMessage);
-
-        // TODO FIGURE OUT WHY DOES IT RETURN NOTFOUND AND WHY DOES IT TRIGGER CustomClaimsTransformation.TransformAsync 
-        // TODO try one of the solutions: adding RegisterUser method to keycloak client idk how tho
-        
-        var response = await responseMessage.Content.ReadAsStringAsync();
-
-        responseMessage.EnsureSuccessStatusCode();
-
-        return user;
+        return user with
+        {
+            Id = userId
+        };
     }
 }
-
-
-internal sealed record UserRepresentationWithPassword(
-    string Id,
-    string Username,
-    string Email,
-    bool EmailVerified,
-    Dictionary<string, string> Attributes,
-    string FirstName,
-    string LastName,
-    CredentialRepresentation CredentialRepresentation,
-    List<string> RequiredActions = null!)
-    : UserRepresentation(Username, Email, EmailVerified, Attributes, FirstName, LastName, RequiredActions);
