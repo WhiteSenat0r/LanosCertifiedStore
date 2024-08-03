331a31793dd78cbe7b43fe05870d91c78f087b74
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-08-03 18:00:21 +0300

    VehicleService.AddAsync implemented

---
+++ src/LanosCertifiedStore.Infrastructure/Vehicles/VehicleService.cs
@@ -0,0 +1,17 @@
+﻿using LanosCertifiedStore.Application.Vehicles;
+using LanosCertifiedStore.Domain.Entities.VehicleRelated;
+using LanosCertifiedStore.Persistence.Commands.Common;
+using LanosCertifiedStore.Persistence.Commands.VehicleRelated;
+
+namespace LanosCertifiedStore.Infrastructure.Vehicles;
+
+internal sealed class VehicleService(
+    CreateVehicleCommand createVehicleCommand,
+    SaveChangesCommand saveChangesCommand) : IVehicleService
+{
+    public async Task AddAsync(Vehicle vehicle, CancellationToken cancellationToken)
+    {
+        await createVehicleCommand.Execute(vehicle, cancellationToken);
+        await saveChangesCommand.Execute(cancellationToken);
+    }
+}
---
+++ src/LanosCertifiedStore.Persistence/Commands/VehicleRelated/CreateVehicleCommand.cs
@@ -0,0 +1,12 @@
+﻿using LanosCertifiedStore.Domain.Entities.VehicleRelated;
+using LanosCertifiedStore.Persistence.Contexts.ApplicationDatabaseContext;
+
+namespace LanosCertifiedStore.Persistence.Commands.VehicleRelated;
+
+public sealed class CreateVehicleCommand(ApplicationDatabaseContext context)
+{
+    public async Task Execute(Vehicle vehicle, CancellationToken cancellationToken)
+    {
+        await context.AddAsync(vehicle, cancellationToken);
+    }
+}
